bug fix: linear layer's input channel calculation eq.
bug fix: linear layer's input channel calculation eq.

codes/models/archs/discriminator_vgg_arch.py
@@ -31,7 +31,7 @@ class Discriminator_VGG_128(nn.Module):
         self.conv4_1 = nn.Conv2d(nf * 8, nf * 8, 4, 2, 1, bias=False)
         self.bn4_1 = nn.BatchNorm2d(nf * 8, affine=True)
 
-        self.linear1 = nn.Linear(512 * 4 * 4, 100)
+        self.linear1 = nn.Linear(nf * 8 * 4 * 4, 100)
         self.linear2 = nn.Linear(100, 1)
 
         # activation function