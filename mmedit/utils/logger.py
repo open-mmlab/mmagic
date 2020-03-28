b17498ae58b0edf000162c1bf23e1ed7d4d28c5c
chenkai <chenkai@sensetime.com>
2020-03-28 17:11:47 +0800

    add utils

---
+++ mmedit/utils/logger.py
@@ -0,0 +1,26 @@
+import logging
+
+from mmcv.utils import get_logger
+
+
+def get_root_logger(log_file=None, log_level=logging.INFO):
+    """Get the root logger.
+
+    The logger will be initialized if it has not been initialized. By default a
+    StreamHandler will be added. If `log_file` is specified, a FileHandler will
+    also be added. The name of the root logger is the top-level package name,
+    e.g., "mmsr".
+
+    Args:
+        log_file (str | None): The log filename. If specified, a FileHandler
+            will be added to the root logger.
+        log_level (int): The root logger level. Note that only the process of
+            rank 0 is affected, while other processes will set the level to
+            "Error" and be silent most of the time.
+
+    Returns:
+        logging.Logger: The root logger.
+    """
+    # root logger name: mmedit
+    logger = get_logger(__name__.split('.')[0], log_file, log_level)
+    return logger
