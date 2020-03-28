0b6b8c6876a33df55f98a3fac00393d22633eba6
chenkai <chenkai@sensetime.com>
2020-03-28 22:22:17 +0800

    move build_dataloader to builder.py

--- mmedit/datasets/__init__.py
+++ mmedit/datasets/__init__.py
@@ -1,5 +1,4 @@
-from .build_dataloader import build_dataloader
-from .builder import build_dataset
+from .builder import build_dataloader, build_dataset
 from .registry import DATASETS, PIPELINES
 
 __all__ = ['DATASETS', 'PIPELINES', 'build_dataset', 'build_dataloader']
