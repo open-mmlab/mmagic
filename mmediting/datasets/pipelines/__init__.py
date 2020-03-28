441f76c786c00442ac6070885877e166cccb070b
chenkai <chenkai@sensetime.com>
2020-03-28 18:23:49 +0800

    Revert "Merge branch 'format' into 'master'"
    This reverts merge request !1

--- mmediting/datasets/pipelines/__init__.py
+++
@@ -1,6 +0,0 @@
-from .formating import (Collect, FormatTrimap, ImageToTensor, ToTensor,
-                        GetMaskedImage)
-
-__all__ = [
-    'Collect', 'FormatTrimap', 'ImageToTensor', 'ToTensor', 'GetMaskedImage'
-]
--- mmediting/datasets/registry.py
+++
@@ -1,3 +0,0 @@
-from mmcv.utils import Registry
-
-PIPELINES = Registry('pipeline')
