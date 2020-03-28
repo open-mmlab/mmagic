64d20dc0472be46592a3a42bf4203f4479f9b957
hejiamin <hejiamin@sensetime.com>
2020-03-28 20:36:13 +0800

    Add Compose

--- mmedit/datasets/pipelines/__init__.py
+++ mmedit/datasets/pipelines/__init__.py
@@ -1,8 +1,9 @@
+from .compose import Compose
 from .formating import (Collect, FormatTrimap, GetMaskedImage, ImageToTensor,
                         ToTensor)
 from .trimap import GenerateTrimap, MergeFgAndBg
 
 __all__ = [
     'Collect', 'FormatTrimap', 'ImageToTensor', 'ToTensor', 'GetMaskedImage',
-    'GenerateTrimap', 'MergeFgAndBg'
+    'Compose', 'GenerateTrimap', 'MergeFgAndBg'
 ]
