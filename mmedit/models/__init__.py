13214637c65d78dfaae4942963274831368dcdce
wangxintao <wangxintao@sensetime.com>
2020-04-02 22:42:28 +0800

    add esrgan model

--- mmedit/models/__init__.py
+++ mmedit/models/__init__.py
@@ -8,11 +8,11 @@ from .inpaintors import GLInpaintor, OneStageInpaintor
 from .losses import *  # noqa: F401, F403
 from .mattors import DIM, BaseMattor
 from .registry import BACKBONES, COMPONENTS, LOSSES, MODELS
-from .restorers import SRGAN, BasicRestorer
+from .restorers import ESRGAN, SRGAN, BasicRestorer
 
 __all__ = [
     'BaseModel', 'BasicRestorer', 'OneStageInpaintor', 'build',
     'build_backbone', 'build_component', 'build_loss', 'build_model',
     'BACKBONES', 'COMPONENTS', 'LOSSES', 'BaseMattor', 'DIM', 'MODELS',
-    'SRGAN', 'GLInpaintor'
+    'SRGAN', 'GLInpaintor', 'ESRGAN'
 ]
