ed4c533ef35db2d82bffd755fb43e93a4d44edb4
wangxintao <wangxintao@sensetime.com>
2020-04-22 22:47:23 +0800

    Eval hook

--- mmedit/core/evaluation/__init__.py
+++ mmedit/core/evaluation/__init__.py
@@ -1,3 +1,7 @@
+from .eval_hooks import DistEvalIterHook, EvalIterHook
 from .metrics import mse, psnr, reorder_image, sad, ssim
 
-__all__ = ['mse', 'sad', 'psnr', 'reorder_image', 'ssim']
+__all__ = [
+    'mse', 'sad', 'psnr', 'reorder_image', 'ssim', 'EvalIterHook',
+    'DistEvalIterHook'
+]
