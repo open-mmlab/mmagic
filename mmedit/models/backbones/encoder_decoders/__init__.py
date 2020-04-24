c18a9fa2fe0bcb28e75f4c9ac5575710ac47ce33
xurui4 <xurui4@sensetime.com>
2020-04-24 21:01:46 +0800

    add deepfill encdec

--- mmedit/models/backbones/encoder_decoders/__init__.py
+++ mmedit/models/backbones/encoder_decoders/__init__.py
@@ -8,12 +8,13 @@ from .gl_encoder_decoder import GLEncoderDecoder
 from .necks import ContextualAttentionNeck, GLDilationNeck
 from .pconv_encoder_decoder import PConvEncoderDecoder
 from .simple_encoder_decoder import SimpleEncoderDecoder
+from .two_stage_encoder_decoder import DeepFillEncoderDecoder
 
 __all__ = [
     'GLEncoderDecoder', 'SimpleEncoderDecoder', 'VGG16', 'GLEncoder',
     'PlainDecoder', 'GLDecoder', 'GLDilationNeck', 'PConvEncoderDecoder',
     'PConvEncoder', 'PConvDecoder', 'EncoderDecoder', 'ResNetEnc', 'ResNetDec',
     'ResShortcutEnc', 'ResShortcutDec', 'HolisticIndexBlock',
-    'DepthwiseIndexBlock', 'DeepFillEncoder', 'DeepFillDecoder',
-    'ContextualAttentionNeck'
+    'DepthwiseIndexBlock', 'DeepFillEncoder', 'DeepFillEncoderDecoder',
+    'DeepFillDecoder', 'ContextualAttentionNeck'
 ]
