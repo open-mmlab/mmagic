35b89aa74e1d6af56c8bd9f245df7fbddba9cdb2
wangxintao <wangxintao@sensetime.com>
2020-05-20 16:20:34 +0800

    add TOF net

--- mmedit/models/backbones/sr_backbones/__init__.py
+++ mmedit/models/backbones/sr_backbones/__init__.py
@@ -2,5 +2,6 @@ from .edsr import EDSR
 from .edvr_net import EDVRNet
 from .rrdb_net import RRDBNet
 from .sr_resnet import MSRResNet
+from .tof import TOFlow
 
-__all__ = ['MSRResNet', 'RRDBNet', 'EDSR', 'EDVRNet']
+__all__ = ['MSRResNet', 'RRDBNet', 'EDSR', 'EDVRNet', 'TOFlow']
