3307fdeec106812f31ef0df128c701bbb526a658
xurui4 <xurui4@sensetime.com>
2020-05-22 19:40:40 +0800

    add visual hook

--- mmedit/core/__init__.py
+++ mmedit/core/__init__.py
@@ -1,5 +1,6 @@
 from .evaluation import (DistEvalIterHook, EvalIterHook, mse, psnr,
                          reorder_image, sad, ssim)
+from .hooks import VisualizationHook
 from .misc import tensor2img
 from .optimizer import OPTIMIZERS, build_optimizer, build_optimizers
 from .test import multi_gpu_test, single_gpu_test
@@ -9,5 +10,5 @@ __all__ = [
     'OPTIMIZERS', 'build_optimizer', 'train_model', 'set_random_seed',
     'build_optimizers', 'tensor2img', 'EvalIterHook', 'DistEvalIterHook',
     'multi_gpu_test', 'single_gpu_test', 'mse', 'psnr', 'reorder_image', 'sad',
-    'ssim'
+    'ssim', 'VisualizationHook'
 ]
