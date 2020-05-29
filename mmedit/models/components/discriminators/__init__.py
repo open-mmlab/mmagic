6afd8dc8fa2d24683c4f89348b93b1364794c1e0
xurui4 <xurui4@sensetime.com>
2020-05-29 14:52:43 +0800

    add tmad patch disc

--- mmedit/models/components/discriminators/__init__.py
+++ mmedit/models/components/discriminators/__init__.py
@@ -2,8 +2,9 @@ from .deepfill_disc import DeepFillv1Discriminators
 from .gl_disc import GLDiscs
 from .modified_vgg import ModifiedVGG
 from .multi_layer_disc import MultiLayerDiscriminator
+from .tmad_patch_disc import TMADPatchDiscriminator
 
 __all__ = [
     'GLDiscs', 'ModifiedVGG', 'MultiLayerDiscriminator',
-    'DeepFillv1Discriminators'
+    'DeepFillv1Discriminators', 'TMADPatchDiscriminator'
 ]
