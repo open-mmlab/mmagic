809ddaaafef7c38508e17c331ffe34de466d8980
wangxintao <wangxintao@sensetime.com>
2020-06-03 11:55:39 +0800

    add flops calculation

--- mmedit/utils/__init__.py
+++ mmedit/utils/__init__.py
@@ -1,3 +1,4 @@
+from .flops_counter import get_model_complexity_info
 from .logger import get_root_logger
 
-__all__ = ['get_root_logger']
+__all__ = ['get_root_logger', 'get_model_complexity_info']
