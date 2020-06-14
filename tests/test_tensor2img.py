d1b0494fbffa006a7416b6aead31c4e5d80cd42b
hejiamin <hejiamin@sensetime.com>
2020-06-14 23:00:30 +0800

    fix: tensor2img when input with shape (1, 1, h, w)

--- tests/test_tensor2img.py
+++ tests/test_tensor2img.py
@@ -6,9 +6,13 @@ from torchvision.utils import make_grid
 
 
 def test_tensor2img():
-    tensor_4d = torch.FloatTensor(2, 3, 4, 4).uniform_(0, 1)
+    tensor_4d_1 = torch.FloatTensor(2, 3, 4, 4).uniform_(0, 1)
+    tensor_4d_2 = torch.FloatTensor(1, 3, 4, 4).uniform_(0, 1)
+    tensor_4d_3 = torch.FloatTensor(3, 1, 4, 4).uniform_(0, 1)
+    tensor_4d_4 = torch.FloatTensor(1, 1, 4, 4).uniform_(0, 1)
     tensor_3d_1 = torch.FloatTensor(3, 4, 4).uniform_(0, 1)
     tensor_3d_2 = torch.FloatTensor(3, 6, 6).uniform_(0, 1)
+    tensor_3d_3 = torch.FloatTensor(1, 6, 6).uniform_(0, 1)
     tensor_2d = torch.FloatTensor(4, 4).uniform_(0, 1)
 
     with pytest.raises(TypeError):
@@ -22,10 +26,24 @@ def test_tensor2img():
         tensor2img(torch.FloatTensor(2, 2, 3, 4, 4).uniform_(0, 1))
 
     # 4d
-    rlt = tensor2img(tensor_4d, out_type=np.uint8, min_max=(0, 1))
-    tensor_4d_np = make_grid(tensor_4d, nrow=1, normalize=False).numpy()
-    tensor_4d_np = np.transpose(tensor_4d_np[[2, 1, 0], :, :], (1, 2, 0))
-    np.testing.assert_almost_equal(rlt, (tensor_4d_np * 255).round())
+    rlt = tensor2img(tensor_4d_1, out_type=np.uint8, min_max=(0, 1))
+    tensor_4d_1_np = make_grid(tensor_4d_1, nrow=1, normalize=False).numpy()
+    tensor_4d_1_np = np.transpose(tensor_4d_1_np[[2, 1, 0], :, :], (1, 2, 0))
+    np.testing.assert_almost_equal(rlt, (tensor_4d_1_np * 255).round())
+
+    rlt = tensor2img(tensor_4d_2, out_type=np.uint8, min_max=(0, 1))
+    tensor_4d_2_np = tensor_4d_2.squeeze().numpy()
+    tensor_4d_2_np = np.transpose(tensor_4d_2_np[[2, 1, 0], :, :], (1, 2, 0))
+    np.testing.assert_almost_equal(rlt, (tensor_4d_2_np * 255).round())
+
+    rlt = tensor2img(tensor_4d_3, out_type=np.uint8, min_max=(0, 1))
+    tensor_4d_3_np = make_grid(tensor_4d_3, nrow=1, normalize=False).numpy()
+    tensor_4d_3_np = np.transpose(tensor_4d_3_np[[2, 1, 0], :, :], (1, 2, 0))
+    np.testing.assert_almost_equal(rlt, (tensor_4d_3_np * 255).round())
+
+    rlt = tensor2img(tensor_4d_4, out_type=np.uint8, min_max=(0, 1))
+    tensor_4d_4_np = tensor_4d_4.squeeze().numpy()
+    np.testing.assert_almost_equal(rlt, (tensor_4d_4_np * 255).round())
 
     # 3d
     rlt = tensor2img([tensor_3d_1, tensor_3d_2],
@@ -37,6 +55,10 @@ def test_tensor2img():
     tensor_3d_2_np = np.transpose(tensor_3d_2_np[[2, 1, 0], :, :], (1, 2, 0))
     np.testing.assert_almost_equal(rlt[0], (tensor_3d_1_np * 255).round())
     np.testing.assert_almost_equal(rlt[1], (tensor_3d_2_np * 255).round())
+
+    rlt = tensor2img(tensor_3d_3, out_type=np.uint8, min_max=(0, 1))
+    tensor_3d_3_np = tensor_3d_3.squeeze().numpy()
+    np.testing.assert_almost_equal(rlt, (tensor_3d_3_np * 255).round())
 
     # 2d
     rlt = tensor2img(tensor_2d, out_type=np.uint8, min_max=(0, 1))
