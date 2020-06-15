ab609fedf6c5775d36725ae5d068a8b6eac00631
wangxintao <wangxintao@sensetime.com>
2020-06-15 19:23:29 +0800

    add inference for restoration

--- mmedit/apis/__init__.py
+++ mmedit/apis/__init__.py
@@ -1,4 +1,8 @@
 from .inpainting_inference import inpainting_inference
 from .matting_inference import init_model, matting_inference
+from .restoration_inference import restoration_inference
 
-__all__ = ['init_model', 'matting_inference', 'inpainting_inference']
+__all__ = [
+    'init_model', 'matting_inference', 'inpainting_inference',
+    'restoration_inference'
+]
