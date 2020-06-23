fb1d698bcbde0cfa1e796b214f2b14c8d8524970
xurui4 <xurui4@sensetime.com>
2020-06-23 16:07:46 +0800

    rmove tmad model

--- mmedit/models/backbones/__init__.py
+++ mmedit/models/backbones/__init__.py
@@ -6,12 +6,10 @@ from .encoder_decoders import (VGG16, BGMattingDecoder, BGMattingEncoder,
                                GLEncoder, GLEncoderDecoder, HolisticIndexBlock,
                                IndexedUpsample, IndexNetDecoder,
                                IndexNetEncoder, PConvDecoder, PConvEncoder,
-                               PConvEncoderDecoder, PGDownsampleBlock,
-                               PGUpsampleBlock, PlainDecoder, ResGCADecoder,
-                               ResGCAEncoder, ResidualDilationBlock, ResNetDec,
+                               PConvEncoderDecoder, PlainDecoder,
+                               ResGCADecoder, ResGCAEncoder, ResNetDec,
                                ResNetEnc, ResShortcutDec, ResShortcutEnc,
-                               SimpleEncoderDecoder, TMADDecoder,
-                               TMADDilationNeck, TMADEncoder)
+                               SimpleEncoderDecoder)
 # yapf: enable
 from .generation_backbones import ResnetGenerator, UnetGenerator
 from .sr_backbones import EDSR, SRCNN, EDVRNet, MSRResNet, RRDBNet, TOFlow
@@ -24,8 +22,7 @@ __all__ = [
     'DeepFillEncoder', 'HolisticIndexBlock', 'DepthwiseIndexBlock',
     'ContextualAttentionNeck', 'DeepFillDecoder', 'EDSR',
     'DeepFillEncoderDecoder', 'EDVRNet', 'IndexedUpsample', 'IndexNetEncoder',
-    'IndexNetDecoder', 'ResidualDilationBlock', 'TMADDilationNeck',
-    'BGMattingEncoder', 'TMADEncoder', 'PGDownsampleBlock', 'PGUpsampleBlock',
-    'TMADDecoder', 'BGMattingDecoder', 'TOFlow', 'ResGCAEncoder',
-    'ResGCADecoder', 'SRCNN', 'UnetGenerator', 'ResnetGenerator'
+    'IndexNetDecoder', 'BGMattingEncoder', 'BGMattingDecoder', 'TOFlow',
+    'ResGCAEncoder', 'ResGCADecoder', 'SRCNN', 'UnetGenerator',
+    'ResnetGenerator'
 ]
--- mmedit/models/backbones/encoder_decoders/necks/__init__.py
+++ mmedit/models/backbones/encoder_decoders/necks/__init__.py
@@ -1,8 +1,4 @@
 from .contextual_attention_neck import ContextualAttentionNeck
 from .gl_dilation import GLDilationNeck
-from .tmad_dilation import ResidualDilationBlock, TMADDilationNeck
 
-__all__ = [
-    'GLDilationNeck', 'ContextualAttentionNeck', 'ResidualDilationBlock',
-    'TMADDilationNeck'
-]
+__all__ = ['GLDilationNeck', 'ContextualAttentionNeck']
