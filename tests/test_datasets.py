221a862791f66e6ca94d37463c5fc61665133e39
hejiamin <hejm37@mail2.sysu.edu.cn>
2020-07-11 02:02:01 +0800

    test: revert to pathlib

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -570,8 +570,7 @@ def test_repeat_dataset():
 
 
 def test_reds_dataset():
-    repo_dpath = osp.dirname(osp.dirname(__file__))
-    root_path = Path(osp.join(repo_dpath, 'data'))
+    root_path = Path(__file__).parent / 'data'
 
     txt_content = ('000/00000001.png (720, 1280, 3)\n'
                    '001/00000001.png (720, 1280, 3)\n'
@@ -697,8 +696,7 @@ def test_reds_dataset():
 
 
 def test_vimeo90k_dataset():
-    repo_dpath = osp.dirname(osp.dirname(__file__))
-    root_path = Path(osp.join(repo_dpath, 'data'))
+    root_path = Path(__file__).parent / 'data'
 
     txt_content = ('00001/0266 (256, 448, 3)\n00002/0268 (256, 448, 3)\n')
     mocked_open_function = mock_open(read_data=txt_content)
@@ -738,8 +736,7 @@ def test_vimeo90k_dataset():
 
 
 def test_vid4_dataset():
-    repo_dpath = osp.dirname(osp.dirname(__file__))
-    root_path = Path(osp.join(repo_dpath, 'data'))
+    root_path = Path(__file__).parent / 'data'
 
     txt_content = ('calendar 1 (320,480,3)\ncity 2 (320,480,3)\n')
     mocked_open_function = mock_open(read_data=txt_content)
