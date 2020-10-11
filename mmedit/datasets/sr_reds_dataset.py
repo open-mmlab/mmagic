74ce54f34c8c81bc4e7c311d139e2a0c4309fc71
wwhio <wangwenhao@live.com>
2020-10-11 09:01:03 +0800

    fix item in key
    from `000/00000000.png (720, 1280, 3)` to key[0] = `000/00000000.png`

--- mmedit/datasets/sr_reds_dataset.py
+++ mmedit/datasets/sr_reds_dataset.py
@@ -83,7 +83,7 @@ class SRREDSDataset(BaseSRDataset):
                 dict(
                     lq_path=self.lq_folder,
                     gt_path=self.gt_folder,
-                    key=key,
+                    key=key.split('(')[0].strip(),
                     max_frame_num=100,  # REDS has 100 frames for each clip
                     num_input_frames=self.num_input_frames))
 
