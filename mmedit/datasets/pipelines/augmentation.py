dc442e280413416231dff03d50b1a5343241deee
lizz <lizz@sensetime.com>
2021-02-26 15:05:39 +0800

    Fix randint range
    Signed-off-by: lizz <lizz@sensetime.com>

--- mmedit/datasets/pipelines/augmentation.py
+++ mmedit/datasets/pipelines/augmentation.py
@@ -610,7 +610,7 @@ class RandomMaskDilation:
         self.binary_thr = binary_thr
 
     def _random_dilate(self, img):
-        kernel_size = np.random.randint(self.kernel_min, self.kernel_max)
+        kernel_size = np.random.randint(self.kernel_min, self.kernel_max + 1)
         kernel = np.ones((kernel_size, kernel_size), dtype=np.uint8)
         dilate_kernel_size = kernel_size
         img_ = cv2.dilate(img, kernel, iterations=1)
