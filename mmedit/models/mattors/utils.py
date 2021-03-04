a845d725b259a123baf145fd0900afc22376f123
yaochaorui <yaochaorui@sensetime.com>
2021-03-04 16:12:36 +0800

    Added utis. May change.

--- mmedit/models/mattors/utils.py
+++ mmedit/models/mattors/utils.py
@@ -1,3 +1,10 @@
+import torch
+
+# For RGB !
+group_norm_std = [0.229, 0.224, 0.225]
+group_norm_mean = [0.485, 0.456, 0.406]
+
+
 def get_unknown_tensor(trimap, meta):
     """Get 1-channel unknown area tensor from the 3 or 1-channel trimap tensor.
 
@@ -20,3 +27,50 @@ def get_unknown_tensor(trimap, meta):
         # 0 for bg, 128/255 for unknown, 1 for fg
         weight = trimap.eq(128 / 255).float()
     return weight
+
+
+def fba_fusion(alpha, img, F, B):
+    """Postprocess the predicted.
+
+    This class is adopted from
+    https://github.com/MarcoForte/FBA_Matting.
+
+    Args:
+        alpha (Tensor): Tensor with shape (N, 1, H, W).
+        img (Tensor): Tensor with shape (N, 3, H, W).
+        F (Tensor): Tensor with shape (N, 3, H, W).
+        B (Tensor): Tensor with shape (N, 3, H, W).
+
+    Returns:
+        alpha (Tensor): Tensor with shape (N, 1, H, W).
+        F (Tensor): Tensor with shape (N, 3, H, W).
+        B (Tensor): Tensor with shape (N, 3, H, W).
+    """
+    F = ((alpha * img + (1 - alpha**2) * F - alpha * (1 - alpha) * B))
+    B = ((1 - alpha) * img + (2 * alpha - alpha**2) * B - alpha *
+         (1 - alpha) * F)
+
+    F = torch.clamp(F, 0, 1)
+    B = torch.clamp(B, 0, 1)
+    la = 0.1
+    alpha = (alpha * la + torch.sum((img - B) * (F - B), 1, keepdim=True)) / (
+        torch.sum((F - B) * (F - B), 1, keepdim=True) + la)
+    alpha = torch.clamp(alpha, 0, 1)
+    return alpha, F, B
+
+
+def groupnorm_normalise_image(img, format='nhwc'):
+    """with format: batch, height, width, channel.
+
+    Accept rgb in range 0,1
+    """
+    if (format == 'nhwc'):
+        for i in range(3):
+            img[...,
+                i] = (img[..., i] - group_norm_mean[i]) / group_norm_std[i]
+    else:
+        for i in range(3):
+            img[..., i, :, :] = (img[..., i, :, :] -
+                                 group_norm_mean[i]) / group_norm_std[i]
+
+    return img
