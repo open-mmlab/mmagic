1c397ce32f14cebc640ea4913f6d93f4e74e9882
李尹硕 <SENSETIME\liyinshuo@cn0014004493l.domain.sensetime.com>
2021-03-13 21:46:21 +0800

    Add docstring, rename parameter and change the function of resize.

--- mmedit/datasets/pipelines/down_sampling.py
+++ mmedit/datasets/pipelines/down_sampling.py
@@ -3,8 +3,7 @@ import random
 
 import numpy as np
 import torch
-from PIL import Image
-from torchvision import transforms
+from mmcv import imresize
 
 from ..registry import PIPELINES
 
@@ -14,38 +13,44 @@ class DownSampling:
     """Generate LQ image from GT (and crop).
 
     Args:
-        scale_min (int): The minimum of upsampling scale. Default: 1.
-        scale_max (int): The maximum of upsampling scale. Default: 4.
-        inp_size (int): The input size, i.e. cropped lr patch size.
+        scale_min (int): The minimum of upsampling scale, inclusive.
+            Default: 1.
+        scale_max (int): The maximum of upsampling scale, exclusive.
+            Default: 4.
+        patch_size (int): The cropped lr patch size.
             Default: None, means no crop.
+
+        Scale will be in the range of [scale_min, scale_max).
     """
 
-    def __init__(self, scale_min=1, scale_max=4, inp_size=None):
+    def __init__(self, scale_min=1, scale_max=4, patch_size=None):
         assert scale_max >= scale_min
         self.scale_min = scale_min
         self.scale_max = scale_max
-        self.inp_size = inp_size
+        self.patch_size = patch_size
 
     def __call__(self, results):
         """Call function.
 
         Args:
             results (dict): A dict containing the necessary information and
-                data for augmentation.
+                data for augmentation. 'gt' is required.
 
         Returns:
             dict: A dict containing the processed data and information.
+                modified 'gt', supplement 'lq' and 'scale' to keys.
         """
         img = results['gt']
         scale = random.uniform(self.scale_min, self.scale_max)
-        if self.inp_size is None:
+
+        if self.patch_size is None:
             h_lr = math.floor(img.shape[-3] / scale + 1e-9)
             w_lr = math.floor(img.shape[-2] / scale + 1e-9)
             img = img[:round(h_lr * scale), :round(w_lr * scale), :]
             img_down = resize_fn(img, (w_lr, h_lr))
             crop_lr, crop_hr = img_down, img
         else:
-            w_lr = self.inp_size
+            w_lr = self.patch_size
             w_hr = round(w_lr * scale)
             x0 = random.randint(0, img.shape[-3] - w_hr)
             y0 = random.randint(0, img.shape[-2] - w_hr)
@@ -61,7 +66,7 @@ class DownSampling:
         repr_str = self.__class__.__name__
         repr_str += (f'scale_min={self.scale_min}, '
                      f'scale_max={self.scale_max}, '
-                     f'inp_size={self.inp_size}')
+                     f'patch_size={self.patch_size}')
 
         return repr_str
 
@@ -70,11 +75,11 @@ def resize_fn(img, size):
     if isinstance(size, int):
         size = (size, size)
     if isinstance(img, np.ndarray):
-        return np.asarray(Image.fromarray(img).resize(size, Image.BICUBIC))
+        return imresize(img, size, interpolation='bicubic', backend='pillow')
     elif isinstance(img, torch.Tensor):
-        return transforms.ToTensor()(
-            transforms.Resize(size,
-                              Image.BICUBIC)(transforms.ToPILImage()(img)))
+        image = imresize(
+            img.numpy(), size, interpolation='bicubic', backend='pillow')
+        return torch.from_numpy(image)
 
     else:
         raise TypeError('img should got np.ndarray or torch.Tensor,'
--- tests/test_down_sampling.py
+++ tests/test_down_sampling.py
@@ -4,23 +4,24 @@ from mmedit.datasets.pipelines import DownSampling
 
 
 def test_down_sampling():
-    img = np.uint8(np.random.randn(480, 640, 3) * 255)
-    inputs = dict(gt=img)
-
-    down_sampling1 = DownSampling(scale_min=1, scale_max=4, inp_size=None)
-    results1 = down_sampling1(inputs)
+    img1 = np.uint8(np.random.randn(480, 640, 3) * 255)
+    inputs1 = dict(gt=img1)
+    down_sampling1 = DownSampling(scale_min=1, scale_max=4, patch_size=None)
+    results1 = down_sampling1(inputs1)
     assert set(list(results1.keys())) == set(['gt', 'lq', 'scale'])
     assert repr(down_sampling1) == (
         down_sampling1.__class__.__name__ +
         f'scale_min={down_sampling1.scale_min}, ' +
         f'scale_max={down_sampling1.scale_max}, ' +
-        f'inp_size={down_sampling1.inp_size}')
+        f'patch_size={down_sampling1.patch_size}')
 
-    down_sampling2 = DownSampling(scale_min=1, scale_max=4, inp_size=48)
-    results2 = down_sampling2(inputs)
+    img2 = np.uint8(np.random.randn(480, 640, 3) * 255)
+    inputs2 = dict(gt=img2)
+    down_sampling2 = DownSampling(scale_min=1, scale_max=4, patch_size=48)
+    results2 = down_sampling2(inputs2)
     assert set(list(results2.keys())) == set(['gt', 'lq', 'scale'])
     assert repr(down_sampling2) == (
         down_sampling2.__class__.__name__ +
         f'scale_min={down_sampling2.scale_min}, ' +
         f'scale_max={down_sampling2.scale_max}, ' +
-        f'inp_size={down_sampling2.inp_size}')
+        f'patch_size={down_sampling2.patch_size}')
