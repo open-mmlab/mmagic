ad517d4a0ece924e396a06ab348c8793a0af10da
ys-li <56712176+Yshuo-Li@users.noreply.github.com>
2021-03-15 14:12:38 +0800

    Add down_sampling.py for generating LQ image from GT image. (#222)
    * Add down_sampling.py for generating LQ image from GT image, which is required in LIIF.
    
    * Add '__repr__' and test_down_sampling.py.
    
    * Add docstring, rename parameter and change the function of resize.
    
    * Fine-tuning code and docstring of RandomDownSampling class.
    
    * Remove hardcode of bicubic and pillow.
    
    Co-authored-by: 李尹硕 <SENSETIME\liyinshuo@cn0014004493l.domain.sensetime.com>

--- mmedit/datasets/pipelines/__init__.py
+++ mmedit/datasets/pipelines/__init__.py
@@ -5,6 +5,7 @@ from .augmentation import (BinarizeImage, Flip, GenerateFrameIndices,
 from .compose import Compose
 from .crop import (Crop, CropAroundCenter, CropAroundFg, CropAroundUnknown,
                    FixedCrop, ModCrop, PairedRandomCrop)
+from .down_sampling import RandomDownSampling
 from .formating import (Collect, FormatTrimap, GetMaskedImage, ImageToTensor,
                         ToTensor)
 from .loading import (GetSpatialDiscountMask, LoadImageFromFile,
@@ -25,6 +26,6 @@ __all__ = [
     'MergeFgAndBg', 'CompositeFg', 'TemporalReverse', 'LoadImageFromFileList',
     'GenerateFrameIndices', 'GenerateFrameIndiceswithPadding', 'FixedCrop',
     'LoadPairedImageFromFile', 'GenerateSoftSeg', 'GenerateSeg', 'PerturbBg',
-    'CropAroundFg', 'GetSpatialDiscountMask',
+    'CropAroundFg', 'GetSpatialDiscountMask', 'RandomDownSampling',
     'GenerateTrimapWithDistTransform', 'TransformTrimap'
 ]
