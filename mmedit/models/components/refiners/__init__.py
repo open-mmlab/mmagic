f8b62a3f58990b53645967d623fcde209229fa07
李尹硕 <SENSETIME\liyinshuo@cn0014004493l.domain.sensetime.com>
2021-03-16 21:28:05 +0800

    Add model: multilayer perceptrons.

--- mmedit/models/components/refiners/__init__.py
+++ mmedit/models/components/refiners/__init__.py
@@ -1,4 +1,5 @@
 from .deepfill_refiner import DeepFillRefiner
+from .mlp import MLP
 from .plain_refiner import PlainRefiner
 
-__all__ = ['PlainRefiner', 'DeepFillRefiner']
+__all__ = ['PlainRefiner', 'DeepFillRefiner', 'MLP']
