f7793a920eda77195b85e35819f9ab0033400681
李尹硕 <SENSETIME\liyinshuo@cn0014004493l.domain.sensetime.com>
2021-03-16 20:39:20 +0800

    Add LIIF.

--- mmedit/datasets/pipelines/utils.py
+++ mmedit/datasets/pipelines/utils.py
@@ -1,6 +1,7 @@
 import logging
 
 import numpy as np
+import torch
 from mmcv.utils import print_log
 
 _integer_types = (
@@ -124,3 +125,29 @@ def random_choose_unknown(unknown, crop_size):
     left = np.clip(center_w - delta_w, 0, w - crop_w)
 
     return top, left
+
+
+def make_coord(shape, ranges=None, flatten=True):
+    """ Make coordinates at grid centers.
+
+    Args:
+        shape (tuple): shape of image.
+        ranges (tuple): range of coordinate value. Default: None.
+        flatten (bool): flatten to (n, 2) or Not. Default: True.
+
+    return:
+        coord (Tensor): coordinates.
+    """
+    coord_seqs = []
+    for i, n in enumerate(shape):
+        if ranges is None:
+            v0, v1 = -1, 1
+        else:
+            v0, v1 = ranges[i]
+        r = (v1 - v0) / (2 * n)
+        seq = v0 + r + (2 * r) * torch.arange(n).float()
+        coord_seqs.append(seq)
+    coord = torch.stack(torch.meshgrid(*coord_seqs), dim=-1)
+    if flatten:
+        coord = coord.view(-1, coord.shape[-1])
+    return coord
