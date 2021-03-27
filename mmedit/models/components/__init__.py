7d3c668bd2272224a1d50a3329279292fe9dba3d
nbei <nbeiray@gmail.com>
2021-03-27 20:18:24 +0800

    add stylegan2 with pretrained models

--- mmedit/models/components/__init__.py
+++ mmedit/models/components/__init__.py
@@ -1,8 +1,10 @@
 from .discriminators import (DeepFillv1Discriminators, GLDiscs, ModifiedVGG,
                              MultiLayerDiscriminator, PatchDiscriminator)
 from .refiners import DeepFillRefiner, PlainRefiner
+from .stylegan2 import StyleGAN2Discriminator, StyleGANv2Generator
 
 __all__ = [
     'PlainRefiner', 'GLDiscs', 'ModifiedVGG', 'MultiLayerDiscriminator',
-    'DeepFillv1Discriminators', 'DeepFillRefiner', 'PatchDiscriminator'
+    'DeepFillv1Discriminators', 'DeepFillRefiner', 'PatchDiscriminator',
+    'StyleGAN2Discriminator', 'StyleGANv2Generator'
 ]
