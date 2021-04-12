1cfce3a3a381224025695fb405a464870e0378dc
Kelvin C.K. Chan <kelvinckchan@outlook.com>
2021-04-12 15:13:23 +0800

    Support Y-channel PSNR and SSIM (#250)
    * Support Y-channel PSNR and SSIM
    
    * fix bug
    
    * update naming and unittest

--- mmedit/core/evaluation/metrics.py
+++ mmedit/core/evaluation/metrics.py
@@ -165,7 +165,7 @@ def reorder_image(img, input_order='HWC'):
     return img
 
 
-def psnr(img1, img2, crop_border=0, input_order='HWC'):
+def psnr(img1, img2, crop_border=0, input_order='HWC', convert_to=None):
     """Calculate PSNR (Peak Signal-to-Noise Ratio).
 
     Ref: https://en.wikipedia.org/wiki/Peak_signal-to-noise_ratio
@@ -177,6 +177,10 @@ def psnr(img1, img2, crop_border=0, input_order='HWC'):
             pixels are not involved in the PSNR calculation. Default: 0.
         input_order (str): Whether the input order is 'HWC' or 'CHW'.
             Default: 'HWC'.
+        convert_to (str): Whether to convert the images to other color models.
+            If None, the images are not altered. When computing for 'Y',
+            the images are assumed to be in BGR order. Options are 'Y' and
+            None. Default: None.
 
     Returns:
         float: psnr result.
@@ -190,6 +194,14 @@ def psnr(img1, img2, crop_border=0, input_order='HWC'):
             '"HWC" and "CHW"')
     img1 = reorder_image(img1, input_order=input_order)
     img2 = reorder_image(img2, input_order=input_order)
+
+    if isinstance(convert_to, str) and convert_to.lower() == 'y':
+        img1, img2 = img1.astype(np.float32), img2.astype(np.float32)
+        img1 = mmcv.bgr2ycbcr(img1 / 255., y_only=True) * 255.
+        img2 = mmcv.bgr2ycbcr(img2 / 255., y_only=True) * 255.
+    elif convert_to is not None:
+        raise ValueError(f'Wrong color model. Supported values are '
+                         '"Y" and None.')
 
     if crop_border != 0:
         img1 = img1[crop_border:-crop_border, crop_border:-crop_border, None]
@@ -236,7 +248,7 @@ def _ssim(img1, img2):
     return ssim_map.mean()
 
 
-def ssim(img1, img2, crop_border=0, input_order='HWC'):
+def ssim(img1, img2, crop_border=0, input_order='HWC', convert_to=None):
     """Calculate SSIM (structural similarity).
 
     Ref:
@@ -255,6 +267,10 @@ def ssim(img1, img2, crop_border=0, input_order='HWC'):
             pixels are not involved in the SSIM calculation. Default: 0.
         input_order (str): Whether the input order is 'HWC' or 'CHW'.
             Default: 'HWC'.
+        convert_to (str): Whether to convert the images to other color models.
+            If None, the images are not altered. When computing for 'Y',
+            the images are assumed to be in BGR order. Options are 'Y' and
+            None. Default: None.
 
     Returns:
         float: ssim result.
@@ -268,6 +284,16 @@ def ssim(img1, img2, crop_border=0, input_order='HWC'):
             '"HWC" and "CHW"')
     img1 = reorder_image(img1, input_order=input_order)
     img2 = reorder_image(img2, input_order=input_order)
+
+    if isinstance(convert_to, str) and convert_to.lower() == 'y':
+        img1, img2 = img1.astype(np.float32), img2.astype(np.float32)
+        img1 = mmcv.bgr2ycbcr(img1 / 255., y_only=True) * 255.
+        img2 = mmcv.bgr2ycbcr(img2 / 255., y_only=True) * 255.
+        img1 = np.expand_dims(img1, axis=2)
+        img2 = np.expand_dims(img2, axis=2)
+    elif convert_to is not None:
+        raise ValueError(f'Wrong color model. Supported values are '
+                         '"Y" and None')
 
     if crop_border != 0:
         img1 = img1[crop_border:-crop_border, crop_border:-crop_border, None]
