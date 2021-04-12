67ad672735f0b27512cba14e614eb049b38981d1
ckkelvinchan <kelvinckchan@outlook.com>
2021-04-12 11:12:51 +0800

    Add IconVSR architecture

--- mmedit/models/backbones/sr_backbones/__init__.py
+++ mmedit/models/backbones/sr_backbones/__init__.py
@@ -1,11 +1,13 @@
 from .basicvsr_net import BasicVSRNet
 from .edsr import EDSR
 from .edvr_net import EDVRNet
+from .iconvsr import IconVSR
 from .rrdb_net import RRDBNet
 from .sr_resnet import MSRResNet
 from .srcnn import SRCNN
 from .tof import TOFlow
 
 __all__ = [
-    'MSRResNet', 'RRDBNet', 'EDSR', 'EDVRNet', 'TOFlow', 'SRCNN', 'BasicVSRNet'
+    'MSRResNet', 'RRDBNet', 'EDSR', 'EDVRNet', 'TOFlow', 'SRCNN',
+    'BasicVSRNet', 'IconVSR'
 ]
