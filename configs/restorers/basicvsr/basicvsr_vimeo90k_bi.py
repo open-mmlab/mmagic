05d6f082e6dabd99ba43e3490b5bac76c6fde8bf
ckkelvinchan <kelvinckchan@outlook.com>
2021-04-14 11:21:54 +0800

    fix bug

--- configs/restorers/basicvsr/basicvsr_vimeo90k_bi.py
+++ configs/restorers/basicvsr/basicvsr_vimeo90k_bi.py
@@ -36,7 +36,7 @@ train_pipeline = [
         direction='horizontal'),
     dict(type='Flip', keys=['lq', 'gt'], flip_ratio=0.5, direction='vertical'),
     dict(type='RandomTransposeHW', keys=['lq', 'gt'], transpose_ratio=0.5),
-    dict(type='MirrorExtend', keys=['lq', 'gt']),
+    dict(type='MirrorSequence', keys=['lq', 'gt']),
     dict(type='FramesToTensor', keys=['lq', 'gt']),
     dict(type='Collect', keys=['lq', 'gt'], meta_keys=['lq_path', 'gt_path'])
 ]
@@ -73,7 +73,7 @@ test_pipeline = [
         key='gt',
         channel_order='rgb'),
     dict(type='RescaleToZeroOne', keys=['lq', 'gt']),
-    dict(type='MirrorExtend', keys=['lq']),
+    dict(type='MirrorSequence', keys=['lq']),
     dict(type='FramesToTensor', keys=['lq', 'gt']),
     dict(
         type='Collect',
