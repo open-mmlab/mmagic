a784deb93cf06ff05e5f1b9d0a79d6f44903c4bf
liyinshuo <liyinshuo@sensetime.com>
2021-04-22 15:55:14 +0800

    Tiny Fix

--- mmedit/models/backbones/sr_backbones/__init__.py
+++ mmedit/models/backbones/sr_backbones/__init__.py
@@ -1,8 +1,8 @@
 from .basicvsr_net import BasicVSRNet
 from .edsr import EDSR
 from .edvr_net import EDVRNet
-from .rdn import RDN
 from .iconvsr import IconVSR
+from .rdn import RDN
 from .rrdb_net import RRDBNet
 from .sr_resnet import MSRResNet
 from .srcnn import SRCNN
@@ -10,5 +10,5 @@ from .tof import TOFlow
 
 __all__ = [
     'MSRResNet', 'RRDBNet', 'EDSR', 'EDVRNet', 'TOFlow', 'SRCNN',
-    'BasicVSRNet', 'RDN', 'IconVSR'
+    'BasicVSRNet', 'IconVSR', 'RDN'
 ]
