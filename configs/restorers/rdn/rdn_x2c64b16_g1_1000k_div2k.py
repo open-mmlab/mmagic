72544c1f70e66ba2783ee6250010805a233ef5b1
ys-li <56712176+Yshuo-Li@users.noreply.github.com>
2021-04-27 10:23:05 +0800

    Config files of RDN (#260)
    * Add config files of RDN.
    
    * Tiny Fix
    
    Co-authored-by: liyinshuo <liyinshuo@sensetime.com>

---
+++ configs/restorers/rdn/rdn_x2c64b16_g1_1000k_div2k.py
@@ -0,0 +1,116 @@
+exp_name = 'rdn_x2c64b16_g1_1000k_div2k'
+
+scale = 2
+# model settings
+model = dict(
+    type='BasicRestorer',
+    generator=dict(
+        type='RDN',
+        in_channels=3,
+        out_channels=3,
+        mid_channels=64,
+        num_blocks=16,
+        upscale_factor=scale),
+    pixel_loss=dict(type='L1Loss', loss_weight=1.0, reduction='mean'))
+# model training and testing settings
+train_cfg = None
+test_cfg = dict(metrics=['PSNR', 'SSIM'], crop_border=scale)
+
+# dataset settings
+train_dataset_type = 'SRAnnotationDataset'
+val_dataset_type = 'SRFolderDataset'
+train_pipeline = [
+    dict(
+        type='LoadImageFromFile',
+        io_backend='disk',
+        key='lq',
+        flag='color',
+        channel_order='rgb'),
+    dict(
+        type='LoadImageFromFile',
+        io_backend='disk',
+        key='gt',
+        flag='color',
+        channel_order='rgb'),
+    dict(type='RescaleToZeroOne', keys=['lq', 'gt']),
+    dict(type='PairedRandomCrop', gt_patch_size=64),
+    dict(
+        type='Flip', keys=['lq', 'gt'], flip_ratio=0.5,
+        direction='horizontal'),
+    dict(type='Flip', keys=['lq', 'gt'], flip_ratio=0.5, direction='vertical'),
+    dict(type='RandomTransposeHW', keys=['lq', 'gt'], transpose_ratio=0.5),
+    dict(type='Collect', keys=['lq', 'gt'], meta_keys=['lq_path', 'gt_path']),
+    dict(type='ImageToTensor', keys=['lq', 'gt'])
+]
+test_pipeline = [
+    dict(
+        type='LoadImageFromFile',
+        io_backend='disk',
+        key='lq',
+        flag='color',
+        channel_order='rgb'),
+    dict(
+        type='LoadImageFromFile',
+        io_backend='disk',
+        key='gt',
+        flag='color',
+        channel_order='rgb'),
+    dict(type='RescaleToZeroOne', keys=['lq', 'gt']),
+    dict(type='Collect', keys=['lq', 'gt'], meta_keys=['lq_path', 'gt_path']),
+    dict(type='ImageToTensor', keys=['lq', 'gt'])
+]
+
+data = dict(
+    workers_per_gpu=1,
+    train_dataloader=dict(samples_per_gpu=16, drop_last=True),
+    val_dataloader=dict(samples_per_gpu=1),
+    test_dataloader=dict(samples_per_gpu=1),
+    train=dict(
+        type='RepeatDataset',
+        times=1000,
+        dataset=dict(
+            type=train_dataset_type,
+            lq_folder='data/DIV2K/DIV2K_train_LR_bicubic/X2_sub',
+            gt_folder='data/DIV2K/DIV2K_train_HR_sub',
+            ann_file='data/DIV2K/meta_info_DIV2K800sub_GT.txt',
+            pipeline=train_pipeline,
+            scale=scale)),
+    val=dict(
+        type=val_dataset_type,
+        lq_folder='data/val_set5/Set5_bicLRx2',
+        gt_folder='data/val_set5/Set5_mod12',
+        pipeline=test_pipeline,
+        scale=scale,
+        filename_tmpl='{}'),
+    test=dict(
+        type=val_dataset_type,
+        lq_folder='data/val_set5/Set5_bicLRx2',
+        gt_folder='data/val_set5/Set5_mod12',
+        pipeline=test_pipeline,
+        scale=scale,
+        filename_tmpl='{}'))
+
+# optimizer
+optimizers = dict(generator=dict(type='Adam', lr=1e-4, betas=(0.9, 0.999)))
+
+# learning policy
+total_iters = 1000000
+lr_config = dict(
+    policy='Step',
+    by_epoch=False,
+    step=[200000, 400000, 600000, 800000],
+    gamma=0.5)
+
+checkpoint_config = dict(interval=5000, save_optimizer=True, by_epoch=False)
+evaluation = dict(interval=5000, save_image=True, gpu_collect=True)
+log_config = dict(
+    interval=100, hooks=[dict(type='TextLoggerHook', by_epoch=False)])
+visual_config = None
+
+# runtime settings
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+work_dir = f'./work_dirs/{exp_name}'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
