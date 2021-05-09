604c102dddc2b12ccd415df0f255d8f788e6f269
ckkelvinchan <kelvinckchan@outlook.com>
2021-05-09 20:52:17 +0800

    remove redundancy in test_glean.py

--- tests/test_glean.py
+++ tests/test_glean.py
@@ -1,13 +1,8 @@
-from unittest.mock import patch
-
+import mmcv
 import pytest
 import torch
-from mmcv.runner import obj_from_dict
 
 from mmedit.models import build_model
-from mmedit.models.backbones import GLEANStyleGANv2
-from mmedit.models.components import StyleGAN2Discriminator
-from mmedit.models.losses import GANLoss, L1Loss
 
 
 def test_glean():
@@ -17,9 +12,9 @@ def test_glean():
         generator=dict(
             type='GLEANStyleGANv2',
             in_size=16,
-            out_size=128,
+            out_size=64,
             style_channels=512),
-        discriminator=dict(type='StyleGAN2Discriminator', in_size=128),
+        discriminator=dict(type='StyleGAN2Discriminator', in_size=64),
         pixel_loss=dict(type='L1Loss', loss_weight=1.0, reduction='mean'),
         gan_loss=dict(
             type='GANLoss',
@@ -29,209 +24,46 @@ def test_glean():
             loss_weight=5e-3))
 
     train_cfg = None
-    test_cfg = None
+    test_cfg = mmcv.Config(dict(metrics=['PSNR'], crop_border=0))
 
     # build restorer
     restorer = build_model(model_cfg, train_cfg=train_cfg, test_cfg=test_cfg)
 
-    # test attributes
-    assert restorer.__class__.__name__ == 'GLEAN'
-    assert isinstance(restorer.generator, GLEANStyleGANv2)
-    assert isinstance(restorer.discriminator, StyleGAN2Discriminator)
-    assert isinstance(restorer.pixel_loss, L1Loss)
-    assert isinstance(restorer.gan_loss, GANLoss)
-
     # prepare data
     inputs = torch.rand(1, 3, 16, 16)
-    targets = torch.rand(1, 3, 128, 128)
+    targets = torch.rand(1, 3, 64, 64)
     data_batch = {'lq': inputs, 'gt': targets}
 
-    # prepare optimizer
-    optim_cfg = dict(type='Adam', lr=2e-4, betas=(0.9, 0.999))
-    optimizer = {
-        'generator':
-        obj_from_dict(
-            optim_cfg, torch.optim,
-            dict(params=getattr(restorer, 'generator').parameters())),
-        'discriminator':
-        obj_from_dict(
-            optim_cfg, torch.optim,
-            dict(params=getattr(restorer, 'discriminator').parameters()))
-    }
-
-    # no forward train in GAN models, raise ValueError
-    with pytest.raises(ValueError):
-        restorer(**data_batch, test_mode=False)
-
-    # test forward_test
-    with torch.no_grad():
-        outputs = restorer(**data_batch, test_mode=True)
-    assert torch.equal(outputs['lq'], data_batch['lq'])
-    assert torch.is_tensor(outputs['output'])
-    assert outputs['output'].size() == (1, 3, 128, 128)
-
-    # test forward_dummy
-    with torch.no_grad():
-        output = restorer.forward_dummy(data_batch['lq'])
-    assert torch.is_tensor(output)
-    assert output.size() == (1, 3, 128, 128)
-
-    # val_step
-    with torch.no_grad():
-        outputs = restorer.val_step(data_batch)
-    assert torch.equal(outputs['lq'], data_batch['lq'])
-    assert torch.is_tensor(outputs['output'])
-    assert outputs['output'].size() == (1, 3, 128, 128)
-
-    # test train_step
-    with patch.object(
-            restorer,
-            'perceptual_loss',
-            return_value=(torch.tensor(1.0), torch.tensor(2.0))):
-        outputs = restorer.train_step(data_batch, optimizer)
-        assert isinstance(outputs, dict)
-        assert isinstance(outputs['log_vars'], dict)
-        for v in [
-                'loss_perceptual', 'loss_gan', 'loss_d_real', 'loss_d_fake',
-                'loss_pix'
-        ]:
-            assert isinstance(outputs['log_vars'][v], float)
-        assert outputs['num_samples'] == 1
-        assert torch.equal(outputs['results']['lq'], data_batch['lq'])
-        assert torch.equal(outputs['results']['gt'], data_batch['gt'])
-        assert torch.is_tensor(outputs['results']['output'])
-        assert outputs['results']['output'].size() == (1, 3, 128, 128)
-
-    # test train_step and forward_test (gpu)
-    if torch.cuda.is_available():
-        restorer = restorer.cuda()
-        optimizer = {
-            'generator':
-            obj_from_dict(
-                optim_cfg, torch.optim,
-                dict(params=getattr(restorer, 'generator').parameters())),
-            'discriminator':
-            obj_from_dict(
-                optim_cfg, torch.optim,
-                dict(params=getattr(restorer, 'discriminator').parameters()))
-        }
-        data_batch = {'lq': inputs.cuda(), 'gt': targets.cuda()}
-
-        # forward_test
-        with torch.no_grad():
-            outputs = restorer(**data_batch, test_mode=True)
-        assert torch.equal(outputs['lq'], data_batch['lq'].cpu())
-        assert torch.is_tensor(outputs['output'])
-        assert outputs['output'].size() == (1, 3, 128, 128)
-
-        # val_step
-        with torch.no_grad():
-            outputs = restorer.val_step(data_batch)
-        assert torch.equal(outputs['lq'], data_batch['lq'].cpu())
-        assert torch.is_tensor(outputs['output'])
-        assert outputs['output'].size() == (1, 3, 128, 128)
-
-        # train_step
-        with patch.object(
-                restorer,
-                'perceptual_loss',
-                return_value=(torch.tensor(1.0).cuda(),
-                              torch.tensor(2.0).cuda())):
-            outputs = restorer.train_step(data_batch, optimizer)
-            assert isinstance(outputs, dict)
-            assert isinstance(outputs['log_vars'], dict)
-            for v in [
-                    'loss_perceptual', 'loss_gan', 'loss_d_real',
-                    'loss_d_fake', 'loss_pix'
-            ]:
-                assert isinstance(outputs['log_vars'][v], float)
-            assert outputs['num_samples'] == 1
-            assert torch.equal(outputs['results']['lq'],
-                               data_batch['lq'].cpu())
-            assert torch.equal(outputs['results']['gt'],
-                               data_batch['gt'].cpu())
-            assert torch.is_tensor(outputs['results']['output'])
-            assert outputs['results']['output'].size() == (1, 3, 128, 128)
-
-    # test disc_steps and disc_init_steps
-    data_batch = {'lq': inputs.cpu(), 'gt': targets.cpu()}
-    train_cfg = dict(disc_steps=2, disc_init_steps=2)
     restorer = build_model(model_cfg, train_cfg=train_cfg, test_cfg=test_cfg)
-    with patch.object(
-            restorer,
-            'perceptual_loss',
-            return_value=(torch.tensor(1.0), torch.tensor(2.0))):
-        outputs = restorer.train_step(data_batch, optimizer)
-        assert isinstance(outputs, dict)
-        assert isinstance(outputs['log_vars'], dict)
-        for v in ['loss_d_real', 'loss_d_fake']:
-            assert isinstance(outputs['log_vars'][v], float)
-        assert outputs['num_samples'] == 1
-        assert torch.equal(outputs['results']['lq'], data_batch['lq'])
-        assert torch.equal(outputs['results']['gt'], data_batch['gt'])
-        assert torch.is_tensor(outputs['results']['output'])
-        assert outputs['results']['output'].size() == (1, 3, 128, 128)
+    meta = [{'lq_path': ''}]
 
-    # test no discriminator (testing mode)
-    model_cfg_ = model_cfg.copy()
-    model_cfg_.pop('discriminator')
-    restorer = build_model(model_cfg_, train_cfg=train_cfg, test_cfg=test_cfg)
-    with torch.no_grad():
-        outputs = restorer(**data_batch, test_mode=True)
-    assert torch.equal(outputs['lq'], data_batch['lq'])
-    assert torch.is_tensor(outputs['output'])
-    assert outputs['output'].size() == (1, 3, 128, 128)
+    # test forward_test (cpu)
+    with pytest.raises(ValueError):  # iteration is not None or number
+        with torch.no_grad():
+            restorer(
+                **data_batch,
+                test_mode=True,
+                save_image=True,
+                meta=meta,
+                iteration='1')
+    with pytest.raises(AssertionError):  # test with metric but gt is None
+        with torch.no_grad():
+            data_batch.pop('gt')
+            restorer(**data_batch, test_mode=True)
 
-    # test without pixel loss and perceptual loss
-    model_cfg_ = model_cfg.copy()
-    model_cfg_.pop('pixel_loss')
-    restorer = build_model(model_cfg_, train_cfg=None, test_cfg=None)
-
-    outputs = restorer.train_step(data_batch, optimizer)
-    assert isinstance(outputs, dict)
-    assert isinstance(outputs['log_vars'], dict)
-    for v in ['loss_gan', 'loss_d_real', 'loss_d_fake']:
-        assert isinstance(outputs['log_vars'][v], float)
-    assert outputs['num_samples'] == 1
-    assert torch.equal(outputs['results']['lq'], data_batch['lq'])
-    assert torch.equal(outputs['results']['gt'], data_batch['gt'])
-    assert torch.is_tensor(outputs['results']['output'])
-    assert outputs['results']['output'].size() == (1, 3, 128, 128)
-
-    # test train_step w/o loss_percep
-    restorer = build_model(model_cfg, train_cfg=None, test_cfg=None)
-    with patch.object(
-            restorer, 'perceptual_loss',
-            return_value=(None, torch.tensor(2.0))):
-        outputs = restorer.train_step(data_batch, optimizer)
-        assert isinstance(outputs, dict)
-        assert isinstance(outputs['log_vars'], dict)
-        for v in [
-                'loss_style', 'loss_gan', 'loss_d_real', 'loss_d_fake',
-                'loss_pix'
-        ]:
-            assert isinstance(outputs['log_vars'][v], float)
-        assert outputs['num_samples'] == 1
-        assert torch.equal(outputs['results']['lq'], data_batch['lq'])
-        assert torch.equal(outputs['results']['gt'], data_batch['gt'])
-        assert torch.is_tensor(outputs['results']['output'])
-        assert outputs['results']['output'].size() == (1, 3, 128, 128)
-
-    # test train_step w/o loss_style
-    restorer = build_model(model_cfg, train_cfg=None, test_cfg=None)
-    with patch.object(
-            restorer, 'perceptual_loss',
-            return_value=(torch.tensor(2.0), None)):
-        outputs = restorer.train_step(data_batch, optimizer)
-        assert isinstance(outputs, dict)
-        assert isinstance(outputs['log_vars'], dict)
-        for v in [
-                'loss_perceptual', 'loss_gan', 'loss_d_real', 'loss_d_fake',
-                'loss_pix'
-        ]:
-            assert isinstance(outputs['log_vars'][v], float)
-        assert outputs['num_samples'] == 1
-        assert torch.equal(outputs['results']['lq'], data_batch['lq'])
-        assert torch.equal(outputs['results']['gt'], data_batch['gt'])
-        assert torch.is_tensor(outputs['results']['output'])
-        assert outputs['results']['output'].size() == (1, 3, 128, 128)
+    # test forward_test (gpu)
+    if torch.cuda.is_available():
+        data_batch = {'lq': inputs.cuda(), 'gt': targets.cuda()}
+        restorer = restorer.cuda()
+        with pytest.raises(ValueError):  # iteration is not None or number
+            with torch.no_grad():
+                restorer(
+                    **data_batch,
+                    test_mode=True,
+                    save_image=True,
+                    meta=meta,
+                    iteration='1')
+        with pytest.raises(AssertionError):  # test with metric but gt is None
+            with torch.no_grad():
+                data_batch.pop('gt')
+                restorer(**data_batch, test_mode=True)
