b066bb6c4eab63b17a4d7816ea4e12a9f7792ab2
ys-li <56712176+Yshuo-Li@users.noreply.github.com>
2021-05-25 20:04:44 +0800

    [Feature] Add TTSR. (#321)
    * [Feature] Add TTSR.
    
    * Add test
    
    * Fix
    
    Co-authored-by: liyinshuo <liyinshuo@sensetime.com>

--- tests/test_ttsr.py
+++ tests/test_ttsr.py
@@ -1,6 +1,9 @@
+import numpy as np
 import torch
+from mmcv.runner import obj_from_dict
+from mmcv.utils.config import Config
 
-from mmedit.models import build_backbone
+from mmedit.models import build_backbone, build_model
 from mmedit.models.backbones.sr_backbones.ttsr_net import (CSFI2, CSFI3, SFE,
                                                            MergeFeatures)
 
@@ -56,3 +59,88 @@ def test_ttsr_net():
     outputs = ttsr(inputs, s, t_level3, t_level2, t_level1)
 
     assert outputs.shape == (2, 3, 96, 96)
+
+
+def test_ttsr():
+
+    model_cfg = dict(
+        type='TTSR',
+        generator=dict(
+            type='TTSRNet',
+            in_channels=3,
+            out_channels=3,
+            mid_channels=64,
+            num_blocks=(16, 16, 8, 4)),
+        extractor=dict(type='LTE'),
+        transformer=dict(type='SearchTransformer'),
+        pixel_loss=dict(type='L1Loss', loss_weight=1.0, reduction='mean'))
+
+    scale = 4
+    train_cfg = None
+    test_cfg = Config(dict(metrics=['PSNR', 'SSIM'], crop_border=scale))
+
+    # build restorer
+    restorer = build_model(model_cfg, train_cfg=train_cfg, test_cfg=test_cfg)
+
+    # test attributes
+    assert restorer.__class__.__name__ == 'TTSR'
+
+    # prepare data
+    inputs = torch.rand(1, 3, 16, 16)
+    targets = torch.rand(1, 3, 64, 64)
+    ref = torch.rand(1, 3, 64, 64)
+    data_batch = {
+        'lq': inputs,
+        'gt': targets,
+        'ref': ref,
+        'lq_up': ref,
+        'ref_downup': ref
+    }
+
+    # prepare optimizer
+    optim_cfg = dict(type='Adam', lr=1e-4, betas=(0.9, 0.999))
+    optimizer = obj_from_dict(optim_cfg, torch.optim,
+                              dict(params=restorer.parameters()))
+
+    # test train_step and forward_test (cpu)
+    outputs = restorer.train_step(data_batch, optimizer)
+    assert isinstance(outputs, dict)
+    assert isinstance(outputs['log_vars'], dict)
+    assert isinstance(outputs['log_vars']['loss_pix'], float)
+    assert outputs['num_samples'] == 1
+    assert outputs['results']['lq'].shape == data_batch['lq'].shape
+    assert outputs['results']['gt'].shape == data_batch['gt'].shape
+    assert torch.is_tensor(outputs['results']['output'])
+    assert outputs['results']['output'].size() == (1, 3, 64, 64)
+
+    # test train_step and forward_test (gpu)
+    if torch.cuda.is_available():
+        restorer = restorer.cuda()
+        data_batch = {
+            'lq': inputs.cuda(),
+            'gt': targets.cuda(),
+            'ref': ref.cuda(),
+            'lq_up': ref.cuda(),
+            'ref_downup': ref.cuda()
+        }
+
+        # train_step
+        optimizer = obj_from_dict(optim_cfg, torch.optim,
+                                  dict(params=restorer.parameters()))
+        outputs = restorer.train_step(data_batch, optimizer)
+        assert isinstance(outputs, dict)
+        assert isinstance(outputs['log_vars'], dict)
+        assert isinstance(outputs['log_vars']['loss_pix'], float)
+        assert outputs['num_samples'] == 1
+        assert outputs['results']['lq'].shape == data_batch['lq'].shape
+        assert outputs['results']['gt'].shape == data_batch['gt'].shape
+        assert torch.is_tensor(outputs['results']['output'])
+        assert outputs['results']['output'].size() == (1, 3, 64, 64)
+
+        # val_step
+        result = restorer.val_step(data_batch, meta=[{'gt_path': ''}])
+        assert isinstance(result, dict)
+        assert isinstance(result['eval_result'], dict)
+        assert result['eval_result'].keys() == set({'PSNR', 'SSIM'})
+        assert isinstance(result['eval_result']['PSNR'], np.float64)
+        assert isinstance(result['eval_result']['SSIM'], np.float64)
