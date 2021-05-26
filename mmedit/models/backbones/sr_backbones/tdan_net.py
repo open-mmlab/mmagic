807228bb71ddc0b69f30a3b19cdc3826ef297afb
ckkelvinchan <kelvinckchan@outlook.com>
2021-05-26 10:28:25 +0800

    Update backbone

--- mmedit/models/backbones/sr_backbones/tdan_net.py
+++ mmedit/models/backbones/sr_backbones/tdan_net.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 from mmcv.cnn import ConvModule, constant_init
-from mmcv.ops import DeformConv2dPack, deform_conv2d
+from mmcv.ops import DeformConv2d, DeformConv2dPack, deform_conv2d
 from mmcv.runner import load_checkpoint
 from torch.nn.modules.utils import _pair
 
@@ -11,7 +11,7 @@ from mmedit.models.registry import BACKBONES
 from mmedit.utils import get_root_logger
 
 
-class AugmentedDeformConv2dPack(DeformConv2dPack):
+class AugmentedDeformConv2dPack(DeformConv2d):
     """Augmented Deformable Convolution Pack.
 
     Different from DeformConv2dPack, which generates offsets from the
@@ -19,13 +19,17 @@ class AugmentedDeformConv2dPack(DeformConv2dPack):
     generate the offsets.
 
     Args:
-        in_channels (int): Same as nn.Conv2d.
-        out_channels (int): Same as nn.Conv2d.
-        kernel_size (int or tuple[int]): Same as nn.Conv2d.
-        stride (int or tuple[int]): Same as nn.Conv2d.
-        padding (int or tuple[int]): Same as nn.Conv2d.
-        dilation (int or tuple[int]): Same as nn.Conv2d.
-        groups (int): Same as nn.Conv2d.
+        in_channels (int): Number of channels in the input feature.
+        out_channels (int): Number of channels produced by the convolution.
+        kernel_size (int or tuple[int]): Size of the convolving kernel.
+        stride (int or tuple[int]): Stride of the convolution. Default: 1.
+        padding (int or tuple[int]): Zero-padding added to both sides of the
+            input. Default: 0.
+        dilation (int or tuple[int]): Spacing between kernel elements.
+            Default: 1.
+        groups (int): Number of blocked connections from input channels to
+            output channels. Default: 1.
+        deform_groups (int): Number of deformable group partitions.
         bias (bool or str): If specified as `auto`, it will be decided by the
             norm_cfg. Bias will be set as True if norm_cfg is None, otherwise
             False.
@@ -61,31 +65,48 @@ class TDANNet(nn.Module):
     Paper:
         TDAN: Temporally-Deformable Alignment Network for Video Super-
         Resolution, CVPR, 2020
+
+    Args:
+        in_channels (int): Number of channels of the input image. Default: 3.
+        mid_channels (int): Number of channels of the intermediate features.
+            Default: 64.
+        out_channels (int): Number of channels of the output image. Default: 3.
+        num_blocks (list[int]): Number of residual blocks before and after
+            temporal alignment. Default: [5, 10].
     """
 
-    def __init__(self):
+    def __init__(self,
+                 in_channels=3,
+                 mid_channels=64,
+                 out_channels=3,
+                 num_blocks=[5, 10]):
 
         super().__init__()
 
         self.feat_extract = nn.Sequential(
-            ConvModule(3, 64, 3, padding=1),
-            make_layer(ResidualBlockNoBN, 5, mid_channels=64))
+            ConvModule(in_channels, mid_channels, 3, padding=1),
+            make_layer(
+                ResidualBlockNoBN, num_blocks[0], mid_channels=mid_channels))
 
         self.feat_aggregate = nn.Sequential(
-            nn.Conv2d(128, 64, 3, padding=1, bias=True),
-            DeformConv2dPack(64, 64, 3, padding=1, deform_groups=8),
-            DeformConv2dPack(64, 64, 3, padding=1, deform_groups=8))
+            nn.Conv2d(mid_channels * 2, mid_channels, 3, padding=1, bias=True),
+            DeformConv2dPack(
+                mid_channels, mid_channels, 3, padding=1, deform_groups=8),
+            DeformConv2dPack(
+                mid_channels, mid_channels, 3, padding=1, deform_groups=8))
         self.align_1 = AugmentedDeformConv2dPack(
-            64, 64, 3, padding=1, deform_groups=8)
-        self.align_2 = DeformConv2dPack(64, 64, 3, padding=1, deform_groups=8)
-        self.to_rgb = nn.Conv2d(64, 3, 3, padding=1, bias=True)
+            mid_channels, mid_channels, 3, padding=1, deform_groups=8)
+        self.align_2 = DeformConv2dPack(
+            mid_channels, mid_channels, 3, padding=1, deform_groups=8)
+        self.to_rgb = nn.Conv2d(mid_channels, 3, 3, padding=1, bias=True)
 
         self.reconstruct = nn.Sequential(
-            ConvModule(3 * 5, 64, 3, padding=1),
-            make_layer(ResidualBlockNoBN, 10, mid_channels=64),
-            PixelShufflePack(64, 64, 2, upsample_kernel=3),
-            PixelShufflePack(64, 64, 2, upsample_kernel=3),
-            nn.Conv2d(64, 3, 3, 1, 1, bias=False))
+            ConvModule(mid_channels * 5, mid_channels, 3, padding=1),
+            make_layer(
+                ResidualBlockNoBN, num_blocks[1], mid_channels=mid_channels),
+            PixelShufflePack(mid_channels, mid_channels, 2, upsample_kernel=3),
+            PixelShufflePack(mid_channels, mid_channels, 2, upsample_kernel=3),
+            nn.Conv2d(mid_channels, out_channels, 3, 1, 1, bias=False))
 
     def forward(self, lrs):
         """Forward function for TDANNet.
@@ -94,9 +115,9 @@ class TDANNet(nn.Module):
             lrs (Tensor): Input LR sequence with shape (n, t, c, h, w).
 
         Returns:
-            Tensor: Output HR image with shape (n, c, 4h, 4w).
+            tuple[Tensor]: Output HR image with shape (n, c, 4h, 4w) and
+            aligned LR images with shape (n, t, c, h, w).
         """
-
         n, t, c, h, w = lrs.size()
         lr_center = lrs[:, t // 2, :, :, :]  # LR center frame
 
@@ -116,9 +137,10 @@ class TDANNet(nn.Module):
 
                 aligned_feat = self.align_2(self.align_1(feat_neig, feat_agg))
                 aligned_lrs.append(self.to_rgb(aligned_feat))
+
         aligned_lrs = torch.cat(aligned_lrs, dim=1)
 
-        # return HR center frame and the aligned LR frames
+        # output HR center frame and the aligned LR frames
         return self.reconstruct(aligned_lrs), aligned_lrs.view(n, t, c, h, w)
 
     def init_weights(self, pretrained=None, strict=True):
