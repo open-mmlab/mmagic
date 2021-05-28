4e6dca7e1f5cd6e1016b22ce1dfec3f4e8d4c5da
ckkelvinchan <kelvinckchan@outlook.com>
2021-05-28 11:26:02 +0800

    Add model to __init__

--- mmedit/models/restorers/__init__.py
+++ mmedit/models/restorers/__init__.py
@@ -5,9 +5,10 @@ from .esrgan import ESRGAN
 from .glean import GLEAN
 from .liif import LIIF
 from .srgan import SRGAN
+from .tdan import TDAN
 from .ttsr import TTSR
 
 __all__ = [
     'BasicRestorer', 'SRGAN', 'ESRGAN', 'EDVR', 'LIIF', 'BasicVSR', 'TTSR',
-    'GLEAN'
+    'GLEAN', 'TDAN'
 ]
