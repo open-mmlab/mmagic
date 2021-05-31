9ce281e5955186a8082b9a15e9bfc1b6662f0252
liyinshuo <liyinshuo@sensetime.com>
2021-05-31 11:19:18 +0800

    Rename

--- mmedit/datasets/pipelines/generate_assistant.py
+++ mmedit/datasets/pipelines/generate_assistant.py
@@ -64,9 +64,9 @@ class GenerateHeatmap:
 
         x_range = np.arange(start=0, stop=w, dtype=int)
         y_range = np.arange(start=0, stop=h, dtype=int)
-        xx, yy = np.meshgrid(x_range, y_range)
-        d2 = (xx - keypoint[0])**2 + (yy - keypoint[1])**2
-        exponent = d2 / 2.0 / self.sigma / self.sigma
+        grid_x, grid_y = np.meshgrid(x_range, y_range)
+        dist = (grid_x - keypoint[0])**2 + (grid_y - keypoint[1])**2
+        exponent = dist / 2.0 / self.sigma / self.sigma
         heatmap = np.exp(-exponent)
         return heatmap
 
