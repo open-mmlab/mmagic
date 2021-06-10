03433756f388fa59c1a9715b8d36b50b9b011170
ys-li <56712176+Yshuo-Li@users.noreply.github.com>
2021-06-10 16:37:50 +0800

    Fix (#350)
    Co-authored-by: liyinshuo <liyinshuo@sensetime.com>

--- tests/test_dic_net.py
+++ tests/test_dic_net.py
@@ -1,7 +1,7 @@
 import torch
 
-from mmedit.models.backbones.sr_backbones.dic_net import (FeedbackBlock,
-                                                          FeedbackBlockCustom)
+from mmedit.models.backbones.sr_backbones.dic_net import (
+    FeedbackBlock, FeedbackBlockCustom, FeedbackBlockHeatmapAttention)
 
 
 def test_feedback_block():
@@ -20,3 +20,14 @@ def test_feedback_block_custom():
     model = FeedbackBlockCustom(3, 16, 3, 8)
     x2 = model(x1)
     assert x2.shape == (2, 16, 32, 32)
+
+
+def test_feedback_block_heatmap_attention():
+    x1 = torch.rand(2, 16, 32, 32)
+    heatmap = torch.rand(2, 5, 32, 32)
+
+    model = FeedbackBlockHeatmapAttention(16, 2, 8, 5, 2)
+    x2 = model(x1, heatmap)
+    assert x2.shape == x1.shape
+    x3 = model(x2, heatmap)
+    assert x3.shape == x2.shape
