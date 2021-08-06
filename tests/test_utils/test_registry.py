fd0aa4d6f1b0aa126e850afdb0b004a135d5c14e
liyinshuo <liyinshuo@sensetime.com>
2021-08-06 11:33:21 +0800

    Remove test_registry.py

--- tests/test_utils/test_registry.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-
-def test_old_fashion_registry_importing():
-    with pytest.warns(DeprecationWarning):
-        from mmedit.models.registry import (  # noqa: F401
-            BACKBONES, COMPONENTS, LOSSES, MODELS)
-    with pytest.warns(DeprecationWarning):
-        from mmedit.datasets.registry import DATASETS, PIPELINES  # noqa: F401
