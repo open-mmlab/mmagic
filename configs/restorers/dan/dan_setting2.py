7151bfa2b69961f387a98372b9f3d8cd46621313
AlexZou <2435588629@qq.com>
2021-08-09 21:19:00 +0800

    Fix some bug to dan_setting2

--- configs/restorers/dan/dan_setting2.py
+++ configs/restorers/dan/dan_setting2.py
@@ -11,20 +11,24 @@ model = dict(
         input_para=10,
         loop=4,
         kernel_size=21,
-        pca_matrix_path='/home/ivdai/Temp/xxx/mmediting/tools/data/super-resolution/div2k/pca_matrix/pca_aniso_matrix_x4.pth'),
+        # Your pca matrix path
+        pca_matrix_path='mmediting/tools/data/super-resolution/div2k/pca_matrix/pca_aniso_matrix_x4.pth'),
     pixel_loss=dict(type='MSELoss', loss_weight=1.0, reduction='mean'))
 # model training and testing settings
-train_cfg = dict(pca_matrix_path='/home/ivdai/Temp/xxx/mmediting/tools/data/super-resolution/div2k/pca_matrix/pca_aniso_matrix_x4.pth',
-                 scale=scale,
-                 degradation=dict(
-                    random_kernel = True,
-                    ksize=21,
-                    code_length=10,
-                    sig_min=0.6,
-                    sig_max=5.0,
-                    rate_iso=0,
-                    random_disturb=True
-                 ))
+
+train_cfg = dict(
+    # Your pca matrix path
+    pca_matrix_path='mmediting/tools/data/super-resolution/div2k/pca_matrix/pca_aniso_matrix_x4.pth',
+    scale=scale,
+    degradation=dict(
+        random_kernel=True,
+        ksize=21,
+        code_length=10,
+        sig_min=0.6,
+        sig_max=5.0,
+        rate_iso=0,
+        random_disturb=True
+    ))
 test_cfg = dict(metrics=['PSNR', 'SSIM'], crop_border=scale)
 
 # dataset settings
@@ -84,20 +88,20 @@ data = dict(
         times=1000,
         dataset=dict(
             type=train_dataset_type,
-            gt_folder='/home/ivdai/Temp/xxx/dataset/DF2K_train_HR_sub',
+            gt_folder='dataset/DF2K_train_HR_sub',  # Your training data path
             pipeline=train_pipeline,
             scale=scale)),
     val=dict(
         type=val_dataset_type,
-        lq_folder='/home/ivdai/Temp/xxx/dataset/DIV2KRK/lr_x4',
-        gt_folder='/home/ivdai/Temp/xxx/dataset/DIV2KRK/gt',
+        lq_folder='dataset/DIV2KRK/lr_x4',  # Your validation data LR path
+        gt_folder='dataset/DIV2KRK/gt',  # Your validation data HR path
         pipeline=test_pipeline,
         scale=scale,
         filename_tmpl='{}'),
     test=dict(
         type=val_dataset_type,
-        lq_folder='/home/ivdai/Temp/xxx/dataset/DIV2KRK/lr_x4',
-        gt_folder='/home/ivdai/Temp/xxx/dataset/DIV2KRK/gt',
+        lq_folder='dataset/DIV2KRK/lr_x4',  # Your validation data LR path
+        gt_folder='dataset/DIV2KRK/gt',  # Your validation data HR path
         pipeline=test_pipeline,
         scale=scale,
         filename_tmpl='{}'))
@@ -107,7 +111,7 @@ optimizers = dict(generator=dict(type='Adam', lr=6.25e-6, betas=(0.9, 0.999)))
 
 # learning policy
 total_iters = 400000
-lr_config = dict(policy='Step', by_epoch=False, step=[100000,200000,300000], gamma=0.5)
+lr_config = dict(policy='Step', by_epoch=False, step=[100000, 200000, 300000], gamma=0.5)
 
 checkpoint_config = dict(interval=1000, save_optimizer=True, by_epoch=False)
 evaluation = dict(interval=1000, save_image=False, gpu_collect=True)
@@ -124,6 +128,6 @@ visual_config = None
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
 work_dir = f'./work_dirs/{exp_name}'
-load_from = '/home/ivdai/Temp/xxx/mmediting/danx4_l1_256/iter_40000.pth'
+load_from = 'mmediting/danx4_l1_256/iter_40000.pth'
 resume_from = None
 workflow = [('train', 1)]
