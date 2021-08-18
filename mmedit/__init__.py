30fa5d8410d2a84dcbf04cd77a58daef2bbbd683
lizz <innerlee@users.noreply.github.com>
2021-08-18 16:10:55 +0800

    use version_info of mmcv (#480)
    Signed-off-by: lizz <lizz@sensetime.com>

--- mmedit/__init__.py
+++ mmedit/__init__.py
@@ -2,21 +2,24 @@ import mmcv
 
 from .version import __version__, version_info
 
+try:
+    from mmcv.utils import digit_version
+except ImportError:
+
+    def digit_version(version_str):
+        digit_ver = []
+        for x in version_str.split('.'):
+            if x.isdigit():
+                digit_ver.append(int(x))
+            elif x.find('rc') != -1:
+                patch_version = x.split('rc')
+                digit_ver.append(int(patch_version[0]) - 1)
+                digit_ver.append(int(patch_version[1]))
+        return digit_ver
+
+
 MMCV_MIN = '1.3'
-MMCV_MAX = '1.4'
-
-
-def digit_version(version_str):
-    digit_ver = []
-    for x in version_str.split('.'):
-        if x.isdigit():
-            digit_ver.append(int(x))
-        elif x.find('rc') != -1:
-            patch_version = x.split('rc')
-            digit_ver.append(int(patch_version[0]) - 1)
-            digit_ver.append(int(patch_version[1]))
-    return digit_ver
-
+MMCV_MAX = '1.5'
 
 mmcv_min_version = digit_version(MMCV_MIN)
 mmcv_max_version = digit_version(MMCV_MAX)
