2c5a965100e7e12420074fc96d6d0c83e44cc6b8
ckkelvinchan <kelvinckchan@outlook.com>
2021-09-15 17:07:18 +0800

    face demo

--- mmedit/apis/__init__.py
+++ mmedit/apis/__init__.py
@@ -2,6 +2,7 @@
 from .generation_inference import generation_inference
 from .inpainting_inference import inpainting_inference
 from .matting_inference import init_model, matting_inference
+from .restoration_face_inference import restoration_face_inference
 from .restoration_inference import restoration_inference
 from .restoration_video_inference import restoration_video_inference
 from .test import multi_gpu_test, single_gpu_test
@@ -10,5 +11,6 @@ from .train import set_random_seed, train_model
 __all__ = [
     'train_model', 'set_random_seed', 'init_model', 'matting_inference',
     'inpainting_inference', 'restoration_inference', 'generation_inference',
-    'multi_gpu_test', 'single_gpu_test', 'restoration_video_inference'
+    'multi_gpu_test', 'single_gpu_test', 'restoration_video_inference',
+    'restoration_face_inference'
 ]
