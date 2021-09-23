99d58ac8ae697a1c91323c72003bed2d7f6918ae
ckkelvinchan <kelvinckchan@outlook.com>
2021-09-23 14:28:01 +0800

    add RealESRGAN model

--- mmedit/models/restorers/__init__.py
+++ mmedit/models/restorers/__init__.py
@@ -6,11 +6,12 @@ from .edvr import EDVR
 from .esrgan import ESRGAN
 from .glean import GLEAN
 from .liif import LIIF
+from .real_esrgan import RealESRGAN
 from .srgan import SRGAN
 from .tdan import TDAN
 from .ttsr import TTSR
 
 __all__ = [
     'BasicRestorer', 'SRGAN', 'ESRGAN', 'EDVR', 'LIIF', 'BasicVSR', 'TTSR',
-    'GLEAN', 'TDAN', 'DIC'
+    'GLEAN', 'TDAN', 'DIC', 'RealESRGAN'
 ]
