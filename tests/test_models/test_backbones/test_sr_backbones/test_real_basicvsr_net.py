41ca37cbb38b2c114715f700be2585413beca388
ckkelvinchan <kelvinckchan@outlook.com>
2021-12-04 10:52:48 +0800

    add test

--- tests/test_models/test_backbones/test_sr_backbones/test_real_basicvsr_net.py
+++ tests/test_models/test_backbones/test_sr_backbones/test_real_basicvsr_net.py
@@ -10,22 +10,46 @@ def test_real_basicvsr_net():
     """Test RealBasicVSR."""
 
     # cpu
+    # is_fix_cleaning = False
     real_basicvsr = RealBasicVSRNet(is_fix_cleaning=False)
-    real_basicvsr = RealBasicVSRNet(is_fix_cleaning=True)
+
+    # is_sequential_cleaning = False
+    real_basicvsr = RealBasicVSRNet(
+        is_fix_cleaning=True, is_sequential_cleaning=False)
     input_tensor = torch.rand(1, 5, 3, 64, 64)
     real_basicvsr.init_weights(pretrained=None)
     output = real_basicvsr(input_tensor)
     assert output.shape == (1, 5, 3, 256, 256)
 
+    # is_sequential_cleaning = True
+    real_basicvsr = RealBasicVSRNet(
+        is_fix_cleaning=True, is_sequential_cleaning=True)
+    output = real_basicvsr(input_tensor)
+    assert output.shape == (1, 5, 3, 256, 256)
+
+    with pytest.raises(TypeError):
+        # pretrained should be str or None
+        real_basicvsr.init_weights(pretrained=[1])
+
     # gpu
     if torch.cuda.is_available():
+        # is_fix_cleaning = False
         real_basicvsr = RealBasicVSRNet(is_fix_cleaning=False).cuda()
-        real_basicvsr = RealBasicVSRNet(is_fix_cleaning=True).cuda()
+
+        # is_sequential_cleaning = False
+        real_basicvsr = RealBasicVSRNet(
+            is_fix_cleaning=True, is_sequential_cleaning=False).cuda()
         input_tensor = torch.rand(1, 5, 3, 64, 64).cuda()
         real_basicvsr.init_weights(pretrained=None)
         output = real_basicvsr(input_tensor)
         assert output.shape == (1, 5, 3, 256, 256)
 
-    with pytest.raises(TypeError):
-        # pretrained should be str or None
-        real_basicvsr.init_weights(pretrained=[1])
+        # is_sequential_cleaning = True
+        real_basicvsr = RealBasicVSRNet(
+            is_fix_cleaning=True, is_sequential_cleaning=True).cuda()
+        output = real_basicvsr(input_tensor)
+        assert output.shape == (1, 5, 3, 256, 256)
+
+        with pytest.raises(TypeError):
+            # pretrained should be str or None
+            real_basicvsr.init_weights(pretrained=[1])
