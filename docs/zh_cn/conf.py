ee0561764fbfb3a4fef6dfd366814746b7298787
ckkelvinchan <kelvinckchan@outlook.com>
2021-12-18 16:59:25 +0800

    update

--- docs/zh_cn/conf.py
+++ docs/zh_cn/conf.py
@@ -15,7 +15,7 @@ import os
 import subprocess
 import sys
 
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
 
