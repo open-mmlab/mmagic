b6ffc3b7befe818cc76a255e83d6d0b02b2d596d
ckkelvinchan <kelvinckchan@outlook.com>
2021-12-18 19:05:59 +0800

    copy merge_docs.sh

--- docs/zh_cn/merge_docs.sh
+++ docs/zh_cn/merge_docs.sh
@@ -1,36 +1,36 @@
 #!/usr/bin/env bash
 
-sed -i '$a\\n<br/><hr/>\n' ../../configs/inpainting/*/*_zh-CN.md
-sed -i '$a\\n<br/><hr/>\n' ../../configs/mattors/*/*_zh-CN.md
-sed -i '$a\\n<br/><hr/>\n' ../../configs/restorers/*/*_zh-CN.md
-sed -i '$a\\n<br/><hr/>\n' ../../configs/synthesizers/*/*_zh-CN.md
+sed -i '$a\\n<br/><hr/>\n' ../../configs/inpainting/*/README.md
+sed -i '$a\\n<br/><hr/>\n' ../../configs/mattors/*/README.md
+sed -i '$a\\n<br/><hr/>\n' ../../configs/restorers/*/README.md
+sed -i '$a\\n<br/><hr/>\n' ../../configs/synthesizers/*/README.md
 
 # gather models
-cat ../../configs/inpainting/*/*_zh-CN.md | sed "s/md###t/html#t/g" | sed "s/#/#&/" | sed '1i\# 补全模型' | sed 's/](\/docs\//](/g' | sed 's=](/=](https://github.com/open-mmlab/mmediting/tree/master/=g' >inpainting_models.md
-cat ../../configs/mattors/*/*_zh-CN.md | sed "s/md###t/html#t/g" | sed "s/#/#&/" | sed '1i\# 抠图模型' | sed 's/](\/docs\//](/g' | sed 's=](/=](https://github.com/open-mmlab/mmediting/tree/master/=g' >mattors_models.md
-cat ../../configs/restorers/*/*_zh-CN.md | sed "s/md###t/html#t/g" | sed "s/#/#&/" | sed '1i\# 超分辨率模型' | sed 's/](\/docs\//](/g' | sed 's=](/=](https://github.com/open-mmlab/mmediting/tree/master/=g' >restorers_models.md
-cat ../../configs/synthesizers/*/*_zh-CN.md | sed "s/md###t/html#t/g" | sed "s/#/#&/" | sed '1i\# 生成模型' | sed 's/](\/docs\//](/g' | sed 's=](/=](https://github.com/open-mmlab/mmediting/tree/master/=g' >synthesizers_models.md
+cat ../../configs/inpainting/*/README.md | sed "s/md###t/html#t/g" | sed "s/#/#&/" | sed '1i\# Inpainting Models' | sed 's/](\/docs\//](/g' | sed 's=](/=](https://github.com/open-mmlab/mmediting/tree/master/=g' >inpainting_models.md
+cat ../../configs/mattors/*/README.md | sed "s/md###t/html#t/g" | sed "s/#/#&/" | sed '1i\# Matting Models' | sed 's/](\/docs\//](/g' | sed 's=](/=](https://github.com/open-mmlab/mmediting/tree/master/=g' >mattors_models.md
+cat ../../configs/restorers/*/README.md | sed "s/md###t/html#t/g" | sed "s/#/#&/" | sed '1i\# Super-Resolution Models' | sed 's/](\/docs\//](/g' | sed 's=](/=](https://github.com/open-mmlab/mmediting/tree/master/=g' >restorers_models.md
+cat ../../configs/synthesizers/*/README.md | sed "s/md###t/html#t/g" | sed "s/#/#&/" | sed '1i\# Generation Models' | sed 's/](\/docs\//](/g' | sed 's=](/=](https://github.com/open-mmlab/mmediting/tree/master/=g' >synthesizers_models.md
 
 # gather datasets
-cat ../../tools/data/generation/*_zh-CN.md > generation_datasets.md
-cat ../../tools/data/inpainting/*_zh-CN.md > inpainting_datasets.md
-cat ../../tools/data/matting/*_zh-CN.md > matting_datasets.md
-cat ../../tools/data/super-resolution/*_zh-CN.md > sr_datasets.md
+cat ../../tools/data/generation/README.md > generation_datasets.md
+cat ../../tools/data/inpainting/README.md > inpainting_datasets.md
+cat ../../tools/data/matting/README.md > matting_datasets.md
+cat ../../tools/data/super-resolution/README.md > sr_datasets.md
 
-sed -i 's=(paired-pix2pix/README_zh-CN.md)=(#paired-dataset-for-pix2pix)=g' generation_datasets.md
-sed -i 's=(unpaired-cyclegan/README_zh-CN.md)=(#unpaired-dataset-for-cyclegan)=g' generation_datasets.md
-sed -i 's=(paris-street-view/README_zh-CN.md)=(#paris-street-view-dataset)=g' inpainting_datasets.md
-sed -i 's=(celeba-hq/README_zh-CN.md)=(#celeba-hq-dataset)=g' inpainting_datasets.md
-sed -i 's=(places365/README_zh-CN.md)=(#places365-dataset)=g' inpainting_datasets.md
-sed -i 's=(comp1k/README_zh-CN.md)=(#composition-1k-dataset)=g' matting_datasets.md
-sed -i 's=(div2k/README_zh-CN.md)=(#div2k-dataset)=g' sr_datasets.md
-sed -i 's=(reds/README_zh-CN.md)=(#reds-dataset)=g' sr_datasets.md
-sed -i 's=(vimeo90k/README_zh-CN.md)=(#vimeo90k-dataset)=g' sr_datasets.md
+sed -i 's=(paired-pix2pix/README.md)=(#paired-dataset-for-pix2pix)=g' generation_datasets.md
+sed -i 's=(unpaired-cyclegan/README.md)=(#unpaired-dataset-for-cyclegan)=g' generation_datasets.md
+sed -i 's=(paris-street-view/README.md)=(#paris-street-view-dataset)=g' inpainting_datasets.md
+sed -i 's=(celeba-hq/README.md)=(#celeba-hq-dataset)=g' inpainting_datasets.md
+sed -i 's=(places365/README.md)=(#places365-dataset)=g' inpainting_datasets.md
+sed -i 's=(comp1k/README.md)=(#composition-1k-dataset)=g' matting_datasets.md
+sed -i 's=(div2k/README.md)=(#div2k-dataset)=g' sr_datasets.md
+sed -i 's=(reds/README.md)=(#reds-dataset)=g' sr_datasets.md
+sed -i 's=(vimeo90k/README.md)=(#vimeo90k-dataset)=g' sr_datasets.md
 
-cat ../../tools/data/generation/*/*_zh-CN.md | sed 's/# Preparing/# /g' | sed "s/#/#&/" | sed 's/](\/docs\//](/g' | sed 's=](/=](https://github.com/open-mmlab/mmediting/tree/master/=g' >> generation_datasets.md
-cat ../../tools/data/inpainting/*/*_zh-CN.md | sed 's/# Preparing/# /g' | sed "s/#/#&/" | sed 's/](\/docs\//](/g' | sed 's=](/=](https://github.com/open-mmlab/mmediting/tree/master/=g' >> inpainting_datasets.md
-cat ../../tools/data/matting/*/*_zh-CN.md | sed 's/# Preparing/# /g' | sed "s/#/#&/" | sed 's/](\/docs\//](/g' | sed 's=](/=](https://github.com/open-mmlab/mmediting/tree/master/=g' >> matting_datasets.md
-cat ../../tools/data/super-resolution/*/*_zh-CN.md | sed 's/# Preparing/# /g' | sed "s/#/#&/" | sed 's/](\/docs\//](/g' | sed 's=](/=](https://github.com/open-mmlab/mmediting/tree/master/=g' >> sr_datasets.md
+cat ../../tools/data/generation/*/README.md | sed 's/# Preparing/# /g' | sed "s/#/#&/" | sed 's/](\/docs\//](/g' | sed 's=](/=](https://github.com/open-mmlab/mmediting/tree/master/=g' >> generation_datasets.md
+cat ../../tools/data/inpainting/*/README.md | sed 's/# Preparing/# /g' | sed "s/#/#&/" | sed 's/](\/docs\//](/g' | sed 's=](/=](https://github.com/open-mmlab/mmediting/tree/master/=g' >> inpainting_datasets.md
+cat ../../tools/data/matting/*/README.md | sed 's/# Preparing/# /g' | sed "s/#/#&/" | sed 's/](\/docs\//](/g' | sed 's=](/=](https://github.com/open-mmlab/mmediting/tree/master/=g' >> matting_datasets.md
+cat ../../tools/data/super-resolution/*/README.md | sed 's/# Preparing/# /g' | sed "s/#/#&/" | sed 's/](\/docs\//](/g' | sed 's=](/=](https://github.com/open-mmlab/mmediting/tree/master/=g' >> sr_datasets.md
 
 # merge configs
 cat configs/config_*.md | sed "s/#/#&/" >> config.md
