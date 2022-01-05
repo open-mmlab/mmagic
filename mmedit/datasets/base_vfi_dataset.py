c396a597257f364eb870e8bf545befb4a74dd6ee
ysli <1375829094@qq.com>
2022-01-05 14:23:54 +0800

    Update

--- mmedit/datasets/base_vfi_dataset.py
+++ mmedit/datasets/base_vfi_dataset.py
@@ -9,7 +9,7 @@ IMG_EXTENSIONS = ('.jpg', '.JPG', '.jpeg', '.JPEG', '.png', '.PNG', '.ppm',
 
 
 class BaseVFIDataset(BaseDataset):
-    """Base class for video interpolation datasets.
+    """Base class for video frame interpolation datasets.
     """
 
     def __init__(self, pipeline, folder, ann_file, test_mode=False):
