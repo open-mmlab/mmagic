264139fc5e8bb75107b342ea83a704c8aeb8198f
lintsuihin <lintsuihin@sensetime.com>
2022-01-17 16:30:39 +0800

    modified based on comments

--- mmedit/models/__init__.py
+++ mmedit/models/__init__.py
@@ -6,8 +6,8 @@ from .builder import (build, build_backbone, build_component, build_loss,
 from .common import *  # noqa: F401, F403
 from .components import *  # noqa: F401, F403
 from .extractors import LTE, FeedbackHourglass
-from .inpaintors import (DeepFillv1Inpaintor, GLInpaintor, OneStageInpaintor,
-                         PConvInpaintor, TwoStageInpaintor)
+from .inpaintors import (AOTInpaintor, DeepFillv1Inpaintor, GLInpaintor,
+                         OneStageInpaintor, PConvInpaintor, TwoStageInpaintor)
 from .losses import *  # noqa: F401, F403
 from .mattors import DIM, GCA, BaseMattor, IndexNet
 from .registry import BACKBONES, COMPONENTS, LOSSES, MODELS
@@ -17,7 +17,7 @@ from .transformers import SearchTransformer
 from .video_interpolator import CAIN, BasicInterpolator
 
 __all__ = [
-    'BaseModel', 'BasicRestorer', 'OneStageInpaintor', 'build',
+    'AOTInpaintor', 'BaseModel', 'BasicRestorer', 'OneStageInpaintor', 'build',
     'build_backbone', 'build_component', 'build_loss', 'build_model',
     'BACKBONES', 'COMPONENTS', 'LOSSES', 'BaseMattor', 'DIM', 'MODELS',
     'GLInpaintor', 'PConvInpaintor', 'SRGAN', 'ESRGAN', 'GCA',
