c90b6059a1928e047c9a922900b5e1e8ac0ed521
ys-li <56712176+Yshuo-Li@users.noreply.github.com>
2022-02-07 15:35:38 +0800

    [Enhance] Add setup multi-processing both in train and test. (#707)
    * Faster training
    
    * Add setup multi-processing both in train and test.

--- mmedit/utils/__init__.py
+++ mmedit/utils/__init__.py
@@ -1,4 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .logger import get_root_logger
+from .setup_env import setup_multi_processes
 
-__all__ = ['get_root_logger']
+__all__ = ['get_root_logger', setup_multi_processes]
