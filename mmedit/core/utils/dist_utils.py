309e8059bae99ceccac3f08241b121afe1bcecbd
Yshuo-Li <1375829094@qq.com>
2022-03-23 16:36:58 +0800

    Update device='cuda'

--- mmedit/core/utils/dist_utils.py
+++ mmedit/core/utils/dist_utils.py
@@ -5,7 +5,7 @@ import torch.distributed as dist
 from mmcv.runner import get_dist_info
 
 
-def sync_random_seed(seed=None, device='cpu'):
+def sync_random_seed(seed=None, device='cuda'):
     """Make sure different ranks share the same seed.
     All workers must call this function, otherwise it will deadlock.
     This method is generally used in `DistributedSampler`,
@@ -14,7 +14,7 @@ def sync_random_seed(seed=None, device='cpu'):
     Args:
         seed (int, Optional): The seed. Default to None.
         device (str): The device where the seed will be put on.
-            Default to 'cpu'.
+            Default to 'cuda'.
     Returns:
         int: Seed to be used.
     """
