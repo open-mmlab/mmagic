54c9101d00539021496eb5896db9063728032845
Yshuo-Li <1375829094@qq.com>
2022-03-29 11:51:23 +0800

    [Fix] Use underline for required aruments in python files.

--- demo/restoration_demo.py
+++ demo/restoration_demo.py
@@ -28,7 +28,6 @@ def parse_args():
 
 def main():
     args = parse_args()
-    print(args)
 
     if not os.path.isfile(args.img_path):
         raise ValueError('It seems that you did not input a valid '
