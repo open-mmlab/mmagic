f625c0ce403aab22a5af5f5d9d1d4f8f9f851a4d
Yshuo-Li <1375829094@qq.com>
2022-03-31 20:49:46 +0800

    Update unittest

--- tests/test_models/test_backbones/test_vfi_backbones/test_tof_vfi_net.py
+++ tests/test_models/test_backbones/test_vfi_backbones/test_tof_vfi_net.py
@@ -17,23 +17,34 @@ def test_tof_vfi_net():
 
     # prepare data
     inputs = torch.rand(1, 2, 3, 256, 248)
-    target = torch.rand(1, 3, 256, 248)
 
     # test on cpu
     output = model(inputs)
     assert torch.is_tensor(output)
-    assert output.shape == target.shape
-    with pytest.raises(OSError):
-        model.init_weights('')
-    with pytest.raises(TypeError):
-        model.init_weights(1)
+    assert output.shape == (1, 3, 256, 248)
 
     # test on gpu
     if torch.cuda.is_available():
         model = model.cuda()
         inputs = inputs.cuda()
-        target = target.cuda()
         output = model(inputs)
         output = model(inputs, True)
         assert torch.is_tensor(output)
-        assert output.shape == target.shape
+        assert output.shape == (1, 3, 256, 256)
+
+    inputs = torch.rand(1, 2, 3, 256, 256)
+    output = model(inputs)
+    assert torch.is_tensor(output)
+
+    with pytest.raises(OSError):
+        model.init_weights('')
+    with pytest.raises(TypeError):
+        model.init_weights(1)
+    with pytest.raises(OSError):
+        model_cfg = dict(
+            type='TOFlowVFINet', flow_cfg=dict(norm_cfg=None, pretrained=''))
+        model = build_backbone(model_cfg)
+    with pytest.raises(TypeError):
+        model_cfg = dict(
+            type='TOFlowVFINet', flow_cfg=dict(norm_cfg=None, pretrained=1))
+        model = build_backbone(model_cfg)
