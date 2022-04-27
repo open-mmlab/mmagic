8e463f3bc05fb59ed646707044b084a5b9a720ab
Yshuo-Li <1375829094@qq.com>
2022-04-27 14:43:53 +0800

    [Feature] Support FLAVR Net

---
+++ tests/test_models/test_backbones/test_vfi_backbones/test_flavr_net.py
@@ -0,0 +1,56 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import pytest
+import torch
+
+from mmedit.models import build_backbone
+from mmedit.models.backbones.vfi_backbones.flavr_net import UpConv3d
+
+
+def test_flavr_net():
+
+    model_cfg = dict(
+        type='FLAVRNet',
+        input_frames=4,
+        output_frames=1,
+        mid_channels_list=[64, 32, 16, 8],
+        encoder_layers_list=[1, 1, 1, 1],
+        bias=False,
+        norm_cfg=None,
+        join_type='concat',
+        up_mode='transpose')
+
+    # build model
+    model = build_backbone(model_cfg)
+
+    # test attributes
+    assert model.__class__.__name__ == 'FLAVRNet'
+
+    # prepare data
+    inputs = torch.rand(1, 4, 3, 256, 248)
+    target = torch.rand(1, 3, 256, 248)
+
+    # test on cpu
+    output = model(inputs)
+    output = model(inputs)
+    assert torch.is_tensor(output)
+    assert output.shape == target.shape
+    with pytest.raises(OSError):
+        model.init_weights('')
+    with pytest.raises(TypeError):
+        model.init_weights(1)
+
+    model.decoder._join_tensors(inputs, inputs)
+
+    UpConv3d(4, 4, 1, 1, 1)
+    conv3d = UpConv3d(4, 4, 1, 1, 1, up_mode='trilinear', batchnorm=True)
+    conv3d(inputs)
+
+    # test on gpu
+    if torch.cuda.is_available():
+        model = model.cuda()
+        inputs = inputs.cuda()
+        target = target.cuda()
+        output = model(inputs)
+        output = model(inputs)
+        assert torch.is_tensor(output)
+        assert output.shape == target.shape
