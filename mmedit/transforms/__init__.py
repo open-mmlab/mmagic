f3f8510cbc250e8609d35ea79425062824927b10
Yshuo-Li <1375829094@qq.com>
2022-05-18 18:01:21 +0800

    [Refactor] Crop

--- mmedit/transforms/__init__.py
+++ mmedit/transforms/__init__.py
@@ -1,6 +1,18 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from .crop import (Crop, CropAroundCenter, CropAroundFg, CropAroundUnknown,
+                   CropLike, FixedCrop, ModCrop, PairedRandomCrop,
+                   RandomResizedCrop)
 from .loading import LoadImageFromFile
 
 __all__ = [
+    'Crop',
+    'CropAroundCenter',
+    'CropAroundFg',
+    'CropAroundUnknown',
+    'CropLike',
+    'FixedCrop',
     'LoadImageFromFile',
+    'ModCrop',
+    'PairedRandomCrop',
+    'RandomResizedCrop',
 ]
