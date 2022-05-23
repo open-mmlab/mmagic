245b467a8cb6f27a21bce7f31a769d1bd0e89f8a
Yshuo-Li <1375829094@qq.com>
2022-05-23 03:32:23 +0000

    [Docs] Update docstring of edit_data_sample

--- mmedit/data_element/edit_data_sample.py
+++ mmedit/data_element/edit_data_sample.py
@@ -27,77 +27,43 @@ class EditDataSample(BaseDataElement):
     Examples:
          >>> import torch
          >>> import numpy as np
-         >>> from mmengine.data import PixelData
-         >>> from mmedit.core import EditDataSample
+         >>> from mmedit.data_element import EditDataSample, PixelData
          >>> data_sample = EditDataSample()
          >>> img_meta = dict(img_shape=(800, 1196, 3))
-         >>> gt_img = PixelData(metainfo=img_meta)
-         >>> gt_img = torch.rand((3, 800, 1196))
+         >>> img = torch.rand((3, 800, 1196))
+         >>> gt_img = PixelData(data=img, metainfo=img_meta)
          >>> data_sample.gt_img = gt_img
          >>> assert 'img_shape' in data_sample.gt_img.metainfo_keys()
-         >>> len(data_sample.gt_img)
-         5
-         >>> print(data_sample)
-        <DetDataSample(
+        <EditDataSample(
 
             META INFORMATION
 
             DATA FIELDS
-            gt_img: <PixelData(
+            _gt_img: <PixelData(
 
                     META INFORMATION
-                    pad_shape: (800, 1216, 3)
                     img_shape: (800, 1196, 3)
 
                     DATA FIELDS
-                    labels: tensor([0.8533, 0.1550, 0.5433, 0.7294, 0.5098])
-                    bboxes:
-                    tensor([[9.7725e-01, 5.8417e-01, 1.7269e-01, 6.5694e-01],
-                            [1.7894e-01, 5.1780e-01, 7.0590e-01, 4.8589e-01],
-                            [7.0392e-01, 6.6770e-01, 1.7520e-01, 1.4267e-01],
-                            [2.2411e-01, 5.1962e-01, 9.6953e-01, 6.6994e-01],
-                            [4.1338e-01, 2.1165e-01, 2.7239e-04, 6.8477e-01]])
-                ) at 0x7f21fb1b9190>
-        ) at 0x7f21fb1b9880>
-         >>> pred_img = PixelData(metainfo=img_meta)
-         >>> pred_img.bboxes = torch.rand((5, 4))
-         >>> pred_img.scores = torch.rand((5,))
-         >>> data_sample.pred_img = pred_img
-         >>> print(data_sample)
-        <DetDataSample(
+                    data: tensor([[[0.8069, 0.4279,  ..., 0.6603, 0.0292],
 
-            META INFORMATION
+                                ...,
 
-            DATA FIELDS
+                                [0.8139, 0.0908,  ..., 0.4964, 0.9672]]])
+                ) at 0x1f6ae000af0>
             gt_img: <PixelData(
 
                     META INFORMATION
                     img_shape: (800, 1196, 3)
-                    pad_shape: (800, 1216, 3)
 
                     DATA FIELDS
-                    labels: tensor([0.2627, 0.3778, 0.2038, 0.3375, 0.9851])
-                    bboxes: tensor([[0.2193, 0.1166, 0.5360, 0.7132],
-                                [0.9293, 0.8853, 0.5077, 0.6340],
-                                [0.0620, 0.4339, 0.4820, 0.0469],
-                                [0.9636, 0.5789, 0.4534, 0.1950],
-                                [0.0142, 0.4320, 0.5696, 0.9699]])
-                ) at 0x7f0b7b8d6190>
-            pred_img: <PixelData(
+                    data: tensor([[[0.8069, 0.4279,  ..., 0.6603, 0.0292],
 
-                    META INFORMATION
-                    img_shape: (800, 1196, 3)
-                    pad_shape: (800, 1216, 3)
+                                ...,
 
-                    DATA FIELDS
-                    bboxes: tensor([[0.0246, 0.9718, 0.7703, 0.7454],
-                                [0.7734, 0.9903, 0.4460, 0.9821],
-                                [0.0435, 0.7106, 0.0547, 0.8913],
-                                [0.7212, 0.6108, 0.8398, 0.4018],
-                                [0.2552, 0.1137, 0.1374, 0.4659]])
-                    scores: tensor([0.5802, 0.9886, 0.9108, 0.2662, 0.9374])
-                ) at 0x7f0b7b8d61c0>
-        ) at 0x7f0b7b8d6880>
+                                [0.8139, 0.0908,  ..., 0.4964, 0.9672]]])
+                ) at 0x1f6ae000af0>
+        ) at 0x1f6a5a99a00>
     """
 
     @property
