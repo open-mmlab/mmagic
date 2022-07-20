65c58c5c124cf93e361c907027c2e1c3b39fd1f9
Yshuo-Li <1375829094@qq.com>
2022-07-20 17:23:32 +0800

    benchmark test

---
+++ .dev_scripts/benchmark_regression/download_models.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# Copyright (c) OpenMMLab. All rights reserved.
+
+# This tool is used to update README.md and README_zh-CN.md in configs
+
+import glob
+import os
+import posixpath as osp  # Even on windows, use posixpath
+import re
+import sys
+
+import wget
+
+MMEditing_ROOT = osp.dirname(osp.dirname(osp.dirname(__file__)))
+DOWNLOAD_DIR = osp.join(MMEditing_ROOT, 'work_dirs', 'download')
+
+
+def find_pth_files(file: str):
+    """Find all strs of pth files from a file.
+
+    Args:
+        file (str): The original file (config or README).
+
+    Returns:
+        List[str]: List of pth files.
+    """
+
+    with open(file, 'r', encoding='utf-8') as f:
+        data = f.read()
+
+    if file.endswith('md'):
+        pth_files = re.findall(r'\[model\]\((https://.*?\.pth)\)', data)
+    else:
+        pth_files = re.findall(r'\=.?\'(https://.*?\.pth)\'', data, re.S)
+
+    return (pth_files)
+
+
+def find_all_pth(md_file):
+    """Find all pretrained checkpoints of a method (pth).
+
+    Args:
+        md_file (str): Path to .md file.
+
+    Returns:
+        Bool: If the target .pth files are downloaded successfully.
+    """
+
+    md_file = md_file.replace(os.sep, '/')
+    config_dir, _ = osp.split(md_file)
+    files = os.listdir(config_dir)
+    config_files = [
+        osp.join(config_dir, file) for file in files if file.endswith('.py')
+    ]
+    all_files = config_files + [md_file]
+    pth_files = []
+    for file in all_files:
+        sub_list = find_pth_files(file)
+        if len(sub_list) > 0:
+            pth_files.extend(sub_list)
+    return pth_files
+
+
+def download_pth(pth_files):
+    """Download all pth files.
+
+    Args:
+        pth_files (List[str]): List of pth files.
+    """
+
+    # clear
+    def clear_path(path: str):
+        path = path.replace(' ', '')
+        path = path.replace('\'', '')
+        path = path.replace('\\', '')
+        path = path.replace('+', '')
+        path = path.replace('\n', '')
+        return path
+
+    pth_files = [clear_path(file) for file in pth_files]
+    pth_files.sort()
+    pth_files = set(pth_files)
+
+    if not osp.exists(DOWNLOAD_DIR):
+        os.makedirs(DOWNLOAD_DIR)
+    for url in pth_files:
+        path = osp.join(DOWNLOAD_DIR, osp.basename(url))
+        print()
+        print(f'download {path} from {url}')
+        wget.download(url, path)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) <= 1:
+        configs_root = osp.join(MMEditing_ROOT, 'configs')
+        file_list = glob.glob(
+            osp.join(configs_root, '**', '*README.md'), recursive=True)
+        file_list.sort()
+    else:
+        file_list = [
+            fn for fn in sys.argv[1:] if osp.basename(fn) == 'README.md'
+        ]
+
+    if not file_list:
+        sys.exit(0)
+
+    pth_files = []
+    for fn in file_list:
+        pth_files.extend(find_all_pth(fn))
+
+    download_pth(pth_files)
---
+++ .dev_scripts/benchmark_regression/test_models.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# Copyright (c) OpenMMLab. All rights reserved.
+
+# This tool is used to update README.md and README_zh-CN.md in configs
+
+import glob
+import os
+import posixpath as osp  # Even on windows, use posixpath
+import sys
+import threading
+
+import yaml
+
+MMEditing_ROOT = osp.dirname(osp.dirname(osp.dirname(__file__)))
+DOWNLOAD_DIR = osp.join(MMEditing_ROOT, 'work_dirs', 'download')
+LOG_DIR = osp.join(MMEditing_ROOT, 'work_dirs', 'test_benchmark')
+PARTITION = 'mm_research'
+
+sem = threading.Semaphore(8)  # The maximum number of restricted threads
+
+
+def slurm_test(info: dict, thread_num):
+    """Slurm test.
+
+    Args:
+        info (dict): Test info from metafile.yml
+    """
+
+    sem.acquire()
+
+    config = info['Config']
+    weights = info['Weights']
+    basename, _ = osp.splitext(osp.basename(config))
+
+    if osp.exists(DOWNLOAD_DIR):
+        weights = osp.join(DOWNLOAD_DIR, osp.basename(weights))
+
+    gpu_cmd = 'GPUS=1 GPUS_PER_NODE=1'
+    base_cmd = 'bash tools/slurm_test.sh'
+    task_cmd = f'{PARTITION} {basename}'
+    out_file = osp.join(LOG_DIR, basename)
+    cmd = f'{gpu_cmd} {base_cmd} {task_cmd} {config} {weights} > {out_file}'
+
+    print(f'RUN {thread_num:03d}: {cmd}')
+    os.system(cmd)
+
+    sem.release()
+
+
+def test_models(meta_file):
+    """Download all pth files.
+
+    Args:
+        pth_files (List[str]): List of pth files.
+    """
+
+    global thread_num
+
+    with open(meta_file, 'r', encoding='utf-8') as f:
+        data = f.read()
+    yaml_data = yaml.load(data, yaml.FullLoader)
+
+    for i in range(len(yaml_data['Models'])):
+        threading.Thread(
+            target=slurm_test,
+            args=(yaml_data['Models'][i], thread_num)).start()
+        thread_num += 1
+
+
+if __name__ == '__main__':
+    if len(sys.argv) <= 1:
+        configs_root = osp.join(MMEditing_ROOT, 'configs')
+        file_list = glob.glob(
+            osp.join(configs_root, '**', '*metafile.yml'), recursive=True)
+        file_list.sort()
+    else:
+        file_list = [
+            fn for fn in sys.argv[1:] if osp.basename(fn) == 'metafile.yml'
+        ]
+
+    if not file_list:
+        sys.exit(0)
+
+    if not osp.exists(LOG_DIR):
+        os.makedirs(LOG_DIR)
+
+    thread_num = 0
+    for fn in file_list:
+        test_models(fn)
