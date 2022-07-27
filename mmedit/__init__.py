2fe55f8957e7e40f96f30f88df028e84b119e344
Z-Fran <jdzhang.cs@gmail.com>
2022-07-27 18:31:04 +0800

    [Improve] Update MMCV and MMENGINE version

--- mmedit/__init__.py
+++ mmedit/__init__.py
@@ -1,5 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import mmcv
+import mmengine
 
 from .version import __version__, version_info
 
@@ -19,16 +20,25 @@ except ImportError:
         return digit_ver
 
 
-MMCV_MIN = '1.3.13'
-MMCV_MAX = '2.0'
-
+MMCV_MIN = '2.0.0rc0'
+MMCV_MAX = '2.0.0'
 mmcv_min_version = digit_version(MMCV_MIN)
 mmcv_max_version = digit_version(MMCV_MAX)
 mmcv_version = digit_version(mmcv.__version__)
 
+MMENGINE_MIN = '0.0.0'
+MMENGINE_MAX = '0.2.0'
+mmengine_min_version = digit_version(MMENGINE_MIN)
+mmengine_max_version = digit_version(MMENGINE_MAX)
+mmengine_version = digit_version(mmengine.__version__)
 
 assert (mmcv_min_version <= mmcv_version <= mmcv_max_version), \
     f'mmcv=={mmcv.__version__} is used but incompatible. ' \
     f'Please install mmcv-full>={mmcv_min_version}, <={mmcv_max_version}.'
+
+assert (mmengine_min_version <= mmengine_version <= mmengine_max_version), \
+    f'mmengine=={mmengine.__version__} is used but incompatible. ' \
+    f'Please install mmengine>={mmengine_min_version}, ' \
+    f'<={mmengine_max_version}.'
 
 __all__ = ['__version__', 'version_info']
