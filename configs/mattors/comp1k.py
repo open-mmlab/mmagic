a200e4a156ed864be41a1e5a59fb7470dd5b3a6b
wangruohui <12756472+wangruohui@users.noreply.github.com>
2022-08-03 13:46:11 +0800

    clean

--- configs/mattors/comp1k.py
+++ configs/mattors/comp1k.py
@@ -3,7 +3,6 @@
 # dataset settings
 dataset_type = 'AdobeComp1kDataset'
 data_root = 'data/adobe_composition-1k'
-# OR configure cluster=openmmlab in ~/petreloss.conf
 # data_root = 's3://openmmlab/datasets/editing/adobe_composition-1k'
 
 train_dataloader = dict(
