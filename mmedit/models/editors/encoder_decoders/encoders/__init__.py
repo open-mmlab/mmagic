590bab602a420385a52a8440a6f80763115518b6
zengyh1900 <zengyh1900@gmail.com>
2022-08-08 01:13:15 +0800

    [Refactor] expand all models under models/

--- mmedit/models/editors/encoder_decoders/encoders/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .aot_encoder import AOTEncoder
-from .deepfill_encoder import DeepFillEncoder
-from .gl_encoder import GLEncoder
-from .pconv_encoder import PConvEncoder
-
-__all__ = ['GLEncoder', 'PConvEncoder', 'DeepFillEncoder', 'AOTEncoder']
--- mmedit/models/editors/refiners/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .deepfill_refiner import DeepFillRefiner
-
-__all__ = ['DeepFillRefiner']
