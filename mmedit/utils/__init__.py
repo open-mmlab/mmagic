9df62a0af644e71ff3906db1d01ba09e6869b8ef
LeoXing1996 <xingzn1996@gmail.com>
2022-08-08 16:36:58 +0800

    revise ut for srgan

--- mmedit/utils/__init__.py
+++ mmedit/utils/__init__.py
@@ -2,9 +2,9 @@
 from .cli import modify_args
 # TODO replace with engine's API
 from .logger import get_root_logger, print_colored_log
-from .setup_env import register_all_modules
 
 __all__ = [
-    'modify_args', 'print_colored_log', 'get_root_logger',
-    'register_all_modules'
+    'modify_args',
+    'print_colored_log',
+    'get_root_logger',
 ]
