519a64ae667fef547a428b4e92b9297cf6cc1de8
Z-Fran <1396925302@qq.com>
2022-08-08 17:32:27 +0800

    refactor vsr models

--- mmedit/models/editors/__init__.py
+++ mmedit/models/editors/__init__.py
@@ -2,19 +2,27 @@
 # from deepfillv2 import DeepFillEncoderDecoder
 
 from .aotgan import AOTBlockNeck, AOTEncoderDecoder, AOTInpaintor
+from .basicvsr import BasicVSR, BasicVSRNet
+from .basicvsr_plusplus_net import BasicVSRPlusPlusNet
 from .cain import CAIN, CAINNet
 from .deepfillv1 import (ContextualAttentionModule, ContextualAttentionNeck,
                          DeepFillDecoder, DeepFillEncoder, DeepFillv1Inpaintor)
 from .deepfillv2 import DeepFillEncoderDecoder, SimpleGatedConvModule
+from .edvr import EDVR, EDVRNet
+from .esrgan import RRDBNet
 from .flavr import FLAVR, FLAVRNet
 from .gca import GCA
 from .global_local import (GLDecoder, GLDilationNeck, GLEncoder,
                            GLEncoderDecoder)
+from .iconvsr import IconVSRNet
 from .indexnet import IndexNet
 from .pconv import (PConvDecoder, PConvEncoder, PConvEncoderDecoder,
                     PConvInpaintor)
+from .real_basicvsr import RealBasicVSR, RealBasicVSRNet
+from .real_esrgan import RealESRGAN, UNetDiscriminatorWithSpectralNorm
 from .srcnn import SRCNNNet
-from .tof import TOFlowVFINet
+from .tdan import TDAN, TDANNet
+from .tof import TOFlowVFINet, TOFlowVSRNet
 
 __all__ = [
     'AOTEncoderDecoder',
@@ -27,6 +35,7 @@ __all__ = [
     'FLAVR',
     'FLAVRNet',
     'TOFlowVFINet',
+    'TOFlowVSRNet',
     'DeepFillEncoder',
     'DeepFillEncoderDecoder',
     'DeepFillDecoder',
@@ -43,4 +52,17 @@ __all__ = [
     'PConvInpaintor',
     'SimpleGatedConvModule',
     'SRCNNNet',
+    'RRDBNet',
+    'RealESRGAN',
+    'UNetDiscriminatorWithSpectralNorm',
+    'EDVR',
+    'EDVRNet',
+    'TDAN',
+    'TDANNet',
+    'BasicVSR',
+    'BasicVSRNet',
+    'BasicVSRPlusPlusNet',
+    'IconVSRNet',
+    'RealBasicVSR',
+    'RealBasicVSRNet',
 ]
