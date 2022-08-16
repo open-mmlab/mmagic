963f7b0b7acba3fb7aa6c526d9fdb697657a2206
Z-Fran <1396925302@qq.com>
2022-08-16 19:37:40 +0800

    fix test_apis UT

--- tests/test_apis/test_inpainting_inference.py
+++ tests/test_apis/test_inpainting_inference.py
@@ -1,1 +1,26 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import os.path as osp
+
+import torch
+
+from mmedit.apis import init_model, inpainting_inference
+
+
+def test_inpainting_inference():
+    if torch.cuda.is_available():
+        device = torch.device('cuda', 0)
+    else:
+        device = torch.device('cpu')
+
+    data_root = osp.join(osp.dirname(__file__), '../../')
+    config = data_root + 'configs/global_local/gl_256x256_8x12_celeba.py'
+    checkpoint = 'https://download.openmmlab.com/mmediting/inpainting/ \
+        global_local/gl_256x256_8x12_celeba_20200619-5af0493f.pth'
+
+    masked_img_path = data_root + 'tests/data/inpainting/celeba_test.png'
+    mask_path = data_root + 'tests/data/inpainting/bbox_mask.png'
+
+    model = init_model(config, checkpoint, device=device)
+
+    result = inpainting_inference(model, masked_img_path, mask_path)
+    assert result.detach().cpu().numpy().shape == (3, 256, 256)
--- tests/test_apis/test_video_interpolation_inference.py
+++ tests/test_apis/test_video_interpolation_inference.py
@@ -1,1 +1,28 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import os.path as osp
+
+import torch
+
+from mmedit.apis import init_model, video_interpolation_inference
+
+
+def test_video_interpolation_inference():
+    if torch.cuda.is_available():
+        device = torch.device('cuda', 0)
+    else:
+        device = torch.device('cpu')
+
+    data_root = osp.join(osp.dirname(__file__), '../../')
+    config = data_root + 'configs/cain/cain_b5_g1b32_vimeo90k_triplet.py'
+    checkpoint = 'https://download.openmmlab.com/mmediting/video_interpolators/ \
+        cain/cain_b5_320k_vimeo-triple_20220117-647f3de2.pth'
+
+    input_dir = data_root + 'tests/data/frames/test_inference.mp4'
+
+    model = init_model(config, checkpoint, device=device)
+
+    video_interpolation_inference(
+        model=model, input_dir=input_dir, output_dir='out', fps=60.0)
+
+
+test_video_interpolation_inference()
