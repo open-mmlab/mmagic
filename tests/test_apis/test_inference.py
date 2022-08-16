aab5a4da4e0449ac7c5f9af47ac50a4571344e72
Z-Fran <1396925302@qq.com>
2022-08-16 20:11:16 +0800

    fix test_apis UT

--- tests/test_apis/test_inference.py
+++ tests/test_apis/test_inference.py
@@ -14,8 +14,8 @@ def test_inference():
 
     data_root = osp.join(osp.dirname(__file__), '../../')
     config = data_root + 'configs/dim/dim_stage3_v16_pln_1x1_1000k_comp1k.py'
-    checkpoint = 'https://download.openmmlab.com/mmediting/mattors/dim/dim_ \
-        stage3_v16_pln_1x1_1000k_comp1k_SAD-50.6_20200609_111851-647f24b6.pth'
+    checkpoint = 'https://download.openmmlab.com/mmediting/mattors/dim/dim_' +\
+        'stage3_v16_pln_1x1_1000k_comp1k_SAD-50.6_20200609_111851-647f24b6.pth'
 
     img_path = data_root + 'tests/data/matting_dataset/merged/GT05.jpg'
     trimap_path = data_root + 'tests/data/matting_dataset/trimap/GT05.png'
