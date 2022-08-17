b0c4ae225ff0e2c36ef673dcb4e47e7253a977e4
zenggyh1900 <zengyh1900@gmail.com>
2022-08-17 15:49:21 +0800

    fix-ut

--- tests/configs/gl_test.py
+++ tests/configs/gl_test.py
@@ -1,4 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+
+input_shape = (256, 256)
+
 global_disc_cfg = dict(
     in_channels=3,
     max_channels=512,
@@ -60,3 +63,62 @@ model_dirty = dict(
     loss_gan=None,
     loss_l1_hole=None,
     loss_l1_valid=None)
+
+model_inference = dict(
+    type='GLInpaintor',
+    data_preprocessor=dict(
+        type='EditDataPreprocessor',
+        mean=[127.5],
+        std=[127.5],
+    ),
+    encdec=dict(
+        type='GLEncoderDecoder',
+        encoder=dict(type='GLEncoder'),
+        decoder=dict(type='GLDecoder'),
+        dilation_neck=dict(
+            type='GLDilationNeck', norm_cfg=dict(type='SyncBN'))),
+    disc=dict(
+        type='GLDiscs',
+        global_disc_cfg=dict(
+            in_channels=3,
+            max_channels=512,
+            fc_in_channels=512 * 4 * 4,
+            fc_out_channels=1024,
+            num_convs=6,
+            norm_cfg=dict(type='SyncBN'),
+        ),
+        local_disc_cfg=dict(
+            in_channels=3,
+            max_channels=512,
+            fc_in_channels=512 * 4 * 4,
+            fc_out_channels=1024,
+            num_convs=5,
+            norm_cfg=dict(type='SyncBN'),
+        ),
+    ))
+
+test_pipeline = [
+    dict(type='LoadImageFromFile', key='gt'),
+    dict(
+        type='LoadMask',
+        mask_mode='bbox',
+        mask_config=dict(
+            max_bbox_shape=(128, 128),
+            max_bbox_delta=40,
+            min_margin=20,
+            img_shape=input_shape)),
+    dict(
+        type='Crop',
+        keys=['gt'],
+        crop_size=(384, 384),
+        random_crop=True,
+    ),
+    dict(
+        type='Resize',
+        keys=['gt'],
+        scale=input_shape,
+        keep_ratio=False,
+    ),
+    dict(type='GetMaskedImage'),
+    dict(type='PackEditInputs'),
+]
