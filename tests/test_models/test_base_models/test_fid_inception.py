113c80b9fae426d07305e3cfec35c80e7c84682d
LeoXing <leoxing1996@gmail.com>
2022-09-17 16:37:02 +0800

    add unit test of FID inception

--- tests/test_models/test_base_models/test_fid_inception.py
+++ tests/test_models/test_base_models/test_fid_inception.py
@@ -1,2 +1,43 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-# TODO:
+import pytest
+import torch
+
+from mmedit.models import InceptionV3
+from mmedit.models.base_models.fid_inception import fid_inception_v3
+
+
+class TestFIDInception:
+
+    @classmethod
+    def setup_class(cls):
+        cls.load_fid_inception = False
+
+    def test_fid_inception(self):
+        inception = InceptionV3(load_fid_inception=self.load_fid_inception)
+        imgs = torch.randn((2, 3, 256, 256))
+        out = inception(imgs)[0]
+        assert out.shape == (2, 2048, 1, 1)
+
+        imgs = torch.randn((2, 3, 512, 512))
+        out = inception(imgs)[0]
+        assert out.shape == (2, 2048, 1, 1)
+
+    @pytest.mark.skipif(not torch.cuda.is_available(), reason='requires cuda')
+    def test_fid_inception_cuda(self):
+        inception = InceptionV3(
+            load_fid_inception=self.load_fid_inception).cuda()
+        imgs = torch.randn((2, 3, 256, 256)).cuda()
+        out = inception(imgs)[0]
+        assert out.shape == (2, 2048, 1, 1)
+
+        imgs = torch.randn((2, 3, 512, 512)).cuda()
+        out = inception(imgs)[0]
+        assert out.shape == (2, 2048, 1, 1)
+
+
+def test_load_fid_inception():
+    fid_net = fid_inception_v3(load_ckpt=False)
+
+    inputs = torch.randn(1, 3, 299, 299)
+    outputs = fid_net(inputs)
+    print(outputs)
