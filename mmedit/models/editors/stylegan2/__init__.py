c9db2b75023d4b87fc854c613bb3f7befbb20133
LeoXing <leoxing1996@gmail.com>
2022-09-17 21:36:54 +0800

    add unit test for styleGAN2 and ADA

--- mmedit/models/editors/stylegan2/__init__.py
+++ mmedit/models/editors/stylegan2/__init__.py
@@ -1,6 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .stylegan2 import StyleGAN2
-from .stylegan2_discriminator import StyleGAN2Discriminator
+from .stylegan2_discriminator import (ADAAug, ADAStyleGAN2Discriminator,
+                                      StyleGAN2Discriminator)
 from .stylegan2_generator import StyleGAN2Generator
 
-__all__ = ['StyleGAN2', 'StyleGAN2Discriminator', 'StyleGAN2Generator']
+__all__ = [
+    'StyleGAN2', 'StyleGAN2Discriminator', 'StyleGAN2Generator',
+    'ADAStyleGAN2Discriminator', 'ADAAug'
+]
