2f0e9106b752cd37f9f869b4c9eaeda155f53b6d
LeoXing1996 <xingzn1996@hotmail.com>
2022-09-22 13:23:55 +0800

    complete unit test of gen metric

--- tests/test_evaluation/test_metrics/test_base_gen_metric.py
+++ tests/test_evaluation/test_metrics/test_base_gen_metric.py
@@ -1,9 +1,18 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-# TODO:
+from unittest.mock import MagicMock, patch
+
+from mmengine.model import MMDistributedDataParallel
+
 from mmedit.evaluation.metrics.base_gen_metric import GenMetric
 
 
-class DemoMetric(GenMetric):
+def mock_collect_fn(results, *args, **kwargs):
+    return results[0]
+
+
+@patch('mmedit.evaluation.metrics.base_gen_metric.collect_results',
+       mock_collect_fn)
+class ToyMetric(GenMetric):
 
     def process(self, data_batch, data_samples):
         return
@@ -13,7 +22,24 @@ class DemoMetric(GenMetric):
 
 
 def test_GenMetric():
-    metric = DemoMetric(10, 10)
+    metric = ToyMetric(10, 10)
     assert metric.real_nums_per_device == 10
 
     metric.real_results = []
+
+    # test get_metric_sampler
+    model = MagicMock()
+    dataset = MagicMock()
+    dataloader = MagicMock()
+    dataloader.batch_size = 4
+    dataloader.dataset = dataset
+    sampler = metric.get_metric_sampler(model, dataloader, [metric])
+    assert sampler.dataset == dataset
+
+    # test prepare
+    model = MagicMock(spec=MMDistributedDataParallel)
+    preprocessor = MagicMock()
+    model.module = MagicMock()
+    model.module.data_preprocessor = preprocessor
+    metric.prepare(model, dataloader)
+    assert metric.data_preprocessor == preprocessor
