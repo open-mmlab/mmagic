6433f3c55d3e1f1e65df6f1552925d2fee28f95b
LeoXing <leoxing1996@gmail.com>
2022-09-23 18:29:44 +0800

    add pickle data hook

---
+++ tests/test_engine/test_hooks/test_pickle_data_hook.py
@@ -0,0 +1,42 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from unittest.mock import MagicMock
+
+import numpy as np
+import torch
+import torch.nn as nn
+
+from mmedit.engine.hooks import PickleDataHook
+
+
+class ToyModel(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.a = torch.randn(10, 10)
+        self.b = np.random.random((10, 10))
+
+
+def test_PickleDataHook():
+    hook = PickleDataHook(
+        output_dir='./', data_name_list=['a', 'b', 'c'], interval=3)
+    runner = MagicMock()
+    runner.iter = 0
+    runner.model = ToyModel()
+
+    # test after train iter
+    hook.after_train_iter(runner, 0, None, None)
+    hook.data_name_list = ['a', 'b']
+    hook.after_train_iter(runner, 0, None, None)
+
+    runner.model = MagicMock()
+    runner.model.module = ToyModel()
+    hook._pickle_data(runner)
+
+    runner.iter = 2
+    hook.after_train_iter(runner, 0, None, None)
+
+    # test after run
+    hook.after_run(runner)
+
+    # test before run
+    hook.before_run(runner)
