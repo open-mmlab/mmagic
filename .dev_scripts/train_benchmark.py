5610fdcbbe66f56741e48572a3049f7ac5080d10
LeoXing1996 <xingzn1996@hotmail.com>
2022-10-25 17:03:33 +0800

    support deterministic training in benchmark

--- .dev_scripts/train_benchmark.py
+++ .dev_scripts/train_benchmark.py
@@ -118,6 +118,10 @@ def parse_args():
         default='work_dirs/benchmark_train',
         help='the dir to save metric')
     parser.add_argument(
+        '--deterministic',
+        action='store_true',
+        help='Whether set `deterministic` during training.')
+    parser.add_argument(
         '--run', action='store_true', help='run script directly')
     parser.add_argument(
         '--local',
@@ -243,6 +247,9 @@ def create_train_job_batch(commands, model_info, args, port, script_name):
     else:
         job_script += '\n\n' + 'export CUDA_VISIBLE_DEVICES=-1\n'
 
+    if args.deterministic:
+        job_script += 'export CUBLAS_WORKSPACE_CONFIG=:4096:8\n'
+
     job_script += (f'export MASTER_PORT={port}\n'
                    f'{runner} -u {script_name} {config} '
                    f'--work-dir={work_dir} '
@@ -253,6 +260,9 @@ def create_train_job_batch(commands, model_info, args, port, script_name):
 
     if args.amp:
         job_script += ' --amp  '
+
+    if args.deterministic:
+        job_script += ' --cfg-options randomness.deterministic=True'
 
     job_script += '\n'
 
