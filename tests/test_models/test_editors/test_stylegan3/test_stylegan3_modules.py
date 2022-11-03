59c448cf9432e53e8c1b6c49737a933313729c9c
LeoXing1996 <xingzn1996@hotmail.com>
2022-11-03 19:19:42 +0800

    add unit test for stylegan3's mapping network

--- tests/test_models/test_editors/test_stylegan3/test_stylegan3_modules.py
+++ tests/test_models/test_editors/test_stylegan3/test_stylegan3_modules.py
@@ -5,8 +5,18 @@ from mmedit.models.editors.stylegan3.stylegan3_modules import MappingNetwork
 
 
 def test_MappingNetwork():
-    mapping_network = MappingNetwork(16, 4, 5, c_dim=8)
+    mapping_network = MappingNetwork(16, 4, 5, cond_channels=8)
     z = torch.randn(1, 16)
     c = torch.randn(1, 8)
     out = mapping_network(z, c)
+    assert out.shape == (1, 5, 4)
+
+    # test w/o conditional input
+    mapping_network = MappingNetwork(16, 4, 5, cond_channels=-1)
+    out = mapping_network(z)
+    assert out.shape == (1, 5, 4)
+
+    # test w/o noise input
+    mapping_network = MappingNetwork(0, 4, 5, cond_channels=8)
+    out = mapping_network(None, c)
     assert out.shape == (1, 5, 4)
