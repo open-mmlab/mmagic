fc111e890f2c66c7034265b9f8525572033525bb
nijkah <nijkah@gmail.com>
2022-11-21 10:35:21 +0900

    fix lint

--- mmedit/core/__init__.py
+++ mmedit/core/__init__.py
@@ -1,6 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .evaluation import (FID, KID, DistEvalIterHook, EvalIterHook, InceptionV3,
-                         L1Evaluation, mae, mse, psnr, reorder_image, sad, ssim)
+                         L1Evaluation, mae, mse, psnr, reorder_image, sad,
+                         ssim)
 from .hooks import MMEditVisualizationHook, VisualizationHook
 from .misc import tensor2img
 from .optimizer import build_optimizers
@@ -10,7 +11,6 @@ from .scheduler import LinearLrUpdaterHook, ReduceLrUpdaterHook
 __all__ = [
     'build_optimizers', 'tensor2img', 'EvalIterHook', 'DistEvalIterHook',
     'mse', 'psnr', 'reorder_image', 'sad', 'ssim', 'LinearLrUpdaterHook',
-    'VisualizationHook', 'MMEditVisualizationHook', 'L1Evaluation',
-    'FID', 'KID', 'InceptionV3', 'build_metric',
-    'ReduceLrUpdaterHook', 'mae'
+    'VisualizationHook', 'MMEditVisualizationHook', 'L1Evaluation', 'FID',
+    'KID', 'InceptionV3', 'build_metric', 'ReduceLrUpdaterHook', 'mae'
 ]
