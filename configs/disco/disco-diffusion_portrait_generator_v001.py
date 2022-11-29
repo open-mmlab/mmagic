44160ed778c4579bd8872cb82b84cb8f9a98a14a
plyfager <2744335995@qq.com>
2022-11-29 14:35:55 +0800

    update url

--- configs/disco/disco-diffusion_portrait_generator_v001.py
+++ configs/disco/disco-diffusion_portrait_generator_v001.py
@@ -1,5 +1,5 @@
 _base_ = ["./disco-diffusion_adm-u-finetuned_imagenet-512x512.py"]
-unet_ckpt_path = 'work_dirs/adm-u-cvt-rgb_portrait-v001.pth'  # noqa
+unet_ckpt_path = 'https://download.openmmlab.com/mmediting/synthesizers/disco/adm-u-cvt-rgb_portrait-v001-f4a3f3bc.pth'  # noqa
 model = dict(
     unet = dict(base_channels=128),
     secondary_model=None,
