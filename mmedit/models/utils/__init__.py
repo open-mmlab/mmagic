ab4c1123d8032b7e2710cdcadf279a48be6b1224
LeoXing1996 <xingzn1996@hotmail.com>
2022-11-29 21:32:08 +0800

    [Feature] Support camera class for EG3D (#1446)
    * add camera for eg3d
    
    * revise docstring of camera
    
    * add unit test for camera
    
    * revise uniform sampling in camera
    
    * revise camera as comment
    
    * move normalize_vecs to tensor_utils

--- mmedit/models/utils/__init__.py
+++ mmedit/models/utils/__init__.py
@@ -6,12 +6,12 @@ from .model_utils import (default_init_weights, generation_init_weights,
                           get_module_device, get_valid_noise_size,
                           get_valid_num_batches, make_layer, set_requires_grad)
 from .sampling_utils import label_sample_fn, noise_sample_fn
-from .tensor_utils import get_unknown_tensor
+from .tensor_utils import get_unknown_tensor, normalize_vecs
 
 __all__ = [
     'default_init_weights', 'make_layer', 'flow_warp',
     'generation_init_weights', 'set_requires_grad', 'extract_bbox_patch',
     'extract_around_bbox', 'get_unknown_tensor', 'noise_sample_fn',
     'label_sample_fn', 'get_valid_num_batches', 'get_valid_noise_size',
-    'get_module_device'
+    'get_module_device', 'normalize_vecs'
 ]
