06271043f7aa25f6bf2ed680b48562f01b4971fc
LeoXing1996 <xingzn1996@hotmail.com>
2022-11-29 21:32:08 +0800

    [Feature] Support camera class for EG3D (#1446)
    * add camera for eg3d
    
    * revise docstring of camera
    
    * add unit test for camera
    
    * revise uniform sampling in camera
    
    * revise camera as comment
    
    * move normalize_vecs to tensor_utils

--- tests/test_models/test_utils/test_tensor_utils.py
+++ tests/test_models/test_utils/test_tensor_utils.py
@@ -1,7 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import torch
 
-from mmedit.models.utils.tensor_utils import get_unknown_tensor
+from mmedit.models.utils.tensor_utils import get_unknown_tensor, normalize_vecs
 
 
 def test_tensor_utils():
@@ -11,3 +11,15 @@ def test_tensor_utils():
     input = torch.rand((2, 1, 128, 128))
     output = get_unknown_tensor(input)
     assert output.detach().numpy().shape == (2, 1, 128, 128)
+
+
+def test_normalize_vector():
+    inp = torch.randn(4, 10)
+    out = normalize_vecs(inp)
+
+    def l2_norm(a):
+        return a / torch.norm(a, dim=1, keepdim=True)
+
+    l2_norm_out = l2_norm(inp)
+
+    assert (out == l2_norm_out).all()
