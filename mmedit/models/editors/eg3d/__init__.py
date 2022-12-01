e173db7b7641a2e5af97df24f0ce6fbaf04dc2c7
LeoXing1996 <xingzn1996@hotmail.com>
2022-12-01 21:52:48 +0800

    revert init file for eg3d-disc

--- mmedit/models/editors/eg3d/__init__.py
+++ mmedit/models/editors/eg3d/__init__.py
@@ -1,10 +1,8 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .camera import GaussianCamera, UniformCamera
 from .dual_discriminator import DualDiscriminator
-from .eg3d import EG3D
 from .eg3d_generator import TriplaneGenerator
 
 __all__ = [
-    'DualDiscriminator', 'TriplaneGenerator', 'EG3D', 'UniformCamera',
-    'GaussianCamera'
+    'DualDiscriminator', 'TriplaneGenerator', 'UniformCamera', 'GaussianCamera'
 ]
