c994afd1ead0ac3488dd9faa7307a2d576bc361e
liuwenran <448073814@qq.com>
2022-12-06 17:17:57 +0800

    [sd] remove unrelated scheduler

--- mmedit/models/editors/stable_diffusion/__init__.py
+++ mmedit/models/editors/stable_diffusion/__init__.py
@@ -1,22 +1,6 @@
 from .configuration_utils import ConfigMixin
 from .models import AutoencoderKL, Transformer2DModel, UNet2DConditionModel, VQModel, StableDiffusionSafetyChecker
-from .schedulers import (
-    DDIMScheduler,
-    DDPMScheduler,
-    DPMSolverMultistepScheduler,
-    EulerAncestralDiscreteScheduler,
-    EulerDiscreteScheduler,
-    HeunDiscreteScheduler,
-    IPNDMScheduler,
-    KarrasVeScheduler,
-    PNDMScheduler,
-    RePaintScheduler,
-    SchedulerMixin,
-    ScoreSdeVeScheduler,
-    VQDiffusionScheduler,
-    LMSDiscreteScheduler,
-)
-
+from .schedulers import DDIMScheduler
 from .stable_diffuser import StableDiffuser
 
 __all__ = [
