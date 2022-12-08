9a4b66a63f895b3c1ca867175ed29a86d83ab7a8
zenggyh1900 <zengyh1900@gmail.com>
2022-12-08 20:37:33 +0800

    fix typo

--- docs/en/conf.py
+++ docs/en/conf.py
@@ -96,11 +96,6 @@ html_theme_options = {
                     'url': 'https://mmediting.readthedocs.io/en/1.x/',
                     'description': '1.x branch',
                 },
-                {
-                    'name': 'MMEditing 1.x',
-                    'url': 'https://mmediting.readthedocs.io/en/dev-1.x/',
-                    'description': 'docs at 1.x branch'
-                },
             ],
             'active':
             True,
@@ -108,16 +103,6 @@ html_theme_options = {
     ],
     'menu_lang':
     'en',
-    'header_note': {
-        'content':
-        'You are reading the documentation for MMEditing 0.x, which '
-        'will soon be deprecated by the end of 2022. We recommend you upgrade '
-        'to MMEditing 1.0 to enjoy fruitful new features and better performance '  # noqa
-        ' brought by OpenMMLab 2.0. Check out the '
-        '<a href="https://github.com/open-mmlab/mmediting/releases">changelog</a>, '  # noqa
-        '<a href="https://github.com/open-mmlab/mmediting/tree/1.x">code</a> '  # noqa
-        'and <a href="https://mmediting.readthedocs.io/en/1.x/">documentation</a> of MMEditing 1.0 for more details.',  # noqa
-    }
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
