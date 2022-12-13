065e2bdb27caa859a93b623483c26e5190c4d10f
plyfager <2744335995@qq.com>
2022-12-13 19:58:01 +0800

    add unit test

---
+++ tests/test_datasets/test_mscoco_dataset.py
@@ -0,0 +1,34 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import os
+from pathlib import Path
+
+
+from mmedit.datasets import MSCoCoDataset
+
+
+class TestMSCoCoDatasets:
+
+    @classmethod
+    def setup_class(cls):
+        cls.data_root = Path(__file__).parent.parent / 'data' / 'coco'
+
+    def test_mscoco(self):
+
+        # test basic usage
+        dataset = MSCoCoDataset(data_root=self.data_root, pipeline=[])
+        assert dataset[0] == dict(
+            gt_label="a good meal",
+            img_path=os.path.join(
+                self.data_root,
+                'train2014/COCO_train2014_000000000009.jpg'),
+            sample_idx=0)
+
+        # test with different phase
+        dataset = MSCoCoDataset(
+            data_root=self.data_root, phase="val", pipeline=[])
+        assert dataset[0] == dict(
+            gt_label="a pair of slippers",
+            img_path=os.path.join(
+                self.data_root,
+                'val2014/COCO_val2014_000000000042.jpg'),
+            sample_idx=0)
