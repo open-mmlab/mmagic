76fe11aad872818c2264d0ceea7afc6f078dd863
liuwenran <448073814@qq.com>
2022-12-16 15:10:24 +0800

    [sd] delete unused code.

--- mmedit/models/editors/ddpm/__init__.py
+++ mmedit/models/editors/ddpm/__init__.py
@@ -1,6 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .ddpm_scheduler import DDPMScheduler
 from .denoising_unet import DenoisingUnet
-from .unet_2d_condition import UNet2DConditionModel
 
-__all__ = ['DDPMScheduler', 'DenoisingUnet', 'UNet2DConditionModel']
+__all__ = ['DDPMScheduler', 'DenoisingUnet']
