30ab3b90ecc8ca7bf7f5c1e5b6783dd1843b4750
Yifei Yang <2744335995@qq.com>
2022-12-19 12:04:02 +0800

    Update mmedit/models/common/sr_backbone_utils.py

--- mmedit/models/common/sr_backbone_utils.py
+++ mmedit/models/common/sr_backbone_utils.py
@@ -59,7 +59,7 @@ class ResidualBlockNoBN(nn.Module):
     def __init__(self, mid_channels=64, res_scale=1.0, groups=1):
         super().__init__()
         self.res_scale = res_scale
-        self.conv1 = nn.Conv2d(mid_channels, mid_channels, 3, 1, 1, bias=True, groups=1)
+        self.conv1 = nn.Conv2d(mid_channels, mid_channels, 3, 1, 1, bias=True, groups= groups)
         self.conv2 = nn.Conv2d(mid_channels, mid_channels, 3, 1, 1, bias=True, groups=1)
 
         self.relu = nn.ReLU(inplace=True)
