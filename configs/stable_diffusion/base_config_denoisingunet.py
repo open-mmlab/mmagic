c1dc67d659d8c5fa5a1d5c4286ac51d9f46f55bb
liuwenran <448073814@qq.com>
2022-12-19 18:04:09 +0800

    [sd] format config

--- configs/stable_diffusion/base_config_denoisingunet.py
+++ configs/stable_diffusion/base_config_denoisingunet.py
@@ -1,13 +1,8 @@
 unet = dict(
-    type="DenoisingUnet",
+    type='DenoisingUnet',
     image_size=512,
     base_channels=320,
-    channels_cfg=[
-        1,
-        2,
-        4,
-        4
-    ],
+    channels_cfg=[1, 2, 4, 4],
     unet_type='stable',
     act_cfg=dict(type='silu', inplace=False),
     cross_attention_dim=768,
@@ -15,33 +10,21 @@ unet = dict(
     in_channels=4,
     layers_per_block=2,
     down_block_types=[
-        "CrossAttnDownBlock2D",
-        "CrossAttnDownBlock2D",
-        "CrossAttnDownBlock2D",
-        "DownBlock2D"
+        'CrossAttnDownBlock2D', 'CrossAttnDownBlock2D', 'CrossAttnDownBlock2D',
+        'DownBlock2D'
     ],
     up_block_types=[
-        "UpBlock2D",
-        "CrossAttnUpBlock2D",
-        "CrossAttnUpBlock2D",
-        "CrossAttnUpBlock2D"
+        'UpBlock2D', 'CrossAttnUpBlock2D', 'CrossAttnUpBlock2D',
+        'CrossAttnUpBlock2D'
     ],
-    output_cfg=dict(var='fixed')
-)
+    output_cfg=dict(var='fixed'))
 
 vae = dict(
-    act_fn="silu",
-    block_out_channels=[
-        128,
-        256,
-        512,
-        512
-    ],
+    act_fn='silu',
+    block_out_channels=[128, 256, 512, 512],
     down_block_types=[
-        "DownEncoderBlock2D",
-        "DownEncoderBlock2D",
-        "DownEncoderBlock2D",
-        "DownEncoderBlock2D"
+        'DownEncoderBlock2D', 'DownEncoderBlock2D', 'DownEncoderBlock2D',
+        'DownEncoderBlock2D'
     ],
     in_channels=3,
     latent_channels=4,
@@ -50,57 +33,50 @@ vae = dict(
     out_channels=3,
     sample_size=512,
     up_block_types=[
-        "UpDecoderBlock2D",
-        "UpDecoderBlock2D",
-        "UpDecoderBlock2D",
-        "UpDecoderBlock2D"
-    ]
-)
+        'UpDecoderBlock2D', 'UpDecoderBlock2D', 'UpDecoderBlock2D',
+        'UpDecoderBlock2D'
+    ])
 
 diffusion_scheduler = dict(
     type='DDIMScheduler',
     variance_type='learned_range',
     beta_end=0.012,
-    beta_schedule="scaled_linear",
+    beta_schedule='scaled_linear',
     beta_start=0.00085,
     num_train_timesteps=1000,
     set_alpha_to_one=False,
-    clip_sample=False
-)
+    clip_sample=False)
 
 tokenizer_path = dict(
     subdir_name='tokenizer',
     merges='merges.txt',
     special_tokens_map='special_tokens_map.json',
     tokenizer_config='tokenizer_config.json',
-    vocab='vocab.json'
-)
+    vocab='vocab.json')
 
 text_encoder_path = dict(
     subdir_name='text_encoder',
     config='config.json',
-    pytorch_model='pytorch_model.bin'
-)
+    pytorch_model='pytorch_model.bin')
 
 feature_extractor_path = dict(
-    subdir_name='feature_extractor',
-    config='preprocessor_config.json'
-)
+    subdir_name='feature_extractor', config='preprocessor_config.json')
 
 safety_checker_path = dict(
     subdir_name='safety_checker',
     config='config.json',
-    pytorch_model='pytorch_model.bin'
-)
+    pytorch_model='pytorch_model.bin')
 
+# yapf: disable
 pretrained_ckpt_path = dict(
-    unet='/nvme/liuwenran/repos/diffusers/resources/stable-diffusion-v1-5/unet/diffusion_pytorch_model.bin',
-    vae='/nvme/liuwenran/repos/diffusers/resources/stable-diffusion-v1-5/vae/diffusion_pytorch_model.bin',
+    unet='/nvme/liuwenran/repos/diffusers/resources/stable-diffusion-v1-5/unet/diffusion_pytorch_model.bin',  # noqa
+    vae='/nvme/liuwenran/repos/diffusers/resources/stable-diffusion-v1-5/vae/diffusion_pytorch_model.bin',  # noqa
     tokenizer=tokenizer_path,
     text_encoder=text_encoder_path,
     feature_extractor=feature_extractor_path,
     safety_checker=safety_checker_path
-)
+    )
+# yapf: enable
 
 model = dict(
     type='StableDiffuser',
