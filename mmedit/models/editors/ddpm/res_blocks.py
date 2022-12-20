5bef4e1768ee1a2b808919796d8697144168dfa6
liuwenran <448073814@qq.com>
2022-12-20 15:45:18 +0800

    [sd] res_block.py add docstring.

--- mmedit/models/editors/ddpm/res_blocks.py
+++ mmedit/models/editors/ddpm/res_blocks.py
@@ -1,16 +1,32 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from functools import partial
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 
 class ResnetBlock2D(nn.Module):
+    """resnet block support down sample and up sample.
+
+    Args:
+        in_channels (int): input channels.
+        out_channels (int): output channels.
+        conv_shortcut (bool): whether to use conv shortcut.
+        dropout (float): dropout rate.
+        temb_channels (int): time embedding channels.
+        groups (int): conv groups.
+        groups_out (int): conv out groups.
+        pre_norm (bool): whether to norm before conv. Todo: remove.
+        eps (float): eps for groupnorm.
+        non_linearity (str): non linearity type.
+        time_embedding_norm (str): time embedding norm type.
+        output_scale_factor (float): factor to scale input and output.
+        use_in_shortcut (bool): whether to use conv in shortcut.
+        up (bool): whether to upsample.
+        down (bool): whether to downsample.
+    """
 
     def __init__(
         self,
-        *,
         in_channels,
         out_channels=None,
         conv_shortcut=False,
@@ -22,7 +38,6 @@ class ResnetBlock2D(nn.Module):
         eps=1e-6,
         non_linearity='swish',
         time_embedding_norm='default',
-        kernel=None,
         output_scale_factor=1.0,
         use_in_shortcut=None,
         up=False,
@@ -83,6 +98,7 @@ class ResnetBlock2D(nn.Module):
                 in_channels, out_channels, kernel_size=1, stride=1, padding=0)
 
     def forward(self, input_tensor, temb):
+        """forward with hidden states and time embeddings."""
         hidden_states = input_tensor
 
         hidden_states = self.norm1(hidden_states)
@@ -125,11 +141,11 @@ class ResnetBlock2D(nn.Module):
 class Upsample2D(nn.Module):
     """An upsampling layer with an optional convolution.
 
-    Parameters:
-        channels: channels in the inputs and outputs.
-        use_conv: a bool determining if a convolution is applied.
-        use_conv_transpose:
-        out_channels:
+    Args:
+        channels (int): channels in the inputs and outputs.
+        use_conv (bool): a bool determining if a convolution is applied.
+        use_conv_transpose (bool): whether to use conv transpose.
+        out_channels (int): output channels.
     """
 
     def __init__(self,
@@ -159,6 +175,7 @@ class Upsample2D(nn.Module):
             self.Conv2d_0 = conv
 
     def forward(self, hidden_states, output_size=None):
+        """forward with hidden states."""
         assert hidden_states.shape[1] == self.channels
 
         if self.use_conv_transpose:
@@ -204,11 +221,11 @@ class Upsample2D(nn.Module):
 class Downsample2D(nn.Module):
     """A downsampling layer with an optional convolution.
 
-    Parameters:
-        channels: channels in the inputs and outputs.
-        use_conv: a bool determining if a convolution is applied.
-        out_channels:
-        padding:
+    Args:
+        channels (int): channels in the inputs and outputs.
+        use_conv (bool): a bool determining if a convolution is applied.
+        out_channels (int): output channels
+        padding (int): padding num
     """
 
     def __init__(self,
@@ -247,6 +264,7 @@ class Downsample2D(nn.Module):
             self.conv = conv
 
     def forward(self, hidden_states):
+        """forward with hidden states."""
         assert hidden_states.shape[1] == self.channels
         if self.use_conv and self.padding == 0:
             pad = (0, 1, 0, 1)
@@ -256,4 +274,3 @@ class Downsample2D(nn.Module):
         hidden_states = self.conv(hidden_states)
 
         return hidden_states
-
