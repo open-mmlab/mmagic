1de497eb48e85d3c42ac2eaa27ecb619f38c1725
liuwenran <448073814@qq.com>
2022-12-22 15:46:31 +0800

    [sd] add vae ut.

---
+++ tests/test_models/test_editors/test_stable_diffusion/test_vae.py
@@ -0,0 +1,14 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import torch
+from mmedit.models.editors.stable_diffusion.vae import AutoencoderKL
+
+
+def test_vae():
+    input = torch.rand((1, 3, 256, 256))
+    vae = AutoencoderKL()
+    output = vae.forward(input)
+    assert output['sample'].shape == (1, 3, 256, 256)
+
+
+if __name__ == '__main__':
+    test_vae()
