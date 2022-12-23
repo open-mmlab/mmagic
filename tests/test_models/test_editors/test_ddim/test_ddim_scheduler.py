b10a0d0fe97a7bc096ef15160aec94ae410b09d6
liuwenran <448073814@qq.com>
2022-12-23 15:28:23 +0800

    [sd] ddim ut

--- tests/test_models/test_editors/test_ddim/test_ddim_scheduler.py
+++ tests/test_models/test_editors/test_ddim/test_ddim_scheduler.py
@@ -1,4 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import pytest
 import torch
 
 from mmedit.models.editors.ddim.ddim_scheduler import DDIMScheduler
@@ -12,6 +13,23 @@ def test_ddim():
     ddim.set_timesteps(10)
     result = ddim.step(modelout, 980, sample)
     assert result['prev_sample'].shape == (1, 4, 32, 32)
+
+    noise = torch.rand((1, 4, 32, 32))
+    result = ddim.add_noise(sample, noise, 10)
+    assert result.shape == (1, 4, 32, 32)
+
+
+def test_ddim_init():
+    ddim = DDIMScheduler(
+        num_train_timesteps=1000, beta_schedule='scaled_linear')
+
+    ddim = DDIMScheduler(
+        num_train_timesteps=1000, beta_schedule='squaredcos_cap_v2')
+
+    assert isinstance(ddim, DDIMScheduler)
+
+    with pytest.raises(Exception):
+        DDIMScheduler(num_train_timesteps=1000, beta_schedule='fake')
 
 
 if __name__ == '__main__':
