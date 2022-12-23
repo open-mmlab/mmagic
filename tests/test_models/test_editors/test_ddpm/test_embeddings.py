b92b6c681c2d57b378187845130c4cf06156fb6f
liuwenran <448073814@qq.com>
2022-12-23 11:59:07 +0800

    [sd] add embeddings ut.

---
+++ tests/test_models/test_editors/test_ddpm/test_embeddings.py
@@ -0,0 +1,23 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import torch
+
+from mmedit.models.editors.ddpm.embeddings import (TimestepEmbedding,
+                                                   get_timestep_embedding)
+
+
+def test_TimestepEmbedding():
+    input = torch.rand((1, 64, 16))
+    timestep_emb = TimestepEmbedding(
+        in_channels=16, time_embed_dim=16, act_fn='mish')
+    output = timestep_emb.forward(input)
+    assert output.shape == (1, 64, 16)
+
+
+def test_get_timestep_embedding():
+    input = torch.tensor([4])
+    emb = get_timestep_embedding(input, embedding_dim=9)
+    assert emb.shape == (1, 9)
+
+
+if __name__ == '__main__':
+    test_get_timestep_embedding()
