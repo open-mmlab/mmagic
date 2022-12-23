bab63381a449ed59b4538c8ef0cc204be5c003cc
liuwenran <448073814@qq.com>
2022-12-23 14:26:22 +0800

    [sd] add denoising unet ut.

--- tests/test_models/test_editors/test_ddpm/test_denoising_unet.py
+++ tests/test_models/test_editors/test_ddpm/test_denoising_unet.py
@@ -1,1 +1,15 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import torch
+
+from mmedit.models.editors.ddpm.denoising_unet import DenoisingUnet
+
+
+def test_DenoisingUnet():
+    input = torch.rand((1, 3, 32, 32))
+    unet = DenoisingUnet(32)
+    output = unet.forward(input, 10)
+    assert output['outputs'].shape == (1, 6, 32, 32)
+
+
+if __name__ == '__main__':
+    test_DenoisingUnet()
