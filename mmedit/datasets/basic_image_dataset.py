dcca5386f080b8aef8d8abd131927b2d6623eafb
Z-Fran <1396925302@qq.com>
2023-01-03 18:47:14 +0800

    fix self.backend_args

--- mmedit/datasets/basic_image_dataset.py
+++ mmedit/datasets/basic_image_dataset.py
@@ -154,7 +154,10 @@ class BasicImageDataset(BaseDataset):
         self.search_key = search_key
         self.filename_tmpl = filename_tmpl
         self.use_ann_file = (ann_file != '')
-        self.backend_args = backend_args
+        if backend_args is None:
+            self.backend_args = None
+        else:
+            self.backend_args = backend_args.copy()
         self.img_suffix = img_suffix
         self.recursive = recursive
         self.file_backend = get_file_backend(
