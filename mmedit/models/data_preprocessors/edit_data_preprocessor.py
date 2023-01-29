3793e504afc64e8190a05c52212aa03cc5380e12
LeoXing1996 <xingzn1996@hotmail.com>
2023-01-29 21:05:53 +0800

    revise color order conversion to support get input channel order from metafile and handle single channel images

--- mmedit/models/data_preprocessors/edit_data_preprocessor.py
+++ mmedit/models/data_preprocessors/edit_data_preprocessor.py
@@ -53,33 +53,37 @@ class EditDataPreprocessor(ImgDataPreprocessor):
     _NON_IMAGE_KEYS = ['noise']
     _NON_CONCENTATE_KEYS = ['num_batches', 'mode', 'sample_kwargs', 'eq_cfg']
 
-    def __init__(self,
-                 mean: Sequence[Union[float, int]] = (127.5, 127.5, 127.5),
-                 std: Sequence[Union[float, int]] = (127.5, 127.5, 127.5),
-                 pad_size_divisor: int = 1,
-                 pad_value: Union[float, int] = 0,
-                 bgr_to_rgb: bool = False,
-                 rgb_to_bgr: bool = False,
-                 non_image_keys: Optional[Tuple[str, List[str]]] = None,
-                 non_concentate_keys: Optional[Tuple[str, List[str]]] = None,
-                 pad_mode: str = 'constant',
-                 norm_data_samples_in_testing: bool = False,
-                 data_keys: Union[List[str], str] = 'gt_img',
-                 input_view: Optional[tuple] = None,
-                 output_view: Optional[tuple] = None):
+    def __init__(
+            self,
+            mean: Sequence[Union[float, int]] = (127.5, 127.5, 127.5),
+            std: Sequence[Union[float, int]] = (127.5, 127.5, 127.5),
+            pad_size_divisor: int = 1,
+            pad_value: Union[float, int] = 0,
+            bgr_to_rgb: bool = False,
+            rgb_to_bgr: bool = False,
+            non_image_keys: Optional[Tuple[str, List[str]]] = None,
+            non_concentate_keys: Optional[Tuple[str, List[str]]] = None,
+            pad_mode: str = 'constant',
+            norm_data_samples_in_testing: bool = False,
+            #  input_keys: Union[List[str], str] = 'inputs',
+            output_channel_order: str = 'unchanged',
+            data_keys: Union[List[str], str] = 'gt_img',
+            input_view: Optional[tuple] = None,
+            output_view: Optional[tuple] = None):
 
         super().__init__(mean, std, pad_size_divisor, pad_value, bgr_to_rgb,
                          rgb_to_bgr)
         # get color order
-        if bgr_to_rgb:
-            input_color_order, output_color_order = 'bgr', 'rgb'
-        elif rgb_to_bgr:
-            input_color_order, output_color_order = 'rgb', 'bgr'
-        else:
-            # 'bgr' order as default
-            input_color_order = output_color_order = 'bgr'
-        self.input_color_order = input_color_order
-        self.output_color_order = output_color_order
+        # if bgr_to_rgb:
+        #     input_color_order, output_color_order = 'bgr', 'rgb'
+        # elif rgb_to_bgr:
+        #     input_color_order, output_color_order = 'rgb', 'bgr'
+        # else:
+        #     # 'bgr' order as default
+        #     input_color_order = output_color_order = 'bgr'
+        # self.input_color_order = input_color_order
+        # self.output_color_order = output_color_order
+        self.output_color_order = output_channel_order
 
         # add user defined keys
         if non_image_keys is not None:
@@ -94,12 +98,15 @@ class EditDataPreprocessor(ImgDataPreprocessor):
         self.pad_mode = pad_mode
         self.pad_size_dict = dict()
         self.norm_data_samples_in_testing = norm_data_samples_in_testing
+        # self.input_keys = input_keys if isinstance(input_keys,
+        #                                            list) else [input_keys]
         self.data_keys = data_keys if isinstance(data_keys,
                                                  list) else [data_keys]
         self.input_view = input_view
         self.output_view = output_view
 
         self._done_padding = False  # flag for padding checking
+        self._conversion_warning_raised = False  # flag for conversion warning
 
     def cast_data(self, data: CastData) -> CastData:
         """Copying data to the target device.
@@ -113,6 +120,62 @@ class EditDataPreprocessor(ImgDataPreprocessor):
         if isinstance(data, (str, int, float)):
             return data
         return super().cast_data(data)
+
+    def _do_conversion(self,
+                       inputs: Tensor,
+                       inputs_order: str = 'bgr',
+                       target_order: Optional[str] = None
+                       ) -> Tuple[Tensor, str]:
+        """return converted inputs and order after conversion."""
+        # inputs: [CHW] or [NCHW]
+        target_order = self.output_color_order \
+            if target_order is None else target_order
+
+        if target_order == 'unchanged' or inputs_order == target_order:
+            return inputs, target_order
+
+        channel_index_mapping = {3: 0, 4: 1, 5: 2}
+        assert inputs.ndim in channel_index_mapping, (
+            'Only support (C, H, W), (N, C, H, W) or (N, t, C, H, W) '
+            f'inputs. But received \'({inputs.shape})\'.')
+        channel_index = channel_index_mapping[inputs.ndim]
+
+        if inputs.shape[channel_index] == 1:
+            if not self._conversion_warning_raised:
+                print_log(
+                    'Cannot convert single channel input to '
+                    f'\'output_color_order\'({self.output_color_order})',
+                    'current', WARNING)
+                self._conversion_warning_raised = True
+            return inputs, 'unchanged'
+
+        if inputs.shape[channel_index] == 4:
+            new_index = [2, 1, 0, 3]
+        else:
+            new_index = [2, 1, 0]
+
+        # do conversion
+        inputs = torch.index_select(
+            inputs, channel_index,
+            torch.IntTensor(new_index).to(inputs.device))
+
+        return inputs, target_order
+
+    def _do_norm(self,
+                 inputs: Tensor,
+                 do_norm: Optional[bool] = None) -> Tensor:
+
+        do_norm = self._enable_normalize if do_norm is None else do_norm
+
+        if do_norm:
+            if self.input_view is None:
+                target_shape = [1 for _ in range(inputs.ndim - 3)] + [-1, 1, 1]
+            else:
+                target_shape = self.input_view
+            mean = self.mean.view(target_shape)
+            std = self.std.view(target_shape)
+            inputs = (inputs - mean) / std
+        return inputs
 
     def _norm_and_conversion(self,
                              inputs: Tensor,
@@ -159,7 +222,8 @@ class EditDataPreprocessor(ImgDataPreprocessor):
             inputs = (inputs - mean) / std
         return inputs
 
-    def _preprocess_image_tensor(self, inputs: Tensor) -> Tensor:
+    def _preprocess_image_tensor(self, inputs: Tensor,
+                                 channel_order: str) -> Tensor:
         """Process image tensor.
 
         Args:
@@ -172,7 +236,9 @@ class EditDataPreprocessor(ImgDataPreprocessor):
             'The input of `_preprocess_image_tensor` should be a NCHW '
             'tensor or a list of tensor, but got a tensor with shape: '
             f'{inputs.shape}')
-        inputs = self._norm_and_conversion(inputs)
+        # inputs = self._norm_and_conversion(inputs)
+        inputs, _ = self._do_conversion(inputs, channel_order)
+        inputs = self._do_norm(inputs)
         h, w = inputs.shape[2:]
         target_h = math.ceil(h / self.pad_size_divisor) * self.pad_size_divisor
         target_w = math.ceil(w / self.pad_size_divisor) * self.pad_size_divisor
@@ -183,8 +249,8 @@ class EditDataPreprocessor(ImgDataPreprocessor):
 
         return batch_inputs
 
-    def _preprocess_image_list(self, tensor_list: List[Tensor]
-                               ) -> Tuple[Tensor, Tensor]:
+    def _preprocess_image_list(self, tensor_list: List[Tensor],
+                               channel_order: str) -> Tuple[Tensor, Tensor]:
         """Preprocess a list of images.
 
         Returns:
@@ -207,7 +273,10 @@ class EditDataPreprocessor(ImgDataPreprocessor):
         padding_sizes[:, :-2] = 0
         if padding_sizes.sum() == 0:
             stacked_tensor = torch.stack(tensor_list)
-            stacked_tensor = self._norm_and_conversion(stacked_tensor)
+            # stacked_tensor = self._norm_and_conversion(stacked_tensor)
+            stacked_tensor, _ = self._do_conversion(stacked_tensor,
+                                                    channel_order)
+            stacked_tensor = self._do_norm(stacked_tensor)
             return stacked_tensor, padding_sizes
 
         # `pad` is the second arguments of `F.pad`. If pad is (1, 2, 3, 4),
@@ -224,10 +293,13 @@ class EditDataPreprocessor(ImgDataPreprocessor):
                                  self.pad_mode, self.pad_value)
             batch_tensor.append(paded_tensor)
         stacked_tensor = torch.stack(batch_tensor)
-        stacked_tensor = self._norm_and_conversion(stacked_tensor)
+        # stacked_tensor = self._norm_and_conversion(stacked_tensor)
+        stacked_tensor, _ = self._do_conversion(stacked_tensor, channel_order)
+        stacked_tensor = self._do_norm(stacked_tensor)
         return stacked_tensor, padding_sizes
 
-    def _preprocess_dict_inputs(self, batch_inputs: dict) -> dict:
+    def _preprocess_dict_inputs(self, batch_inputs: dict,
+                                channel_order) -> dict:
         """Preprocess dict type inputs.
 
         Args:
@@ -262,7 +334,7 @@ class EditDataPreprocessor(ImgDataPreprocessor):
                     batch_inputs[k] = inputs
             elif isinstance(inputs, Tensor) and k not in self._NON_IMAGE_KEYS:
                 batch_inputs[k], padding_sizes = self._preprocess_image_tensor(
-                    inputs)
+                    inputs, channel_order)
                 pad_size_dict[k] = [size for size in padding_sizes]
 
         # NOTE: we only support all key shares the same padding size
@@ -299,12 +371,16 @@ class EditDataPreprocessor(ImgDataPreprocessor):
             list: The list of processed data samples.
         """
         if not training:
-            # conversion to BGR
-            do_conversion = self.output_color_order == 'RGB'
+            # conversion to BGR --> TODO: output color order may wrong
+            if self.output_color_order != 'unchanged':
+                target_order = 'BGR'
+            else:
+                target_order = 'unchanged'
             do_norm = self.norm_data_samples_in_testing
         else:
-            # norm and conversion in training
-            do_norm, do_conversion = True, True
+            # norm in training, conversion as default (None)
+            target_order, do_norm = self.output_color_order, True
+            # do_norm, do_conversion = True, None
 
         for data_sample in data_samples:
             for key in self.data_keys:
@@ -314,14 +390,25 @@ class EditDataPreprocessor(ImgDataPreprocessor):
                               'current', WARNING)
                     break
 
-                data_sample.set_data({
-                    f'{key}':
-                    self._norm_and_conversion(
-                        data_sample.get(key), do_norm, do_conversion)
-                })
+                data = data_sample.get(key)
+                # data_channel_order = data_sample.get_metainfo(
+                #     f'{key}_color_order', None)
+                data_channel_order = data_sample.metainfo.get(
+                    f'{key}_color_order', None)
+                data, color_order = self._do_conversion(
+                    data, data_channel_order, target_order)
+                data = self._do_norm(data, do_norm)
+                data_sample.set_data({f'{key}': data})
+                # data_sample.set_data({
+                #     f'{key}':
+                #     self._norm_and_conversion(
+                #         data_sample.get(key), do_norm, do_conversion)
+                # })
                 data_process_meta = {
                     f'{key}_enable_norm': self._enable_normalize,
-                    f'{key}_enable_conversion': self._channel_conversion,
+                    # f'{key}_enable_conversion': self._channel_conversion,
+                    # save real order
+                    f'{key}_output_color_order': color_order,
                     f'{key}_mean': self.mean,
                     f'{key}_std': self.std
                 }
@@ -345,23 +432,32 @@ class EditDataPreprocessor(ImgDataPreprocessor):
         _batch_inputs = data['inputs']
         _batch_data_samples = data.get('data_samples', None)
 
+        # ops, for translation model?
+        if _batch_data_samples is not None:
+            inputs_channel_order = _batch_data_samples[0].get(
+                'img_channel_order', 'bgr')
+        else:
+            inputs_channel_order = None
+
         # process input
         if isinstance(_batch_inputs, torch.Tensor):
-            _batch_inputs = self._preprocess_image_tensor(_batch_inputs)
+            _batch_inputs = self._preprocess_image_tensor(
+                _batch_inputs, inputs_channel_order)
             _batch_inputs = {'img': _batch_inputs}  # tensor -> dict
             _pad_info = None
         elif is_list_of(_batch_inputs, torch.Tensor):
             _batch_inputs, _pad_info = self._preprocess_image_list(
-                _batch_inputs)
+                _batch_inputs, inputs_channel_order)
             # _batch_inputs = _batch_inputs
         elif isinstance(_batch_inputs, dict):
             _batch_inputs, _pad_info = self._preprocess_dict_inputs(
-                _batch_inputs)
+                _batch_inputs, inputs_channel_order)
         elif is_list_of(_batch_inputs, dict):
             # convert list of dict to dict of list
             keys = _batch_inputs[0].keys()
             dict_input = {k: [inp[k] for inp in _batch_inputs] for k in keys}
-            _batch_inputs, _pad_info = self._preprocess_dict_inputs(dict_input)
+            _batch_inputs, _pad_info = self._preprocess_dict_inputs(
+                dict_input, inputs_channel_order)
         else:
             raise ValueError('Only support following inputs types: '
                              '\'torch.Tensor\', \'List[torch.Tensor]\', '
@@ -407,11 +503,19 @@ class EditDataPreprocessor(ImgDataPreprocessor):
     def _destruct_tensor_norm_and_conversion(self,
                                              batch_tensor: Tensor) -> Tensor:
 
-        if self.output_color_order == 'RGB':
-            if batch_tensor.ndim == 4:
-                batch_tensor = batch_tensor[..., [2, 1, 0]]
-            else:
-                batch_tensor = batch_tensor[[2, 1, 0], ...]
+        # single channel -> return
+        # 3 channel -> check conversion
+        # if self.output_color_order == 'RGB':
+        #     if batch_tensor.ndim == 4:
+        #         batch_tensor = batch_tensor[..., [2, 1, 0]]
+        #     else:
+        #         batch_tensor = batch_tensor[[2, 1, 0], ...]
+
+        # convert output to 'BGR' if able
+        inputs_order = 'bgr' if self.output_color_order is None \
+            else self.output_color_order
+        batch_tensor, _ = self._do_conversion(
+            batch_tensor, inputs_order=inputs_order, target_order='bgr')
 
         if self._enable_normalize:
             if self.output_view is None:
