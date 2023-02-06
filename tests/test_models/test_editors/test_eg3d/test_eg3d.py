052498e348a37f3663300456a4320ea2e9ba89dd
LeoXing1996 <xingzn1996@hotmail.com>
2023-02-06 20:57:02 +0800

    adopt eg3d to new data preprocessor

--- tests/test_models/test_editors/test_eg3d/test_eg3d.py
+++ tests/test_models/test_editors/test_eg3d/test_eg3d.py
@@ -86,8 +86,8 @@ class TestEG3D(TestCase):
         ]
         # NOTE: fake_img and lr_img are PixelData, shape do not contains
         # the number of channel
-        target_shape = [(out_size, out_size), (1, n_points, n_points),
-                        (n_points, n_points), (n_points**2, 3),
+        target_shape = [(3, out_size, out_size), (1, n_points, n_points),
+                        (3, n_points, n_points), (n_points**2, 3),
                         (n_points**2, 3)]
         for output in outputs:
             for key, shape in zip(target_keys, target_shape):
