5bc9a69b2c0567ebaafad5fe6e3afef1bad04493
zengyh1900 <zengyh1900@gmail.com>
2023-03-02 19:20:46 +0800

    register diffusers_scheduler

--- mmedit/models/__init__.py
+++ mmedit/models/__init__.py
@@ -3,11 +3,13 @@ from .base_models import (BaseConditionalGAN, BaseEditModel, BaseGAN,
                           BaseMattor, BaseTranslationModel, BasicInterpolator,
                           ExponentialMovingAverage)
 from .data_preprocessors import EditDataPreprocessor, MattorPreprocessor
+from .diffusion_schedulers import EditDDIMScheduler, EditDDPMScheduler
 from .editors import *  # noqa: F401, F403
 from .losses import *  # noqa: F401, F403
 
 __all__ = [
     'BaseGAN', 'BaseTranslationModel', 'BaseEditModel', 'MattorPreprocessor',
     'EditDataPreprocessor', 'BasicInterpolator', 'BaseMattor',
-    'BasicInterpolator', 'ExponentialMovingAverage', 'BaseConditionalGAN'
+    'BasicInterpolator', 'ExponentialMovingAverage', 'BaseConditionalGAN',
+    'EditDDPMScheduler', 'EditDDIMScheduler'
 ]
---
+++ mmedit/models/diffusion_schedulers/__init__.py
@@ -0,0 +1,34 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .ddim_scheduler import EditDDIMScheduler
+from .ddpm_scheduler import EditDDPMScheduler
+
+__all__ = ['EditDDIMScheduler', 'EditDDPMScheduler']
+
+import inspect
+from typing import List
+
+import diffusers
+
+from mmedit.registry import DIFFUSION_SCHEDULERS
+
+
+def register_diffusers_schedulers() -> List[str]:
+    """Register schedulers in ``diffusers.schedulers`` to the
+    ``DIFFUSION_SCHEDULERS`` registry.
+
+    Returns:
+        List[str]: A list of registered DIFFUSION_SCHEDULERS' name.
+    """
+    DIFFUSERS_SCHEDULERS = []
+    for module_name in dir(diffusers.schedulers):
+        if module_name.startswith('Flax'):
+            continue
+        elif module_name.endswith('Scheduler'):
+            _scheduler = getattr(diffusers.schedulers, module_name)
+            if inspect.isclass(_scheduler):
+                DIFFUSION_SCHEDULERS.register_module(module=_scheduler)
+                DIFFUSERS_SCHEDULERS.append(module_name)
+    return DIFFUSERS_SCHEDULERS
+
+
+DIFFUSERS_SCHEDULERS = register_diffusers_schedulers()
