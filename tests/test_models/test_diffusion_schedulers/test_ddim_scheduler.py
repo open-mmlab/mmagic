4ee329e12ed3275430f6f30e01fd8884d0b45935
zengyh1900 <zengyh1900@gmail.com>
2023-03-09 14:45:33 +0800

    -

--- tests/test_models/test_diffusion_schedulers/test_ddim_scheduler.py
+++ tests/test_models/test_diffusion_schedulers/test_ddim_scheduler.py
@@ -2,13 +2,13 @@
 import pytest
 import torch
 
-from mmedit.models.diffusion_schedulers.ddim_scheduler import DDIMScheduler
+from mmedit.models.diffusion_schedulers.ddim_scheduler import EditDDIMScheduler
 
 
 def test_ddim():
     modelout = torch.rand((1, 8, 32, 32))
     sample = torch.rand((1, 4, 32, 32))
-    ddim = DDIMScheduler(
+    ddim = EditDDIMScheduler(
         num_train_timesteps=1000, variance_type='learned_range')
     ddim.set_timesteps(10)
     result = ddim.step(modelout, 980, sample)
@@ -22,22 +22,22 @@ def test_ddim():
 
 
 def test_ddim_init():
-    ddim = DDIMScheduler(
+    ddim = EditDDIMScheduler(
         num_train_timesteps=1000, beta_schedule='scaled_linear')
 
-    ddim = DDIMScheduler(
+    ddim = EditDDIMScheduler(
         num_train_timesteps=1000, beta_schedule='squaredcos_cap_v2')
 
-    assert isinstance(ddim, DDIMScheduler)
+    assert isinstance(ddim, EditDDIMScheduler)
 
     with pytest.raises(Exception):
-        DDIMScheduler(num_train_timesteps=1000, beta_schedule='fake')
+        EditDDIMScheduler(num_train_timesteps=1000, beta_schedule='fake')
 
 
 def test_ddim_step():
     modelout = torch.rand((1, 8, 32, 32))
     sample = torch.rand((1, 4, 32, 32))
-    ddim = DDIMScheduler(
+    ddim = EditDDIMScheduler(
         num_train_timesteps=1000, variance_type='learned_range')
     with pytest.raises(Exception):
         ddim.step(modelout, 980, sample)
