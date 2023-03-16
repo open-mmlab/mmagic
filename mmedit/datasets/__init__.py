8415c2bf46a54fe3640a2fa94b6ae2300c723fde
LeoXing1996 <xingzn1996@hotmail.com>
2023-03-16 15:33:07 +0800

    support demo dataset for controlnet

--- mmedit/datasets/__init__.py
+++ mmedit/datasets/__init__.py
@@ -4,6 +4,7 @@ from .basic_frames_dataset import BasicFramesDataset
 from .basic_image_dataset import BasicImageDataset
 from .cifar10_dataset import CIFAR10
 from .comp1k_dataset import AdobeComp1kDataset
+from .controlnet_dataset import ControlNetDataset
 from .grow_scale_image_dataset import GrowScaleImgDataset
 from .imagenet_dataset import ImageNet
 from .mscoco_dataset import MSCoCoDataset
@@ -15,5 +16,5 @@ __all__ = [
     'AdobeComp1kDataset', 'BasicImageDataset', 'BasicFramesDataset',
     'BasicConditionalDataset', 'UnpairedImageDataset', 'PairedImageDataset',
     'ImageNet', 'CIFAR10', 'GrowScaleImgDataset', 'SinGANDataset',
-    'MSCoCoDataset'
+    'MSCoCoDataset', 'ControlNetDataset'
 ]
