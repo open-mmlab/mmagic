0f2319f927a325edd9d5e701cba83092fe07ac3b
liuwenran <448073814@qq.com>
2023-03-24 20:24:06 +0800

    fix lint

--- demo/gradio_controlnet_animation.py
+++ demo/gradio_controlnet_animation.py
@@ -1,4 +1,6 @@
+# Copyright (c) OpenMMLab. All rights reserved.
 import gradio as gr
+
 from mmedit.edit import MMEdit
 
 
@@ -21,33 +23,35 @@ def process(video, prompt, a_prompt, negative_prompt,
 block = gr.Blocks().queue()
 with block:
     with gr.Row():
-        gr.Markdown("## Controlnet Animation")
+        gr.Markdown('## Controlnet Animation')
     with gr.Row():
         with gr.Column():
-            video_inp = gr.Video(label="Video source",
-                                 source="upload",
-                                 type="filepath",
-                                 elem_id="input-vid")
-            prompt = gr.Textbox(label="Prompt")
-            run_button = gr.Button(label="Run")
-            with gr.Accordion("Advanced options", open=False):
-                image_resolution = gr.Slider(label="Image Resolution",
-                                             minimum=256,
-                                             maximum=768,
-                                             value=512,
-                                             step=64)
-                a_prompt = gr.Textbox(label="Added Prompt",
-                                      value='best quality, extremely detailed')
+            video_inp = gr.Video(
+                label='Video source',
+                source='upload',
+                type='filepath',
+                elem_id='input-vid')
+            prompt = gr.Textbox(label='Prompt')
+            run_button = gr.Button(label='Run')
+            with gr.Accordion('Advanced options', open=False):
+                image_resolution = gr.Slider(
+                    label='Image Resolution',
+                    minimum=256,
+                    maximum=768,
+                    value=512,
+                    step=64)
+                a_prompt = gr.Textbox(
+                    label='Added Prompt',
+                    value='best quality, extremely detailed')
                 n_prompt = gr.Textbox(
-                    label="Negative Prompt",
+                    label='Negative Prompt',
                     value='longbody, lowres, bad anatomy, bad hands, ' +
-                          'missing fingers, extra digit, fewer digits, '
-                          'cropped, worst quality, low quality')
+                    'missing fingers, extra digit, fewer digits, '
+                    'cropped, worst quality, low quality')
         with gr.Column():
-            video_out = gr.Video(label="ControlNet video result",
-                                 elem_id="video-output")
+            video_out = gr.Video(
+                label='ControlNet video result', elem_id='video-output')
     ips = [video_inp, prompt, a_prompt, n_prompt, image_resolution]
     run_button.click(fn=process, inputs=ips, outputs=video_out)
 
-
-block.launch(server_name='0.0.0.0')
+block.launch(server_name='0.0.0.0', share=True)
