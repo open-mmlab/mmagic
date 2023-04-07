eebdcd86498fa849bf86734d8d4fedc472c31074
liuwenran <448073814@qq.com>
2023-04-07 19:58:21 +0800

    implement with our controlnet

--- configs/controlnet_animation/anythingv3_config.py
+++ configs/controlnet_animation/anythingv3_config.py
@@ -1,5 +1,35 @@
+# config for model
+stable_diffusion_v15_url = 'Linaqruf/anything-v3.0'
+controlnet_hed_url = 'lllyasviel/sd-controlnet-hed'
 control_detector = 'lllyasviel/ControlNet'
 
-controlnet_model = 'lllyasviel/sd-controlnet-hed'
-
-stable_diffusion_model = 'Linaqruf/anything-v3.0'
+model = dict(
+    type='ControlStableDiffusionImg2Img',
+    vae=dict(
+        type='AutoencoderKL',
+        from_pretrained=stable_diffusion_v15_url,
+        subfolder='vae'),
+    unet=dict(
+        type='UNet2DConditionModel',
+        subfolder='unet',
+        from_pretrained=stable_diffusion_v15_url),
+    text_encoder=dict(
+        type='ClipWrapper',
+        clip_type='huggingface',
+        pretrained_model_name_or_path=stable_diffusion_v15_url,
+        subfolder='text_encoder'),
+    tokenizer=stable_diffusion_v15_url,
+    controlnet=dict(
+        type='ControlNetModel', from_pretrained=controlnet_hed_url),
+    scheduler=dict(
+        type='DDPMScheduler',
+        from_pretrained=stable_diffusion_v15_url,
+        subfolder='scheduler'),
+    test_scheduler=dict(
+        type='DDIMScheduler',
+        from_pretrained=stable_diffusion_v15_url,
+        subfolder='scheduler'),
+    data_preprocessor=dict(type='EditDataPreprocessor'),
+    init_cfg=dict(type='init_from_unet'),
+    enable_xformers=False,
+)
