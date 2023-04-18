01f50c667fdf78f4a43996325ed78638fc17ec95
rangoliu <liuwenran@users.noreply.github.com>
2023-04-18 13:54:53 +0800

    [Fix] fix controlnet animation when image shape is not 64x (#1752)
    * bugfix
    
    * fix lint
    
    ---------
    
    Co-authored-by: Z-Fran <49083766+Z-Fran@users.noreply.github.com>

--- mmedit/apis/inferencers/controlnet_animation_inferencer.py
+++ mmedit/apis/inferencers/controlnet_animation_inferencer.py
@@ -164,7 +164,12 @@ class ControlnetAnimationInferencer(BaseMMEditInferencer):
         else:
             image = load_image(all_images[0])
         image = image.resize((image_width, image_height))
-        hed_image = self.hed(image, image_resolution=image_width)
+        detect_resolution = min(image_width, image_height)
+        hed_image = self.hed(
+            image,
+            detect_resolution=detect_resolution,
+            image_resolution=detect_resolution)
+        hed_image = hed_image.resize((image_width, image_height))
 
         result = self.pipe.infer(
             control=hed_image,
