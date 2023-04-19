c47dd2b1152eba41df87e7383afc5527dc41becc
zhangjingdong <1396925302@qq.com>
2023-04-19 16:41:56 +0800

    rename to mmagic in projects

--- projects/glide/models/text2im_unet.py
+++ projects/glide/models/text2im_unet.py
@@ -1,8 +1,8 @@
 import torch
 import torch.nn as nn
 
-from mmedit.models import DenoisingUnet
-from mmedit.registry import MODELS
+from mmagic.models import DenoisingUnet
+from mmagic.registry import MODELS
 from .glide_modules import Transformer
 from .glide_tokenizer import get_encoder
 
