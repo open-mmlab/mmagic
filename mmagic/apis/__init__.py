ba976ec04efc4c7224c2e4eba877d81559fc1392
liuwenran <448073814@qq.com>
2023-04-24 15:56:37 +0800

    remove old demos

--- mmagic/apis/__init__.py
+++ mmagic/apis/__init__.py
@@ -1,18 +1,4 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .inferencers import (Inferencers, calculate_grid_size,
-                          colorization_inference, delete_cfg, init_model,
-                          inpainting_inference, matting_inference,
-                          restoration_face_inference, restoration_inference,
-                          restoration_video_inference,
-                          sample_conditional_model, sample_img2img_model,
-                          sample_unconditional_model, set_random_seed,
-                          video_interpolation_inference)
+from .inferencers import Inferencers
 
-__all__ = [
-    'init_model', 'delete_cfg', 'set_random_seed', 'matting_inference',
-    'inpainting_inference', 'restoration_inference',
-    'restoration_video_inference', 'restoration_face_inference',
-    'video_interpolation_inference', 'sample_conditional_model',
-    'sample_unconditional_model', 'sample_img2img_model',
-    'colorization_inference', 'calculate_grid_size', 'Inferencers'
-]
+__all__ = ['Inferencers']
