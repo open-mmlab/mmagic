8d5b377b1987cdcdf624245160c35e23c6342723
zhangjingdong <1396925302@qq.com>
2023-04-28 18:15:55 +0800

    fix

--- tests/test_models/test_editors/test_stable_diffusion/test_stable_diffusion_tomesd.py
+++ tests/test_models/test_editors/test_stable_diffusion/test_stable_diffusion_tomesd.py
@@ -97,7 +97,10 @@ model = dict(
 @pytest.mark.skipif(
     'win' in platform.system().lower(),
     reason='skip on windows due to limited RAM.')
-@pytest.mark.skipif(torch.__version__ < '1.12.1', reason='required method')
+@pytest.mark.skipif(
+    not hasattr(torch.Tensor, 'scatter_reduce')
+    or torch.__version__ < '1.12.1',
+    reason='required method')
 def test_stable_diffusion():
     StableDiffuser = MODELS.build(Config(model))
     StableDiffuser.tokenizer = dummy_tokenizer()
