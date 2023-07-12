6474d0160208067e575d13452873391e82bae37a
LeoXing1996 <xingzn1996@hotmail.com>
2023-07-12 19:34:15 +0800

    fix num_images_per_prompt in controlnet + revise ut

--- tests/test_models/test_editors/test_controlnet/test_controlnet.py
+++ tests/test_models/test_editors/test_controlnet/test_controlnet.py
@@ -94,20 +94,58 @@ class TestControlStableDiffusion(TestCase):
         control_sd = self.control_sd
         control = torch.ones([1, 3, 64, 64])
 
-        def mock_encode_prompt(*args, **kwargs):
-            return torch.randn(2, 5, 16)  # 2 for cfg
+        def mock_encode_prompt(prompt, do_classifier_free_guidance,
+                               num_images_per_prompt, *args, **kwargs):
+            batch_size = len(prompt) if isinstance(prompt, list) else 1
+            batch_size *= num_images_per_prompt
+            if do_classifier_free_guidance:
+                batch_size *= 2
+            return torch.randn(batch_size, 5, 16)  # 2 for cfg
 
         encode_prompt = control_sd._encode_prompt
         control_sd._encode_prompt = mock_encode_prompt
 
+        prompt = 'an insect robot preparing a delicious meal'
+
+        # one prompt, one control, repeat 1 time
         result = control_sd.infer(
-            'an insect robot preparing a delicious meal',
+            prompt,
             control=control,
             height=64,
             width=64,
             num_inference_steps=1,
             return_type='numpy')
         assert result['samples'].shape == (1, 3, 64, 64)
+
+        # two prompt, two control, repeat 1 time
+        result = control_sd.infer([prompt, prompt],
+                                  control=control,
+                                  height=64,
+                                  width=64,
+                                  num_inference_steps=1,
+                                  return_type='numpy')
+        assert result['samples'].shape == (2, 3, 64, 64)
+
+        # one prompt, one control, repeat 2 times
+        result = control_sd.infer(
+            prompt,
+            control=control,
+            height=64,
+            width=64,
+            num_images_per_prompt=2,
+            num_inference_steps=1,
+            return_type='numpy')
+        assert result['samples'].shape == (2, 3, 64, 64)
+
+        # two prompt, two control, repeat 2 times
+        result = control_sd.infer([prompt, prompt],
+                                  control=[control, control],
+                                  height=64,
+                                  width=64,
+                                  num_images_per_prompt=2,
+                                  num_inference_steps=1,
+                                  return_type='numpy')
+        assert result['samples'].shape == (4, 3, 64, 64)
 
         control_sd._encode_prompt = encode_prompt
 
@@ -126,13 +164,20 @@ class TestControlStableDiffusion(TestCase):
         def mock_encode_prompt(*args, **kwargs):
             return torch.randn(2, 5, 16)  # 2 for cfg
 
+        def mock_infer(*args, **kwargs):
+            return dict(samples=torch.randn(2, 3, 64, 64))
+
         encode_prompt = control_sd._encode_prompt
         control_sd._encode_prompt = mock_encode_prompt
+
+        infer = control_sd.infer
+        control_sd.infer = mock_infer
 
         # control_sd.text_encoder = mock_text_encoder()
         output = control_sd.val_step(data)
         assert len(output) == 1
         control_sd._encode_prompt = encode_prompt
+        control_sd.infer = infer
 
     def test_test_step(self):
         control_sd = self.control_sd
@@ -149,13 +194,20 @@ class TestControlStableDiffusion(TestCase):
         def mock_encode_prompt(*args, **kwargs):
             return torch.randn(2, 5, 16)  # 2 for cfg
 
+        def mock_infer(*args, **kwargs):
+            return dict(samples=torch.randn(2, 3, 64, 64))
+
         encode_prompt = control_sd._encode_prompt
         control_sd._encode_prompt = mock_encode_prompt
+
+        infer = control_sd.infer
+        control_sd.infer = mock_infer
 
         # control_sd.text_encoder = mock_text_encoder()
         output = control_sd.test_step(data)
         assert len(output) == 1
         control_sd._encode_prompt = encode_prompt
+        control_sd.infer = infer
 
     def test_train_step(self):
         control_sd = self.control_sd
