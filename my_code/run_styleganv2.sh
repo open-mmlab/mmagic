42eae6dc5b3563fa672e6f294da8bf96a9a2a6aa
Qi Sun 孙启 <55343765+qsun1@users.noreply.github.com>
2023-08-01 22:35:32 +0800

    Delete my_code directory

--- my_code/run_styleganv2.sh
+++
@@ -1,8 +0,0 @@
-# export PYTHONPATH=./my_code/torch_utils:$PYTHONPATH
-export PYTHONPATH=./my_code:$PYTHONPATH
-python demo/mmagic_inference_demo.py \
-        --model-name styleganv2 \
-        --model-config stylegan2_lion_512x512 \
-        --result-out-dir my_code/tmp_res/1.jpg \
-        --model-ckpt  ./my_code/new_ckpts/stylegan2_lions_512_pytorch_mmagic.pth \
-        --seed 0 \
