6694a252930ca784354137ae27f3a33c8376f8b5
aptsunny <36404164+aptsunny@users.noreply.github.com>
2023-08-01 23:06:54 +0800

    adapt exist diffusion arch

---
+++ mmagic/models/editors/stablesr/fusion_block.py
@@ -0,0 +1,77 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+class ResidualDenseBlock(nn.Module):
+    """Residual Dense Block.
+
+    Used in RRDB block in ESRGAN.
+
+    Args:
+        num_feat (int): Channel number of intermediate features.
+        num_grow_ch (int): Channels for each growth.
+    """
+
+    def __init__(self, num_feat=64, num_grow_ch=32):
+        super(ResidualDenseBlock, self).__init__()
+        self.conv1 = nn.Conv2d(num_feat, num_grow_ch, 3, 1, 1)
+        self.conv2 = nn.Conv2d(num_feat + num_grow_ch, num_grow_ch, 3, 1, 1)
+        self.conv3 = nn.Conv2d(num_feat + 2 * num_grow_ch, num_grow_ch, 3, 1,
+                               1)
+        self.conv4 = nn.Conv2d(num_feat + 3 * num_grow_ch, num_grow_ch, 3, 1,
+                               1)
+        self.conv5 = nn.Conv2d(num_feat + 4 * num_grow_ch, num_feat, 3, 1, 1)
+
+        self.lrelu = nn.LeakyReLU(negative_slope=0.2, inplace=True)
+
+        # initialization
+        default_init_weights(
+            [self.conv1, self.conv2, self.conv3, self.conv4, self.conv5], 0.1)
+
+    def forward(self, x):
+        x1 = self.lrelu(self.conv1(x))
+        x2 = self.lrelu(self.conv2(torch.cat((x, x1), 1)))
+        x3 = self.lrelu(self.conv3(torch.cat((x, x1, x2), 1)))
+        x4 = self.lrelu(self.conv4(torch.cat((x, x1, x2, x3), 1)))
+        x5 = self.conv5(torch.cat((x, x1, x2, x3, x4), 1))
+        # Empirically, we use 0.2 to scale the residual for better performance
+        return x5 * 0.2 + x
+
+
+class RRDB(nn.Module):
+    """Residual in Residual Dense Block.
+
+    Used in RRDB-Net in ESRGAN.
+
+    Args:
+        num_feat (int): Channel number of intermediate features.
+        num_grow_ch (int): Channels for each growth.
+    """
+
+    def __init__(self, num_feat, num_grow_ch=32):
+        super(RRDB, self).__init__()
+        self.rdb1 = ResidualDenseBlock(num_feat, num_grow_ch)
+        self.rdb2 = ResidualDenseBlock(num_feat, num_grow_ch)
+        self.rdb3 = ResidualDenseBlock(num_feat, num_grow_ch)
+
+    def forward(self, x):
+        out = self.rdb1(x)
+        out = self.rdb2(out)
+        out = self.rdb3(out)
+        # Empirically, we use 0.2 to scale the residual for better performance
+        return out * 0.2 + x
+
+
+class Fuse_sft_block_RRDB(nn.Module):
+
+    def __init__(self, in_ch, out_ch, num_block=1, num_grow_ch=32):
+        super().__init__()
+        self.encode_enc_1 = ResBlock(2 * in_ch, in_ch)
+        self.encode_enc_2 = make_layer(
+            RRDB, num_block, num_feat=in_ch, num_grow_ch=num_grow_ch)
+        self.encode_enc_3 = ResBlock(in_ch, out_ch)
+
+    def forward(self, enc_feat, dec_feat, w=1):
+        enc_feat = self.encode_enc_1(torch.cat([enc_feat, dec_feat], dim=1))
+        enc_feat = self.encode_enc_2(enc_feat)
+        enc_feat = self.encode_enc_3(enc_feat)
+        residual = w * enc_feat
+        out = dec_feat + residual
+        return out
---
+++ tests/test_models/test_editors/test_stablesr/test_vqgan.py
@@ -0,0 +1,24 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import torch
+
+from mmagic.models.editors.stable_diffusion.vae import AutoencoderKL
+
+
+def test_vq_encode():
+    input = torch.rand((2, 3, 512, 512))
+    vae = AutoencoderKL(
+        latent_channels=4,
+        down_block_types=('DownEncoderBlock2D', 'DownEncoderBlock2D',
+                          'DownEncoderBlock2D', 'DownEncoderBlock2D'),
+        up_block_types=('UpDecoderBlock2D', 'UpDecoderBlock2D',
+                        'UpDecoderBlock2D', 'UpDecoderBlock2D'),
+        block_out_channels=(128, 256, 512, 512),
+        layers_per_block=2,
+    )
+    _, enc_fea_lq = vae.encode(input, return_feat=True)
+    assert enc_fea_lq[1].shape == (2, 256, 256, 256)
+    assert enc_fea_lq[2].shape == (2, 512, 128, 128)
+
+    samples = torch.rand((2, 4, 64, 64))
+    x_sample = vae.decode(samples, enc_fea_lq[1:3])
+    assert x_sample.shape == (2, 3, 512, 512)
