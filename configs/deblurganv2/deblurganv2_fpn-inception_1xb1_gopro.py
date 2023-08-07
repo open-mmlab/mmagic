cb281d6b9b97b99b13ed1d765e611610ac5bf2c0
xiaomile <15622388695@163.com>
2023-08-07 09:03:06 +0800

    Update configs/deblurganv2/deblurganv2_fpn-inception_1xb1_gopro.py
    Co-authored-by: Yanhong Zeng <zengyh1900@gmail.com>

--- configs/deblurganv2/deblurganv2_fpn-inception_1xb1_gopro.py
+++ configs/deblurganv2/deblurganv2_fpn-inception_1xb1_gopro.py
@@ -128,7 +128,6 @@ val_dataloader = dict(
         type='BasicImageDataset',
         metainfo=dict(dataset_type='gopro', task_name='deblur'),
         data_root=data_root + 'test',
-        # ann_file='meta_info_gopro_test.txt',
         data_prefix=dict(img='input', gt='target'),
         pipeline=val_pipeline))
 
