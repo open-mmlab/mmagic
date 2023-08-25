1c69b3b8c4f97e62560c18e8cbc463f13424db19
limafang <21009201106@stu.xidian.edu.cn>
2023-08-25 19:39:52 +0800

    add_new_config_biggan

---
+++ mmagic/configs/_base_/models/biggan/base_biggan_128x128.py
@@ -0,0 +1,28 @@
+model = dict(
+    type='BigGAN',
+    num_classes=1000,
+    data_preprocessor=dict(type='DataPreprocessor'),
+    generator=dict(
+        type='BigGANGenerator',
+        output_scale=128,
+        noise_size=120,
+        num_classes=1000,
+        base_channels=96,
+        shared_dim=128,
+        with_shared_embedding=True,
+        sn_eps=1e-6,
+        act_cfg=dict(type='ReLU', inplace=True),
+        split_noise=True,
+        auto_sync_bn=False,
+        init_cfg=dict(type='ortho')),
+    discriminator=dict(
+        type='BigGANDiscriminator',
+        input_scale=128,
+        num_classes=1000,
+        base_channels=96,
+        sn_eps=1e-6,
+        act_cfg=dict(type='ReLU', inplace=True),
+        with_spectral_norm=True,
+        init_cfg=dict(type='ortho')),
+    generator_steps=1,
+    discriminator_steps=1)
