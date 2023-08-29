db3a63057611ca403e61477f807a10b9313b7c69
小飞猪 <106524776+ooooo-create@users.noreply.github.com>
2023-08-29 20:29:46 +0800

    [CodeCamp2023-648]MMagic 新 config 体验与适配 GuidedDiffusion (#2005)
    * fix
    
    * fix code
    
    * fix datasets
    
    * fix style

---
+++ mmagic/configs/guided_diffusion/adm-g_ddim25_8xb32_imagenet_64x64.py
@@ -0,0 +1,39 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from mmengine.config import read_base
+
+with read_base():
+    from .adm_ddim250_8xb32_imagenet_64x64 import *
+
+from mmagic.evaluation.metrics import FrechetInceptionDistance
+from mmagic.models.editors.guided_diffusion.classifier import EncoderUNetModel
+
+model.update(
+    dict(
+        classifier=dict(
+            type=EncoderUNetModel,
+            image_size=64,
+            in_channels=3,
+            model_channels=128,
+            out_channels=1000,
+            num_res_blocks=4,
+            attention_resolutions=(2, 4, 8),
+            channel_mult=(1, 2, 3, 4),
+            use_fp16=False,
+            num_head_channels=64,
+            use_scale_shift_norm=True,
+            resblock_updown=True,
+            pool='attention')))
+
+metrics = [
+    dict(
+        type=FrechetInceptionDistance,
+        prefix='FID-Full-50k',
+        fake_nums=50000,
+        inception_style='StyleGAN',
+        sample_model='orig',
+        sample_kwargs=dict(
+            num_inference_steps=250, show_progress=True, classifier_scale=1.))
+]
+
+val_evaluator = dict(metrics=metrics)
+test_evaluator = dict(metrics=metrics)
