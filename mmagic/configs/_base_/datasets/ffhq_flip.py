e95667dec06c1133714c4f926c949e29bf93f53b
hhy150 <2334813735@qq.com>
2023-09-09 19:47:28 +0800

    CodeCamp2023_652_2

--- mmagic/configs/_base_/datasets/ffhq_flip.py
+++ mmagic/configs/_base_/datasets/ffhq_flip.py
@@ -1,15 +1,22 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-dataset_type = 'BasicImageDataset'
+from mmengine.dataset.sampler import DefaultSampler, InfiniteSampler
+
+from mmagic.datasets.basic_image_dataset import BasicImageDataset
+from mmagic.datasets.transforms.aug_shape import Flip
+from mmagic.datasets.transforms.formatting import PackInputs
+from mmagic.datasets.transforms.loading import LoadImageFromFile
+
+dataset_type = BasicImageDataset
 
 train_pipeline = [
-    dict(type='LoadImageFromFile', key='gt'),
-    dict(type='Flip', keys=['gt'], direction='horizontal'),
-    dict(type='PackInputs', keys='gt')
+    dict(type=LoadImageFromFile, key='gt'),
+    dict(type=Flip, keys=['gt'], direction='horizontal'),
+    dict(type=PackInputs, keys='gt')
 ]
 
 val_pipeline = [
-    dict(type='LoadImageFromFile', key='gt'),
-    dict(type='PackInputs', keys=['gt'])
+    dict(type=LoadImageFromFile, key='gt'),
+    dict(type=PackInputs, keys=['gt'])
 ]
 
 # `batch_size` and `data_root` need to be set.
@@ -17,7 +24,7 @@ train_dataloader = dict(
     batch_size=4,
     num_workers=8,
     persistent_workers=True,
-    sampler=dict(type='InfiniteSampler', shuffle=True),
+    sampler=dict(type=InfiniteSampler, shuffle=True),
     dataset=dict(
         type=dataset_type,
         data_prefix=dict(gt=''),
@@ -32,7 +39,7 @@ val_dataloader = dict(
         data_prefix=dict(gt=''),
         data_root=None,  # set by user
         pipeline=val_pipeline),
-    sampler=dict(type='DefaultSampler', shuffle=False),
+    sampler=dict(type=DefaultSampler, shuffle=False),
     persistent_workers=True)
 
 test_dataloader = dict(
@@ -43,5 +50,5 @@ test_dataloader = dict(
         data_prefix=dict(gt=''),
         data_root=None,  # set by user
         pipeline=val_pipeline),
-    sampler=dict(type='DefaultSampler', shuffle=False),
+    sampler=dict(type=DefaultSampler, shuffle=False),
     persistent_workers=True)
--- mmagic/configs/_base_/datasets/unconditional_imgs_flip_512x512.py
+++ mmagic/configs/_base_/datasets/unconditional_imgs_flip_512x512.py
@@ -1,12 +1,19 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-dataset_type = 'BasicImageDataset'
+from mmengine.dataset.sampler import DefaultSampler, InfiniteSampler
+
+from mmagic.datasets.basic_image_dataset import BasicImageDataset
+from mmagic.datasets.transforms.aug_shape import Flip, Resize
+from mmagic.datasets.transforms.formatting import PackInputs
+from mmagic.datasets.transforms.loading import LoadImageFromFile
+
+dataset_type = BasicImageDataset
 
 # TODO:
 train_pipeline = [
-    dict(type='LoadImageFromFile', key='gt'),
-    dict(type='Resize', keys='gt', scale=(512, 512)),
-    dict(type='Flip', keys=['gt'], direction='horizontal'),  # TODO:
-    dict(type='PackInputs')
+    dict(type=LoadImageFromFile, key='gt'),
+    dict(type=Resize, keys='gt', scale=(512, 512)),
+    dict(type=Flip, keys=['gt'], direction='horizontal'),  # TODO:
+    dict(type=PackInputs)
 ]
 
 # `batch_size` and `data_root` need to be set.
@@ -14,7 +21,7 @@ train_dataloader = dict(
     batch_size=None,
     num_workers=4,
     persistent_workers=True,
-    sampler=dict(type='InfiniteSampler', shuffle=True),
+    sampler=dict(type=InfiniteSampler, shuffle=True),
     dataset=dict(
         type=dataset_type,
         data_prefix=dict(gt=''),
@@ -29,7 +36,7 @@ val_dataloader = dict(
         data_prefix=dict(gt=''),
         data_root=None,  # set by user
         pipeline=train_pipeline),
-    sampler=dict(type='DefaultSampler', shuffle=False),
+    sampler=dict(type=DefaultSampler, shuffle=False),
     persistent_workers=True)
 
 test_dataloader = dict(
@@ -40,5 +47,5 @@ test_dataloader = dict(
         data_prefix=dict(gt=''),
         data_root=None,  # set by user
         pipeline=train_pipeline),
-    sampler=dict(type='DefaultSampler', shuffle=False),
+    sampler=dict(type=DefaultSampler, shuffle=False),
     persistent_workers=True)
