a08e162764637d04915fa13c3ce78440e5e29237
Allyoung <allyoung_362@163.com>
2023-09-14 10:08:54 +0800

    [CodeCamp2023-646] MMagic新config体验与适配 Textual Inversion

--- configs/textual_inversion/textual_inversion.py
+++ configs/textual_inversion/textual_inversion.py
@@ -1,4 +1,26 @@
-_base_ = '../_base_/gen_default_runtime.py'
+from mmengine.config import read_base
+
+with read_base():
+    from .._base_.gen_default_runtime import *
+
+from mmagic.models.editors.textual_inversion import TextualInversion
+# from mmagic.models.editors.stable_diffusion import AutoencoderKL
+#AutoencoderKL.__init__() got an unexpected keyword argument 'from_pretrained'
+from mmagic.datasets.textual_inversion_dataset import TextualInversionDataset
+# from mmagic.models.diffusion_schedulers.ddim_scheduler import EditDDIMScheduler
+# from mmagic.models.diffusion_schedulers.ddpm_scheduler import EditDDPMScheduler
+from mmagic.models.editors.disco_diffusion.clip_wrapper import ClipWrapper
+from mmagic.models.data_preprocessors.data_preprocessor import DataPreprocessor
+from mmagic.datasets.transforms.loading import LoadImageFromFile
+
+from mmagic.engine.hooks.visualization_hook import VisualizationHook
+from mmengine.dataset.sampler import InfiniteSampler
+from mmengine.hooks import CheckpointHook, LoggerHook
+from mmagic.datasets.transforms.formatting import PackInputs
+
+from mmagic.datasets.transforms.aug_shape import Resize
+
+from torch.optim import AdamW
 
 # config for model
 dtype = 'fp16'
@@ -13,7 +35,7 @@ val_prompts = [
 ]
 
 model = dict(
-    type='TextualInversion',
+    type=TextualInversion,
     placeholder_token=placeholder_token,
     vae=dict(
         type='AutoencoderKL',
@@ -24,7 +46,7 @@ model = dict(
         from_pretrained=stable_diffusion_v15_url,
         subfolder='unet'),
     text_encoder=dict(
-        type='ClipWrapper',
+        type=ClipWrapper,
         clip_type='huggingface',
         pretrained_model_name_or_path=stable_diffusion_v15_url,
         subfolder='text_encoder'),
@@ -40,24 +62,24 @@ model = dict(
         type='DDIMScheduler',
         from_pretrained=stable_diffusion_v15_url,
         subfolder='scheduler'),
-    data_preprocessor=dict(type='DataPreprocessor', data_keys=None))
+    data_preprocessor=dict(type=DataPreprocessor, data_keys=None))
 
 train_cfg = dict(max_iters=3000)
 
-optim_wrapper = dict(
+optim_wrapper.update(
     modules='.*trainable_embeddings',
-    optimizer=dict(type='AdamW', lr=5e-4),
+    optimizer=dict(type=AdamW, lr=5e-4),
     accumulative_counts=1)
 
 pipeline = [
-    dict(type='LoadImageFromFile', key='img', channel_order='rgb'),
-    dict(type='Resize', scale=(512, 512)),
-    dict(type='PackInputs')
+    dict(type=LoadImageFromFile, key='img', channel_order='rgb'),
+    dict(type=Resize, scale=(512, 512)),
+    dict(type=PackInputs)
 ]
 
-dataset = dict(
-    type='TextualInversionDataset',
-    data_root='./data/',
+dataset=dict(
+    type=TextualInversionDataset,
+    data_root='./tests/data/Textual_Inversion/data/',
     concept_dir='cat_toy',
     placeholder=placeholder_token,
     pipeline=pipeline)
@@ -65,18 +87,18 @@ dataset = dict(
 train_dataloader = dict(
     dataset=dataset,
     num_workers=16,
-    sampler=dict(type='InfiniteSampler', shuffle=True),
+    sampler=dict(type=InfiniteSampler, shuffle=True),
     persistent_workers=True,
     batch_size=1)
 val_cfg = val_evaluator = val_dataloader = None
 test_cfg = test_evaluator = test_dataloader = None
 
 default_hooks = dict(
-    logger=dict(interval=10),
-    checkpoint=dict(type='CheckpointHook', interval=10))
+    logger=dict(type=LoggerHook, interval=10),
+    checkpoint=dict(type=CheckpointHook, interval=10))
 custom_hooks = [
     dict(
-        type='VisualizationHook',
+        type=VisualizationHook,
         interval=50,
         fixed_input=True,
         # visualize train dataset
