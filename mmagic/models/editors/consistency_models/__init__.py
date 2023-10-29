b62c71fa8c8df495286b6b859e3d7ca31fdf8845
xiaomile <15622388695@163.com>
2023-10-29 17:50:41 +0800

    delete code of consistency model

--- mmagic/models/editors/consistency_models/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .consistencymodel import ConsistencyModel
-from .consistencymodel_modules import ConsistencyUNetModel, KarrasDenoiser
-
-__all__ = ['ConsistencyModel', 'ConsistencyUNetModel', 'KarrasDenoiser']
