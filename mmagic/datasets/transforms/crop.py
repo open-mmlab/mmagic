eb3b3e440609d600218ab42bba63d07cbd4ef77b
Yanhong Zeng <zengyh1900@gmail.com>
2023-11-10 21:03:54 +0800

    support using from_pretrained for instance_crop (#2066)
    * support using from_pretrained for instance_crop
    
    * fix lint

--- mmagic/datasets/transforms/crop.py
+++ mmagic/datasets/transforms/crop.py
@@ -958,6 +958,7 @@ class InstanceCrop(BaseTransform):
 
     def __init__(self,
                  config_file,
+                 from_pretrained=None,
                  key='img',
                  box_num_upbound=-1,
                  finesize=256):
@@ -967,6 +968,11 @@ class InstanceCrop(BaseTransform):
             "\"mim install 'mmdet >= 3.0.0'\".")
 
         cfg = get_config(config_file, pretrained=True)
+
+        # loading checkpoint from local path
+        if from_pretrained is not None:
+            cfg.model.backbone.init_cfg.checkpoint = from_pretrained
+
         with DefaultScope.overwrite_default_scope('mmdet'):
             self.predictor = mmdet_apis.init_detector(cfg, cfg.model_path)
 
