5b3e15fd4beb080e70c86bb83aa4120995bb679d
liuwenran <448073814@qq.com>
2023-11-13 16:36:39 +0800

    fix

--- tests/test_models/test_archs/test_wrapper.py
+++ tests/test_models/test_archs/test_wrapper.py
@@ -12,7 +12,7 @@ from mmengine.utils.dl_utils import TORCH_VERSION
 from mmagic.registry import MODELS
 from mmagic.utils import register_all_modules
 
-test_dir = osp.join(osp.dirname(__file__), '../../..', 'tests')
+test_dir = osp.join(osp.dirname(__file__), '..', '..', '..', 'tests')
 config_path = osp.join(test_dir, 'configs', 'diffuser_wrapper_cfg')
 model_path = osp.join(test_dir, 'configs', 'tmp_weight')
 ckpt_path = osp.join(test_dir, 'configs', 'ckpt')
