374ee5016c35c40215d8c97b6df1ff8952867128
rangoliu <liuwenran@users.noreply.github.com>
2023-12-07 21:35:21 +0800

    [Enhance] Improve powerpaint (#2080)
    * add readme
    
    * add readme
    
    * add require
    
    * fix readme
    
    * fix according to readme
    
    * fix lint

--- projects/powerpaint/gradio_PowerPaint.py
+++ projects/powerpaint/gradio_PowerPaint.py
@@ -34,9 +34,9 @@ add_tokens(
     initialize_tokens=['a', 'a', 'a'],
     num_vectors_per_token=10)
 pipe.unet.load_state_dict(
-    torch.load('./models/unet/diffusion_pytorch_model.bin'), strict=False)
+    torch.load('./models/diffusion_pytorch_model.bin'), strict=False)
 pipe.text_encoder.load_state_dict(
-    torch.load('./models/text_encoder/pytorch_model.bin'), strict=False)
+    torch.load('./models/pytorch_model.bin'), strict=False)
 pipe = pipe.to('cuda')
 
 depth_estimator = DPTForDepthEstimation.from_pretrained(
