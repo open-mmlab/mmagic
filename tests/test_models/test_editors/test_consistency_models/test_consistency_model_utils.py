1bb079628e9c39afad50b66daecbf5bc000459c5
xiaomile <15622388695@163.com>
2023-12-13 16:47:40 +0800

    Update test_consistency_model_utils.py

--- tests/test_models/test_editors/test_consistency_models/test_consistency_model_utils.py
+++ tests/test_models/test_editors/test_consistency_models/test_consistency_model_utils.py
@@ -9,9 +9,9 @@ import torch
 
 from mmagic.models.editors.consistency_models.consistencymodel_utils import (
     DeterministicGenerator, DeterministicIndividualGenerator, DummyGenerator,
-    get_generator, get_sample_fn, karras_sample, sample_dpm, sample_euler,
-    sample_euler_ancestral, sample_heun, sample_onestep, sample_progdist,
-    stochastic_iterative_sampler)
+    get_generator, get_sample_fn, get_sigmas_karras, karras_sample, sample_dpm,
+    sample_euler, sample_euler_ancestral, sample_heun, sample_onestep,
+    sample_progdist, stochastic_iterative_sampler)
 from mmagic.registry import MODELS
 from mmagic.utils import register_all_modules
 
@@ -82,12 +82,69 @@ class TestConsistencyModelUtils(TestCase):
         self.assertEqual(get_sample_fn('dpm'), sample_dpm)
         self.assertEqual(get_sample_fn('ancestral'), sample_euler_ancestral)
         self.assertEqual(get_sample_fn('onestep'), sample_onestep)
-        self.assertEqual(get_sample_fn('progdist'), sample_progdist)
         self.assertEqual(get_sample_fn('euler'), sample_euler)
         self.assertEqual(
             get_sample_fn('multistep'), stochastic_iterative_sampler)
+        self.assertEqual(get_sample_fn('progdist'), sample_progdist)
         with pytest.raises(KeyError):
             get_sample_fn('')
+
+        clip_denoised = True
+        unet_cfg = deepcopy(unet_config)
+        diffuse_cfg = deepcopy(denoiser_config)
+        unet = MODELS.build(unet_cfg)
+        diffuse = MODELS.build(diffuse_cfg)
+        model_kwargs = {}
+        device = torch.device('cpu')
+        sigma_max = 80
+        sigma_min = 0.002
+        s_churn = 0.0
+        s_tmin = 0.0
+        s_tmax = float('inf')
+        s_noise = 1.0
+        ts = (1, 2)
+        shape = (4, 3, 64, 64)
+        generator = get_generator('dummy')
+        x_T = generator.randn(*shape, device=device) * sigma_max
+
+        def denoiser(x_t, sigma):
+            """denoiser function."""
+            _, denoised = diffuse.denoise(unet, x_t, sigma, **model_kwargs)
+            if clip_denoised:
+                denoised = denoised.clamp(-1, 1)
+            return denoised
+
+        sample_list = [
+            'heun', 'dpm', 'ancestral', 'onestep', 'progdist', 'euler',
+            'multistep'
+        ]
+        for sample in sample_list:
+            if sample == 'progdist':
+                sigmas = get_sigmas_karras(
+                    2 + 1, sigma_min, sigma_max, 7.0, device=device)
+            else:
+                sigmas = get_sigmas_karras(
+                    2, sigma_min, sigma_max, 7.0, device=device)
+            if sample in ['heun', 'dpm']:
+                sampler_args = dict(
+                    s_churn=s_churn,
+                    s_tmin=s_tmin,
+                    s_tmax=s_tmax,
+                    s_noise=s_noise)
+            elif sample == 'multistep':
+                sampler_args = dict(
+                    ts=ts, t_min=sigma_min, t_max=sigma_max, rho=7.0, steps=2)
+            else:
+                sampler_args = {}
+            assert get_sample_fn(sample)(
+                denoiser,
+                x_T,
+                sigmas,
+                generator,
+                progress=False,
+                callback=None,
+                **sampler_args,
+            ).clamp(-1, 1).shape == shape
 
 
 def teardown_module():
